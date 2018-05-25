c65a955695b71186bf05378241c48891bd7f08fc
gooseandmegander <meganawilliamson@gmail.com>
2018-05-25 10:05:09 -0700

    handling undefined path "." complete

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <stdlib.h>
 
 /**
  * Main
@@ -7,6 +8,33 @@
 int main(int argc, char **argv)
 {
   // Parse command line
+  int i;
+  DIR *dirPtr;
+  struct dirent *direntPtr;
+
+  printf("There are %d command line argument(s):\n\n", argc);
+
+  if (!argv[1])
+  {
+    dirPtr = opendir(".");
+
+    while ((direntPtr = readdir(dirPtr)) != NULL)
+    {
+      printf("%s\n", direntPtr->d_name);
+    }
+
+    int isClosed = closedir(dirPtr);
+    if (isClosed != 0)
+    {
+      printf("CLOSEDIR unsuccessful");
+    }
+    exit(0);
+  }
+
+  for (i = 1; i < argc; i++)
+  {
+    printf("   %s\n", argv[i]);
+  }
 
   // Open directory
 

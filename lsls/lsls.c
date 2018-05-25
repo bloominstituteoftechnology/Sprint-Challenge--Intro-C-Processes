06bd048468b0b70337bfffdac8baa191a2299aeb
zackhitch <zackhitch@users.noreply.github.com>
2018-05-25 10:40:42 -0700

    Completed w/ Stretch - Zack Hitchcock

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <dirent.h>
+#include <sys/stat.h>
 
 /**
  * Main
@@ -7,12 +9,47 @@
 int main(int argc, char **argv)
 {
   // Parse command line
+  DIR *dir;
+  char *currentDir;
+  struct dirent *ent;
+  struct stat buf;
+
+  if (argc > 1)
+  {
+    currentDir = argv[1];
+  }
+  else
+  {
+    currentDir = ".";
+  }
 
   // Open directory
-
-  // Repeatly read and print entries
-
-  // Close directory
-
+  if ((dir = opendir(currentDir)) == NULL)
+  {
+    printf("Directory is empty\n");
+    exit(1);
+  }
+  else
+  {
+    // Repeatedly read and print entries
+    while ((ent = readdir(dir)) != NULL)
+    {
+      stat(ent->d_name, &buf);
+      if ((buf.st_mode & S_IFDIR) != 0)
+      {
+        printf("%10s %s\n", "<DIR>", ent->d_name);
+      }
+      else if ((buf.st_mode & S_IFREG) != 0)
+      {
+        printf("%10lld %s\n", buf.st_size, ent->d_name);
+      }
+      else
+      {
+        printf("%10s\n", ent->d_name);
+      }
+    }
+    // Close directory
+    closedir(dir);
+  }
   return 0;
 }

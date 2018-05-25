915f8624f8d2866436b8dc07142e4bfded62554b
crypto-jones <jeremyjones@Macintosh-58b035f2f989.local>
2018-05-25 10:56:50 -0700

    directory printing. not sure if there is complete functionality

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <stdlib.h>
 
 /**
  * Main
@@ -8,11 +9,32 @@ int main(int argc, char **argv)
 {
   // Parse command line
 
+  for (int i = 1; i < argc; i++)
+  {
+    printf("%s", argv[i]);
+    return 0;
+  }
+
   // Open directory
+  DIR *dir;
+  struct dirent *sd;
+
+  dir = opendir(".");
+
+  if (dir == NULL)
+  {
+    printf("Error! Unable to open directory.\n");
+    exit(1);
+  }
 
   // Repeatly read and print entries
+  while ((sd = readdir(dir)) != NULL)
+  {
+    printf(">> %s\n", sd->d_name);
+  }
 
   // Close directory
+  closedir(dir);
 
   return 0;
 }

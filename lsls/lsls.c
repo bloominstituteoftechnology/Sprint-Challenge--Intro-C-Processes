84e9abc0d357baf01a7af1eba2d27cb568447632
kait-schorr <kaitlynschorr@gmail.com>
2018-05-25 09:22:09 -0500

    Got basic functionality working!

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,16 +1,41 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <stdlib.h>
 
 /**
  * Main
  */
 int main(int argc, char **argv)
 {
+  DIR *dir;
+  struct dirent *dp;
+  char *filename = ".";
+
   // Parse command line
+  if (argc == 2)
+  {
+    filename = argv[1];
+  }
+  else if (argc > 2)
+  {
+    fprintf(stderr, "You can only list the contents of one directory at a time.\n");
+  }
 
   // Open directory
+  if ((dir = opendir(filename)) == NULL)
+  {
+    fprintf(stderr, "Cannot Open .");
+    exit(1);
+  }
 
   // Repeatly read and print entries
+
+  ;
+
+  while ((dp = readdir(dir)) != NULL)
+  {
+    printf("%s\n", dp->d_name);
+  }
 
   // Close directory
 

7b86e2ee507e32e133de6230a24a264b056210b2
mjd02 <mostdarryl@gmail.com>
2018-05-25 13:20:36 -0400

    completed programming portion

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -8,11 +8,32 @@ int main(int argc, char **argv)
 {
   // Parse command line
 
+  char *path;
+  if(argv[1] != NULL){
+    path = argv[1];
+  }
+  else{
+    path = ".";
+  }
+
   // Open directory
+
+  DIR *directory = opendir(path);
 
   // Repeatly read and print entries
 
+  if(directory == NULL){
+    fprintf(stderr,"error opening directory\n");
+    exit(1);
+  }
+  struct dirent *entry;
+  while( (entry = readdir(directory)) != NULL){
+    printf("%s\n",entry->d_name);
+  }
+
   // Close directory
+
+  closedir(directory);
 
   return 0;
 }

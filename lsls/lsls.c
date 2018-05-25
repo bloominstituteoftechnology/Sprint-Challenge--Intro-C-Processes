cbc136130ca10bc6c246bfe57fce05bf811a5162
Justin Kaseman <justinkaseman@live.com>
2018-05-25 11:06:25 -0700

    Print size

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,27 +1,35 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 #include <dirent.h>
+#include <sys/stat.h>
 
 /**
  * Main
  */
 int main(int argc, char **argv)
 {
+  char *str;
+  char* cmdpath;
   DIR *d;
   struct dirent *ent;
+  struct stat buf;
 
   // Parse command line
   if (argc == 1) {
-    d = opendir(".");
+    char home[2] = ".";
+    str = home;
   }
   else if (argc == 2) {
-    d = opendir(argv[1]);
+    str = argv[1];
   }
   else {
     fprintf(stderr, "Supply a single path to ls\n");
+    exit(1);
   }
 
   // Open directory
+  d = opendir(str);
   if (d == NULL) {
     fprintf(stderr, "Something went horribly wrong\n");
     exit(1);
@@ -30,6 +38,15 @@ int main(int argc, char **argv)
   // Repeatly read and print entries
     ent = readdir(d);
     while (ent != NULL) {
+      size_t len = strlen(str);
+      size_t len2 = strlen(ent->d_name);
+      char *str2 = malloc(len + 1 + len2 + 1 );
+      strcpy(str2, str);
+      str2[len] = '/';
+      strcat(str2, ent->d_name);
+      stat(str2, &buf);
+      memset(str2, 0, sizeof(int));
+      printf("%lld     ", buf.st_size);
       printf("%s\n", ent->d_name);
       ent = readdir(d);
     }

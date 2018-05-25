b87113346d8e9b40fcf337853ba406a2755b8890
Erik Alonso <mr.alonsoea@protonmail.ch>
2018-05-25 12:55:24 -0500

    sprint

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -6,13 +6,40 @@
  */
 int main(int argc, char **argv)
 {
-  // Parse command line
+  struct dirent *dp; // pointer to "dirent"
+  if (argc > 1)
+  {
+    char *arrOfArgs[3];               // pointer to array of chars
+    arrOfArgs[0] = strdup("/bin/ls"); // list program ls
+    arrOfArgs[1] = strdup(argv[v]);
+    arrOfArgs[2] = NULL;
+    execvp(arrOfArgs[0], arrOfArgs) // left argument what is ran right argument for that functions parameters
+  }
+  else
+  {
+    DIR *dr = opendir(".");
+    if (dr == NULL)
+    {
+      printf("Could not open current directory");
+      return 0;
+    }
+    while ((dp = readdir(dr)) != NULL)) printf("%s\n",dp->d_name);
 
-  // Open directory
-
-  // Repeatly read and print entries
-
-  // Close directory
+    closedir(dr);
+  }
 
   return 0;
 }
+
+/*
+structs.. functions.. etc..
+
+dirent
+
+strdup
+
+execvp
+
+opendir
+
+*/

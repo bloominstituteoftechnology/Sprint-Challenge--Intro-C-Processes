a034340f8d20e76aa3672b35102b5665050476db
Justin Kaseman <justinkaseman@live.com>
2018-05-25 09:11:47 -0700

    current directory implementation

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <dirent.h>
 
 /**
@@ -7,12 +8,25 @@
 int main(int argc, char **argv)
 {
   // Parse command line
+  // if (argc == 0) {
+  //   char cl[] = ".";
+  // }
 
   // Open directory
-
+  DIR *d = opendir(".");
+  if (d == NULL) {
+    fprintf(stderr, "Bomb detonated\n");
+    exit(1);
+  }
+  else {
   // Repeatly read and print entries
-
+    struct dirent *ent = readdir(d);
+    while (ent != NULL) {
+      printf("%s\n", ent->d_name);
+      ent = readdir(d);
+    }
+  }
   // Close directory
-
+  closedir(d);
   return 0;
 }

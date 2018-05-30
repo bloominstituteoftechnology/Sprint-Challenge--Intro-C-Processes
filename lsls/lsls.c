f36e48e65c31326a5263b856ab151dff7ca07710
Edward Manda <lilkups@yahoo.com>
2018-05-30 01:25:03 -0500

    File size done!

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <sys/stat.h>
 
 /**
  * Main
@@ -36,8 +37,39 @@ int main(int argc, char **argv)
   // Repeatly read and print entries
   while ((ent = readdir(d)) != NULL)
   {
-    printf("%s\n", ent->d_name);
+    // This prints out the name of the file
+    // printf("%s\n", ent->d_name);
+
+    // we will print out the filesize
+    char fullpath[10000];
+    struct stat stat_buf;
+
+    // get the full path
+    snprintf(fullpath, sizeof(fullpath), "%s/%s", dirname, ent->d_name);
+
+    // stat the given file to find the number of bytes it contains
+    if (stat(fullpath, &stat_buf) == -1)
+    {
+      fprintf(stderr, "Failed to stat file %s\n", fullpath);
+      exit(3);
+    }
+
+    if (stat_buf.st_mode & S_IFREG)
+    {
+      // print the file name and its size in bytes
+      printf("%10lld %s\n", stat_buf.st_size, ent->d_name);
+    }
+    else if (stat_buf.st_mode & S_IFDIR)
+    {
+      // we have a directory; mark it
+      printf("%10s %s\n", "<DIR>", ent->d_name);
+    }
+    else
+    {
+      printf("%10s %s\n", "", ent->d_name);
+    }
   }
+
   // Close directory
   closedir(d);
   return 0;

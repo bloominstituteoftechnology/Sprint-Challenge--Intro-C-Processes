f9e2d8efb1772018557d03fe632895d4a39a4097
Cruise Brantley <cruise.brantley@gmail.com>
2018-06-22 13:17:51 -0500

    Correctly prints size as well as defines directories for all dirs

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,29 +1,37 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <sys/stat.h>
+#include <string.h>
 
 int main(int argc, char **argv)
 {
   struct dirent *dir;
   DIR *d;
-  // char path[50];
+  char path[50];
+  struct stat buf;
 
   printf("\n");
 
   for (int i = 1; i < argc; i++)
   {
     d = opendir(argv[i]);
-    // strcpy(path, argv[i]);
     if (d != NULL)
     {
       printf("Directory for \"%s\"\n", argv[i]);
       while ((dir = readdir(d)) != NULL)
       {
+        strcpy(path, argv[i]);
+        strcat(path, "/");
+        strcat(path, dir->d_name);
         if (dir->d_type == 4)
         {
-          printf("<DIR>  %s\n", dir->d_name);
+          printf("  <DIR>  %s\n", dir->d_name);
         }
         else
-          printf("       %s\n", dir->d_name);
+        {
+          stat(path, &buf);
+          printf("%7ld  %s\n", buf.st_size, dir->d_name);
+        }
       }
 
       closedir(d);

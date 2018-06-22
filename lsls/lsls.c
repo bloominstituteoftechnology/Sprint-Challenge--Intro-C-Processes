880cf49349caaaa593807a1fb82d286099c864e0
Cruise Brantley <cruise.brantley@gmail.com>
2018-06-22 14:37:33 -0500

    Added case for no arguments returning current directory

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -11,38 +11,61 @@ int main(int argc, char **argv)
   struct stat buf;
 
   printf("\n");
-
-  for (int i = 1; i < argc; i++)
+  if (argc == 1)
+    argv[0] = ".";
+  d = opendir(argv[0]);
+  if (d != NULL)
   {
-    d = opendir(argv[i]);
-    if (d != NULL)
+    printf("Directory for \"%s\"\n", argv[0]);
+    while ((dir = readdir(d)) != NULL)
     {
-      printf("Directory for \"%s\"\n", argv[i]);
-      while ((dir = readdir(d)) != NULL)
+      strcpy(path, argv[0]);
+      strcat(path, "/");
+      strcat(path, dir->d_name);
+      if (dir->d_type == 4)
       {
-        strcpy(path, argv[i]);
-        strcat(path, "/");
-        strcat(path, dir->d_name);
-        if (dir->d_type == 4)
-        {
-          printf("  <DIR>  %s\n", dir->d_name);
-        }
-        else
-        {
-          stat(path, &buf);
-          printf("%7ld  %s\n", buf.st_size, dir->d_name);
-        }
+        printf("  <DIR>  %s\n", dir->d_name);
       }
-
-      closedir(d);
-      printf("\n");
+      else
+      {
+        stat(path, &buf);
+        printf("%7ld  %s\n", buf.st_size, dir->d_name);
+      }
     }
-    else
-    {
-      perror("");
-      printf(" \"%s\"\n", argv[i]);
-      return 1;
-    }
+    printf("\n");
   }
+  else
+    for (int i = 1; i < argc; i++)
+    {
+      d = opendir(argv[i]);
+      if (d != NULL)
+      {
+        printf("Directory for \"%s\"\n", argv[i]);
+        while ((dir = readdir(d)) != NULL)
+        {
+          strcpy(path, argv[i]);
+          strcat(path, "/");
+          strcat(path, dir->d_name);
+          if (dir->d_type == 4)
+          {
+            printf("  <DIR>  %s\n", dir->d_name);
+          }
+          else
+          {
+            stat(path, &buf);
+            printf("%7ld  %s\n", buf.st_size, dir->d_name);
+          }
+        }
+
+        closedir(d);
+        printf("\n");
+      }
+      else
+      {
+        perror("");
+        printf(" \"%s\"\n", argv[i]);
+        return 1;
+      }
+    }
   return 0;
 }

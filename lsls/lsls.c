c50016361c08ccea5b0335583dbc1a9211e2d2b0
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2018-06-22 08:43:00 -0700

    added code for default no file specified

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -6,6 +6,15 @@
  */
 int main(int argc, char **argv)
 {
+  DIR *d;
+  struct dirent *dir;
+  d = opendir(".");
+  if (d) {
+    while ((dir = readdir(d)) != NULL) {
+      printf("%s\n", dir->d_name);
+    }
+    closedir(d);
+  }
   // Parse command line
 
   // Open directory

5c1cd4b212ac3db90d19c796ff8b8d8ff9ad39c2
Jacob Leon Lyerla <umasour@yahoo.com>
2018-06-22 09:39:32 -0700

    Lsls close but not working yet

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,18 +1,32 @@
 #include <stdio.h>
 #include <dirent.h>
 
+
 /**
  * Main
  */
 int main(int argc, char **argv)
 {
+  char *directoryPath;
   // Parse command line
-
+if(argc == 1)
+{
+  directoryPath='.';
+}
+else if(argc == 2);
+{
+directoryPath = argv[1];
+}
   // Open directory
-
+  struct dirent *entry;
+DIR *openedDirecectory =opendir(directoryPath);
   // Repeatly read and print entries
+  while((entry = readdir(openedDirecectory)) !=NULL)
+  {
+    printf("Entry: %s\n",entry->d_name);
+  }
 
   // Close directory
-
+closedir(openedDirecectory);
   return 0;
 }

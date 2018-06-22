f20bf9dc0cf3fa89ce5b4e709a5fd57bee5f128e
Justin M <jekm321@gmail.com>
2018-06-22 12:32:27 -0400

    lsls.exe prints directory content correctly, starts from my G:/js/git folder tho due to how i installed it xD

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -6,13 +6,30 @@
  */
 int main(int argc, char **argv)
 {
+  struct dirent *pDirent;
+  DIR *pDir;
+  
   // Parse command line
-
+  if (argc < 2)
+  {
+    printf("Insufficient data provided --> Usage: name.exe <directory name>\n");
+    return 1;
+  }
+  
   // Open directory
-
+  pDir = opendir(argv[1]);
+  if (pDir == NULL) {
+    printf("Can't open dir %s \n", argv[1]);
+    return 1;
+  }
+  
   // Repeatly read and print entries
-
+  while ((pDirent = readdir(pDir)) != NULL) {
+    printf("[%s]\n", pDirent->d_name);
+  }
+  
   // Close directory
+  closedir(pDir);
 
   return 0;
 }

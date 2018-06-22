3de92347c2f60e602a8d5a37b352b257d8b31689
Justin M <jekm321@gmail.com>
2018-06-22 15:34:14 -0400

    fixed up arg check and error message

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -8,26 +8,35 @@ int main(int argc, char **argv)
 {
   struct dirent *pDirent;
   DIR *pDir;
-  
+
   // Parse command line
-  if (argc < 2)
+  if (argc == 1)
   {
-    printf("Insufficient data provided --> Usage: name.exe <directory name>\n");
+    pDir = opendir(".");
+  }
+  else if (argc == 2)
+  {
+    pDir = opendir(argv[1]);
+  }
+  else
+  {
+    printf("Usage: name.exe <directory name>\n");
     return 1;
   }
-  
+
   // Open directory
-  pDir = opendir(argv[1]);
-  if (pDir == NULL) {
+  if (pDir == NULL)
+  {
     printf("Can't open dir %s \n", argv[1]);
     return 1;
   }
-  
+
   // Repeatly read and print entries
-  while ((pDirent = readdir(pDir)) != NULL) {
+  while ((pDirent = readdir(pDir)) != NULL)
+  {
     printf("[%s]\n", pDirent->d_name);
   }
-  
+
   // Close directory
   closedir(pDir);
 

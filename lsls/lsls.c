fb7c6f77a2820dd693e650b69bfa15039457eba5
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2018-06-22 09:10:43 -0700

    added code for listing other directories

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -6,25 +6,24 @@
  */
 int main(int argc, char **argv)
 {
-  DIR *d;
   struct dirent *dir;
-  d = opendir(".");
+  char *directName;
+  if (argc == 1) {
+    directName = ".";
+  } else if (argc == 2) {
+    directName = argv[1];
+  } else {
+    fprintf(stderr, "./lsls [directName]\n");
+  }
+
+  DIR *d = opendir(directName);
   if (d == NULL) {
-    printf("Could not open current directory");
+    printf("Could not open current directory\n");
+    return 0;
   }
-  if (d) {
-    while ((dir = readdir(d)) != NULL) {
-      printf("%s\n", dir->d_name);
-    }
-    closedir(d);
+  while ((dir = readdir(d)) != NULL) {
+    printf("%s\n", dir->d_name);
   }
-  // Parse command line
-
-  // Open directory
-
-  // Repeatly read and print entries
-
-  // Close directory
-
+  closedir(d);
   return 0;
 }

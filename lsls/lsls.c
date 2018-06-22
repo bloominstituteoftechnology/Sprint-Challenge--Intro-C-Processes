36e8132d1aec967433e94a4cc0d7712172ba1863
Nedu Robert <nedumrobert@gmail.com>
2018-06-22 16:07:39 +0100

    Implemented program to print out directory listing

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <dirent.h>
 
 /**
@@ -7,12 +8,36 @@
 int main(int argc, char **argv)
 {
   // Parse command line
+  DIR *dir;
+  struct dirent *sd;
+
+  if(argc > 2)
+  {
+    printf("Usage: %s <directory> \n", argv[0]);
+    return 0;
+  }
+
+  if (argc == 1)
+  {
+    dir = opendir(".");
+    argv[1] = ".";
+  }
 
   // Open directory
+  if((dir = opendir(argv[1])) == NULL)
+  {
+    printf("Error! Unable to open directory.\n");
+    exit(1);
+  }
 
-  // Repeatly read and print entries
+  // Repeatly read and print entries //readdir()
+  while((sd=readdir(dir)) != NULL)
+  {
+    printf("%s \n", sd->d_name);
+  }
 
   // Close directory
+  closedir(dir);
 
   return 0;
 }

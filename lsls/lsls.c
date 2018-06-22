77a5a289ee44ffb88242604ed8e6c56ca8eea001
SyCoDe <sycode@gmail.com>
2018-06-22 13:22:40 -0600

    task 2 completed

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,18 +1,36 @@
 #include <stdio.h>
 #include <dirent.h>
 
-/**
- * Main
- */
 int main(int argc, char **argv)
 {
-  // Parse command line
+  // Parse command line & open directory
+  struct dirent *dir_entry;
+  char *dirName;
 
-  // Open directory
+  if (argc == 1)
+  {
+    dirName = "."; // sets default if no arg provided
+  }
+  else if (argc == 2)
+  {
+    dirName = argv[1]; 
+  }
+  else
+  {
+    fprintf(stderr, "Please provide a valid directory name\n");
+  }
+
+  DIR *dir = opendir(dirName);
 
   // Repeatly read and print entries
 
+  while ((dir_entry = readdir(dir)) != NULL)
+  {
+    printf("%s\n", dir_entry->d_name);
+  }
+
   // Close directory
+  closedir(dir);
 
   return 0;
 }

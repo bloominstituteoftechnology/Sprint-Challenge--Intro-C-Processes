148291b60739ccdefdf915cb8a6d33bcb2a09d26
tanking <tanking@github.com>
2018-06-22 13:15:25 -0400

    Opens a directory and prints current directory

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -7,15 +7,25 @@
  */
 int main(int argc, char **argv)
 {
+  struct dirent *pDirent;
+  DIR *open;
+  char* currentDir[] = { "ls", ".", NULL };
+
   // Parse command line
   
   // Open directory
-  char* myargs[] = { "ls", ".", NULL };
-  execvp("ls", myargs);
+  open = opendir(currentDir[1]);
+  if (open == NULL) {
+    fprintf (stderr, "cannot print\n");
+    return 1;
+  }
 
   // Repeatly read and print entries
-
+  while ((pDirent = readdir(open)) != NULL) {
+    printf("%s\n", pDirent -> d_name);
+  }
+  
   // Close directory
-
+  closedir(open);
   return 0;
 }

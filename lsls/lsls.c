dd7deaedc20bc84330f173530ee689461317fa35
Sakkey <mstesney12@gmail.com>
2018-06-22 20:47:37 +0100

    Q&A assist

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,18 +1,34 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <stdlib.h>
 
 /**
  * Main
  */
 int main(int argc, char **argv)
 {
+  char *dirname;
   // Parse command line
-
+  if (argc == 1) {
+    dirname = ".";
+  } else if (argc == 2) {
+    dirname = argv[1];
+  } else {
+    fprintf(stderr, "usage: ./lsls [dirname]\n");
+    exit(1);
+  }
   // Open directory
-
+  DIR *d =  opendir(dirname);
+  if (d == NULL) {
+    fprintf(stderr, "./lsls: cannot open directory %s\n", dirname);
+    exit(2);
+  }
   // Repeatly read and print entries
-
+struct dirent *entry;
+  while ((entry = readdir(d)) != NULL) {
+    printf("%s\n", entry->d_name);
+  }
   // Close directory
-
+  closedir(d);
   return 0;
 }

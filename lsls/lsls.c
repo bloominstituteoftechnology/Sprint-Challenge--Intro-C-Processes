084c56cda31560c10e6a0aa7f2c7795ffc5dbc2d
kyle <kauzlarich.kyle@gmail.com>
2018-06-22 09:37:10 -0700

    Partially Working i have ls being called on the current folder and the program accepts an imput for the folder to call it on

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <string.h>
 
 /**
  * Main
@@ -7,11 +10,25 @@
 int main(int argc, char **argv)
 {
   // Parse command line
-
+  //char dir = *argv[argc - 1];
+  printf("%s", argv[argc - 1]);
+  printf("\n");
   // Open directory
 
   // Repeatly read and print entries
+  const char *ls = "/bin/ls";
+  int rc = fork();
 
+  if (rc == 0) {
+    execvp(ls, &argv[argc - 1]);
+    printf("you shouldnt be seeing this");
+    return -1;
+  } else if (rc != 0) {
+    waitpid(rc, NULL, 0);
+    printf("testing child should have called ls");
+    return 0;
+  }
+  return 0;
   // Close directory
 
   return 0;

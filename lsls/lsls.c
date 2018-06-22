4520cf23eb05721a113f49e78210d3b1db4d02a6
K <kkhaag@gmail.com>
2018-06-22 10:53:42 -0600

    Sprint seems complete..

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -6,22 +6,56 @@
 /**
  * Main
  */
+
+// argv[0] == ./lsls
+// argv[1] == null
+// argv[2] == TERM_PROGRAM=Apple_Terminal
+// argv[3] == SHELL=/bin/bash
+// argv[4] == TERM=xterm-256color
+
 int main(int argc, char **argv)
 {
-  printf("Is this working?\n");
+ char *dirname;
+  if(argc == 0)
+  {
+    dirname = ".";
+  }
+  else if(argc == 1)
+  {
+    dirname = argv[0];
+    printf("At argv[0] the dirname = %s\n", dirname);
+  }
+  else if(argc == 2)
+  {
+    dirname = argv[1];
+    printf("At argv[1] the dirname = %s\n", dirname);
+  }
+  else 
+  {
+    fprintf(stderr, "Error working with %s\n", dirname);
+  }
+
+    // VERY HELPFUL FOR TESTING
+    // int i;
+
+    // printf("There are %d command line argument(s):\n", argc);
+
+    // for (i = 0; i < argc; i++) {
+    //     printf("   %s\n", argv[i]);
+    // }
 
   DIR *dir;
-  dir = opendir(".");
+  dir = opendir(dirname);
   struct dirent *dp;
 
   if(dir == NULL)
   {
-    fprintf(stderr, "ERROR Failed to open DIR");
+    fprintf(stderr, "ERROR Failed to open dir %s\n", dirname);
     exit(1);
   }
 
 
-  while((dp=readdir(dir)) != NULL) 
+  while((dp = readdir(dir)) != NULL) 
   {
     printf(">> %s\n", dp->d_name);
   }

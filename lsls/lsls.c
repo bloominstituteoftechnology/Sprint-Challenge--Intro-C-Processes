b3e2d404adceba69e3214736bcd3f15aa55ae543
Henry K <mykim1891@hotmail.com>
2018-06-22 09:10:04 -0700

    FIrst commit; working on open directory

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,15 +1,20 @@
 #include <stdio.h>
 #include <dirent.h>
 
+// opendir()
+
 /**
  * Main
  */
 int main(int argc, char **argv)
 {
+  DIR *dir;
+  struct dirent *dp;
+
   // Parse command line
 
   // Open directory
-
+  
   // Repeatly read and print entries
 
   // Close directory

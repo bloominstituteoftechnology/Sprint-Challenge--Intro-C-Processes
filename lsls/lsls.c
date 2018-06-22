574eeeed1249fc05f4c22bbf1e6e58460f741605
unknown <bsa6.23.94@gmail.com>
2018-06-22 11:45:23 -0400

    lsls done

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -8,16 +8,26 @@ int main(int argc, char **argv)
 {
   // Parse command line
   char* path;
-  struct dirent *de;
-  DIR *dr;
-  
+  struct dirent *et;
 
-
+  if(argc > 1) {
+    path = argv[1];
+  }
+  else {
+    path = ".";
+  }
   // Open directory
-
+  DIR *dr = opendir(path);
   // Repeatly read and print entries
-
+  if(dr == NULL) {
+    printf("Directory is null");
+    exit(1);
+  }
+while((et = readdir(dr)) != NULL) {
+    printf("%s\n", et->d_name);
+  }
   // Close directory
+  closedir(dr);
 
   return 0;
 }

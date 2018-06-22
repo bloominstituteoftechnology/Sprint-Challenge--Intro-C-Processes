4f1b43ac0ccb028f50a578ad22ee76dc76f5cdd0
imsdhk <jappleleaf@gmail.com>
2018-06-22 12:46:19 -0400

    answered readme

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -22,17 +22,55 @@ int main(int argc, char **argv)
   */
   struct dirent *ent;
 
+  char **errorCode; 
+  /*
+   If the user does not specify a directory print out the contents of the current directory,
+    which is called `.`
+  */
   if(argc < 2) {
     filePath = opendir("."); 
-  } else {
+  } else { // provieded a directory as the first args 
     filePath = opendir(argv[1]); 
   }
   // Open directory
 
-  if(filePath == NULL) { // if no argv[1] --> invalid
+  if(filePath == NULL) { // if argv[1] --> invalid
      printf ("Cannot open directory --> '%s'\n", argv[1]);
      exit(1); 
   } 
+/*
+https://www.gnu.org/software/libc/manual/html_node/Opening-a-Directory.html
+
+http://man7.org/linux/man-pages/man3/opendir.3.html
+
+The opendir() and fdopendir() functions return a pointer to the
+directory stream.  On error, NULL is returned, and errno is set
+appropriately.
+
+
+
+EACCES
+Read permission is denied for the directory named by dirname.
+
+EMFILE
+The process has too many files open.
+
+ENFILE
+The entire system, or perhaps the file system which contains the directory, cannot support any additional open files at the moment. (This problem cannot happen on GNU/Hurd systems.)
+
+ENOMEM
+Not enough memory available.
+
+  else if(opendir(filePath) == NULL) {
+  fprintf("error code --> %s", errno); 
+    switch(errno){
+      case EACCES:
+      case EMFILE:
+      case ENFILE:
+      case ENOMEM:
+    }
+  }
+*/
 
   // Repeatly read and print entries
   while ((ent = readdir(filePath)) != NULL) { //Returns `NULL` if there are no more directory entires.

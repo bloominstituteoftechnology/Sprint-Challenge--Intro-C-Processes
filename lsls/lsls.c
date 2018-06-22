ff3c9ab960fe87dda8035d5ac594ca73c3e18b44
2940cristian <2940cristian@gmail.com>
2018-06-22 10:44:17 -0700

    lsls complete

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -7,12 +7,37 @@
 int main(int argc, char **argv)
 {
   // Parse command line
+  struct dirent *ent;
+
+    char *next;
+  if(argc == 1) {
+    next = ".";
+  }
+
+  else {
+    next = argv[1];
+  }
 
   // Open directory
 
+  DIR *d = opendir(next);
+
   // Repeatly read and print entries
 
+  if(d == NULL) {
+    printf("NULL");
+    return 0;
+  }
+
+  else {
+    while((ent = readdir(d)) != NULL) {
+      printf("%s\n", ent->d_name);
+    }
+  }
+
   // Close directory
+
+  closedir(d);
 
   return 0;
 }

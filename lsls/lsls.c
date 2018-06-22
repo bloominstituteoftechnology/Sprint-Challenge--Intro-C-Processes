23cb249e8f69b46c39a85e4f2a690b45718283c1
nickbrenn <nbren@tutanota.com>
2018-06-22 12:53:29 -0400

    Got the basic program working and listing files in a path

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -6,13 +6,35 @@
  */
 int main(int argc, char **argv)
 {
-  // Parse command line
+  int i;
+  struct dirent *dp;
+  char *path;
 
-  // Open directory
+  printf("There are %d command line argument(s):\n", argc);
 
-  // Repeatly read and print entries
+  for (i = 0; i < argc; i++) {
+      printf("   %s\n", argv[i]);
+  }
 
-  // Close directory
+  if(argv[1]){
+    path = argv[1];
+  } else path = ".";
 
+  DIR *d = opendir(path);
+
+  if(d == NULL){
+    fprintf(stderr, "Invalid path specified");
+    return 1;
+  }
+
+  // put two readdirs first if you want to get rid of the . and .. default paths displaying every time you run this
+  dp = readdir(d);
+  dp = readdir(d);
+
+  while((dp = readdir(d)) != NULL){
+    printf("Filename: %s\n", dp->d_name);
+  }
+  
+  closedir(d);
   return 0;
 }

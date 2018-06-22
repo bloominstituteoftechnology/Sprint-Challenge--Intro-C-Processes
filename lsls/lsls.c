5966e178d8e32f26afa4879646a6ad795b0ab715
Michal Chmielniak <michal@Michals-MacBook-Pro.local>
2018-06-22 11:29:20 -0400

    added in user directory for user speciics and then attempted to implement stretch goal print file size in bytes

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,27 +1,52 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 #include <dirent.h>
+#include <sys/stat.h>
 
 /**
  * Main
  */
 int main(int argc, char **argv)
 {
+  char *str;
+  char* cmdpath;
+  DIR *d;
+  struct dirent *ent;
+  struct stat buf;
+
   // Parse command line
-  if (argc == 0) {
-     char cl[] = ".";
- }
+  if (argc == 1) {
+    char home[2] = ".";
+    str = home;
+  }
+  else if (argc == 2) {
+    str = argv[1];
+  }
+  else {
+    fprintf(stderr, "Nope Unsuccseful\n");
+    exit(1);
+  }
 
   // Open directory
-  DIR *d = opendir(".");
+  d = opendir(str);
   if (d == NULL) {
-    fprintf(stderr, "Directory\n");
+    fprintf(stderr, "Error Opendir Unsuccesful\n");
     exit(1);
   }
   else {
   // Repeatly read and print entries
-    struct dirent *ent = readdir(d);
+    ent = readdir(d);
     while (ent != NULL) {
+      size_t len = strlen(str);
+      size_t len2 = strlen(ent->d_name);
+      char *str2 = malloc(len + 1 + len2 + 1 );
+      strcpy(str2, str);
+      str2[len] = '/';
+      strcat(str2, ent->d_name);
+      stat(str2, &buf);
+      memset(str2, 0, sizeof(int));
+      printf("%lld     ", buf.st_size);
       printf("%s\n", ent->d_name);
       ent = readdir(d);
     }

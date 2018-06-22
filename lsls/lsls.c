5c80f0e767754ad2a6aac56b7bafd19dcf295cae
Cruise Brantley <cruise.brantley@gmail.com>
2018-06-22 12:43:00 -0500

    Prints <DIR> before folders

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -5,17 +5,24 @@ int main(int argc, char **argv)
 {
   struct dirent *dir;
   DIR *d;
+  // char path[50];
+
+  printf("\n");
 
   for (int i = 1; i < argc; i++)
   {
     d = opendir(argv[i]);
+    // strcpy(path, argv[i]);
     if (d != NULL)
     {
-      dir = readdir(d);
-
       while ((dir = readdir(d)) != NULL)
       {
-        printf("%s\n", dir->d_name);
+        if (dir->d_type == 4)
+        {
+          printf("<DIR>  %s\n", dir->d_name);
+        }
+        else
+          printf("       %s\n", dir->d_name);
       }
 
       closedir(d);

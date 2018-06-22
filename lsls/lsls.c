f5a34e033c0a49ad503b489d749ce5d0a16a08d1
Mark Wong <fmarkwong@gmail.com>
2018-06-22 10:44:54 -0700

    lsls

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -6,13 +6,20 @@
  */
 int main(int argc, char **argv)
 {
-  // Parse command line
+    char* dirName = argv[1] == NULL ? "." : argv[1];
+    struct dirent* ent;
+    DIR* dirPointer;
 
-  // Open directory
+    if (!(dirPointer = opendir(dirName))) {
+        fprintf(stderr, "Error: Could not open directory %s\n", dirName);
+        return -1;
+    }
 
-  // Repeatly read and print entries
+    while(ent = readdir(dirPointer)) {
+        printf("%s\n", ent->d_name);
+    }
 
-  // Close directory
+    closedir(dirPointer);
 
   return 0;
 }

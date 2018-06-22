48080840be15abf8ddbd36aa71f769e3a3cecac5
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2018-06-22 08:50:41 -0700

    added null command

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -9,6 +9,9 @@ int main(int argc, char **argv)
   DIR *d;
   struct dirent *dir;
   d = opendir(".");
+  if (d == NULL) {
+    printf("Could not open current directory");
+  }
   if (d) {
     while ((dir = readdir(d)) != NULL) {
       printf("%s\n", dir->d_name);

6ed283c02d6cd35c8ecef35ea09977bd11f8256d
Ashiko124 <greeneselectronics@gmail.com>
2018-06-22 16:36:11 -0600

    Finished the sprint with the help of alex

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -8,28 +8,30 @@
 int main(int argc, char **argv)
 {
   // Parse command line
-  DIR *dir;
+  char *dir; //
 
   if(argc == 1) { // argc is the number of arguments being passed 
                   // argv is the arguments being passed.
-    
+    dir = ".";
   } else if ( argc == 2) {
-
+    dir = argv[1];
   } else {
-
+    printf("error to many directories [dir] \n  ");
+    return 1;
   }
 
-  dir = opendir(".");
-  if(dir == NULL) {
+  DIR *d =opendir(dir);
+
+  if(d == NULL) {
     printf("stdError cannot open directory \n");
     exit(1);
   }
   struct dirent *ent;
-  while((ent=readdir(dir)) != NULL) {
+  while((ent=readdir(d)) != NULL) {
       printf("%s\n", ent->d_name);
   }
 
-  closedir(dir);
+  closedir(d);
 
   return 0;
 }

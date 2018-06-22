0c97dcdc5719c149513f0036e02723f446dec793
Devon Bacon <devon.bacon@gmail.com>
2018-06-22 10:08:11 -0700

    lsls.c - set up directory struct

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,18 +1,15 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <unistd.h>
+#include <stdlib.h>
 
 /**
  * Main
  */
 int main(int argc, char **argv)
 {
-  // Parse command line
-
-  // Open directory
-
-  // Repeatly read and print entries
-
-  // Close directory
+  DIR *dir;
+  struct dirent *dp;
 
   return 0;
 }

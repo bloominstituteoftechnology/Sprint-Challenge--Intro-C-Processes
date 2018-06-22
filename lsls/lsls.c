db00fdc77aa433b3aa210e73261482d90a2a1a6a
Anthony <anthony@anthonys-mbp.lan>
2018-06-22 11:51:40 -0400

    Sprint Challenge
    In lsls.c:
    Added framework for opendir, readdir, closedir.

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <sys/types.h>
 
 /**
  * Main
@@ -9,10 +10,20 @@ int main(int argc, char **argv)
   // Parse command line
 
   // Open directory
+  DIR *dir;
+  struct dirent *dp;
 
+  if ((dir = opendir ('.')) == NULL) {
+      perror ('Cannot open .');
+      exit(1);
+  }
+  while ((dp = readdir (dir)) != NULL) {
+
+  }
   // Repeatly read and print entries
 
   // Close directory
+  closedir();
 
   return 0;
 }

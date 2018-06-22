9988ba6379529c7bbaf44f04fcace32508964a9f
Anthony <anthony@anthonys-mbp.lan>
2018-06-22 13:40:52 -0400

    In lsls.c:
    Added functionality to open, read, and close directory.

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -7,23 +7,31 @@
  */
 int main(int argc, char **argv)
 {
-  // Parse command line
+  DIR *DirA;
+  struct dirent *DirB;
 
+  // Parse Command Line
+  if (argc == 1) {
+    DirA = opendir('.');
+  } else if (argc == 2) {
+    DirA = opendir(argv[1]);
+  } else {
+    printf('Error');
+  }
+  
   // Open directory
-  DIR *opendir(char *path);
-  struct dirent *dp;
-
-  if ((path = opendir ('.')) == NULL) {
-      perror ('Cannot open directory');
-      exit(1);
+  if (DirA == NULL) {
+    printf('Error opening Directory\n');
+    exit(1);
   }
-  while ((dp = readdir (dir)) != NULL) {
 
-  }
   // Repeatly read and print entries
-
+  while ((DirB = readdirectory(DirA)) != NULL) {
+      printf('%s\n', DirB->d_name);
+  }
+  
   // Close directory
-  closedir();
+  closedir(DirA);
 
   return 0;
 }

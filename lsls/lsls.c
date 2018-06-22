89fe423569bf225172f0dbbb6d4cbc50939d452c
cgoez <goez2001@gmail.com>
2018-06-22 09:05:41 -0700

    Initial commit, parse cmd, test dir

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -6,9 +6,23 @@
  */
 int main(int argc, char **argv)
 {
+  DIR *dir;
+  struct dirent *pDirent;
   // Parse command line
-
+  if (argc < 2)
+  {
+    dir = opendir(".");
+    argv[1] = ".";
+  }
+  else
+  {
+    dir = opendir(argv[1]);
+  }
   // Open directory
+  if (dir == NULL) {
+    printf("The directory is empty! :(\n");
+    exit(1);
+  }
 
   // Repeatly read and print entries
 

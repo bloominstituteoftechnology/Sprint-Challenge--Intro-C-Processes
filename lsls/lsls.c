2a676c357bd5f4cecc9dd0f0fe684917b042ec3c
odax <nr10@albion.edu>
2018-06-22 15:48:41 -0400

    implemented directory path functionality

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -7,18 +7,32 @@
 int main(int argc, char **argv)
 {
   // Parse command line
-    struct dirent *de;
+  char *dirname;
+  //lets check the number of arguments passed into the ls command
+  
+  if (argc == 1) {
+  //all that's inside is lsls
+  //no args were passed into the program  
+  dirname = ".";
+  } else if (argc == 2) {
+    dirname = argv[1];
+  } else {
+    fprintf(stderr, "usage: .lsls [dirname]\n");
+    exit(1);
+  }
 
   // Open directory
-    DIR *dr = opendir(".");
+    DIR *dr = opendir(dirname);
     if (dr == NULL) {
-      printf("Could not open current directory");
-      return 0;
+      printf("./lsls: Could not open current directory %s\n", dirname);
+      exit(2);
     }
 
   // Repeatly read and print entries
-    while ((de = readdir(dr)) != NULL) {
-      printf("%s\n", de -> d_name);
+    struct dirent *entry;
+
+    while ((entry = readdir(dr)) != NULL) {
+      printf("%s\n", entry -> d_name);
     }
     
   // Close directory

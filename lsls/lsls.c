2975e8c7aa47dc52643b4f14ffabe8c24f5e40b4
cgoez <goez2001@gmail.com>
2018-06-22 09:58:42 -0700

    Comments

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -11,25 +11,25 @@ int main(int argc, char **argv)
   struct dirent *pDirent;
 
   // Parse command line
-  if (argc < 2)
+  if (argc < 2) // Default current dir
   {
     dir = opendir(".");
     argv[1] = ".";
   }
   else
   {
-    dir = opendir(argv[1]);
+    dir = opendir(argv[1]); // dir path
   }
   // Open directory
   if (dir == NULL)
   {
-    printf("The directory is empty! :(\n");
+    printf("The directory is empty! :(\n"); // test ./lsls x
     exit(1);
   }
   // Repeatly read and print entries
   else
   {
-    while ((pDirent = readdir(dir)) != NULL)
+    while ((pDirent = readdir(dir)) != NULL) // read while dir not empty
     {
       printf("%s\n", (*pDirent).d_name);
     }

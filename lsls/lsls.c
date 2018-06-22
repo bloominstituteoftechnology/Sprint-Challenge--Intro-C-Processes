e262909fcb57bd67976deab2a1a05ece3d678351
Shawn Stewart <s.stewart0220@gmail.com>
2018-06-22 12:57:51 -0400

    Completed Task 2

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -6,13 +6,32 @@
  */
 int main(int argc, char **argv)
 {
+  struct dirent *pDirent;
+  DIR *pDir;
+  char *path[1];
+
   // Parse command line
+  if (argc < 2) {
+    *path = ".";
+  } else {
+    *path = argv[1];
+  }
 
   // Open directory
+  pDir = opendir(*path);
+
+  if (pDir == NULL) {
+    printf("Error: failed to open directory %s\n", *path);
+    return 1;
+  }
 
   // Repeatly read and print entries
+  while ((pDirent = readdir(pDir)) != NULL) {
+    printf("[%s]\n", pDirent->d_name);
+  }
 
   // Close directory
+  closedir(pDir);
 
   return 0;
 }

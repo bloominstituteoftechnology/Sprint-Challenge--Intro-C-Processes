506c3590ff3f5911983d2c05be50da81dff8ff65
sadrak105 <sadrak10@gmail.com>
2018-06-22 12:51:53 -0400

    opendir and readdir added

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -11,10 +11,21 @@ int main(int argc, char **argv)
   // Parse command line
 
   // Open directory
+  DIR *dir;
+  struct dirent *sd;
+  dir = opendir(".");
+
+  if (dir == NULL) {
+    printf("Error, cannot open directory!\n");
+    exit(1);
+  }
 
   // Repeatly read and print entries
+  while((sd = readdir(dir)) != NULL) {
+    printf(">> %s\n", sd->d_name);
+  }
 
   // Close directory
-
+  closedir(dir);
   return 0;
 }

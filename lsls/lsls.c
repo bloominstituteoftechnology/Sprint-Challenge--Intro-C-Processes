ba062e27e7257b7df479c1f264bc5710d3faa7c9
odax <nr10@albion.edu>
2018-06-22 14:45:42 -0400

    attempted lsls.c, could only create so that it prints what is in the current directory

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -7,12 +7,21 @@
 int main(int argc, char **argv)
 {
   // Parse command line
+    struct dirent *de;
 
   // Open directory
+    DIR *dr = opendir(".");
+    if (dr == NULL) {
+      printf("Could not open current directory");
+      return 0;
+    }
 
   // Repeatly read and print entries
-
+    while ((de = readdir(dr)) != NULL) {
+      printf("%s\n", de -> d_name);
+    }
+    
   // Close directory
-
+  closedir(dr);
   return 0;
 }

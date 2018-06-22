b58e567b29672ecec8accc66b718f92525d5ab5d
Jacob Leon Lyerla <umasour@yahoo.com>
2018-06-22 10:27:35 -0700

    Finished
    Fixed the error with the simi-colon in the else if, also added in comments to my code, hopefully making it more readable.

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -9,26 +9,44 @@ int main(int argc, char **argv)
 {
   char *directoryPath;
   // Parse command line
+/*
+   If there there is no directory given than,
+   this code reverts to a default of  ".".
+*/
 if(argc == 1)
 {
   directoryPath=".";
 }
-else if(argc == 2);
+/*
+  else if there is a directory given take argive at the index of 1 and set the directory path to that.
+*/
+else if(argc == 2)
 {
 directoryPath = argv[1];
 }
   // Open directory
+  // set variable pointer for directory entries
   struct dirent *entry;
+  /*
+    Open the directory passing in the path that we parsed above, set that to a directory stream pointer, i named mine openDirectory
+  */
 DIR *openedDirecectory =opendir(directoryPath);
-  // Repeatly read and print entries
+/*
+  if the directory opened rturned Null, print an error
+  and exit.
+*/
   if (openedDirecectory ==NULL)
   {
     fprintf(stderr,"cannot open directory");
     exit(1);
   }
+    // Repeatly read and print entries
+    /*
+      while there are still directorys to read and we are not getting null, then print out the directory name of the entry
+    */
   while((entry = readdir(openedDirecectory)) !=NULL)
   {
-    printf("Entry: %s\n", entry->d_name);
+    printf("Directory: %s\n", entry->d_name);
   }
 
   // Close directory

7ca98af0612a87a68697dc66ad53b9bd9226cfee
Anthony <anthony@anthonys-mbp.lan>
2018-06-22 12:04:49 -0400

    Short Answer Questions
    In Readme:
    Answered Q1 and Q2.

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -10,11 +10,11 @@ int main(int argc, char **argv)
   // Parse command line
 
   // Open directory
-  DIR *dir;
+  DIR *opendir(char *path);
   struct dirent *dp;
 
-  if ((dir = opendir ('.')) == NULL) {
-      perror ('Cannot open .');
+  if ((path = opendir ('.')) == NULL) {
+      perror ('Cannot open directory');
       exit(1);
   }
   while ((dp = readdir (dir)) != NULL) {

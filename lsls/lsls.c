cfa7553b1f88a3492b3af2d75e47b3b3e917ce4c
jeffrey <jeffreyneilflynn@gmail.com>
2018-06-22 11:14:18 -0400

    outlined basic knowledge to complete the task at hand

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,9 +1,27 @@
 #include <stdio.h>
 #include <dirent.h>
 
-/**
- * Main
- */
+/*
+
+  1. accept some user input that specifies a directory 
+  2. `ls` / print the files/directories listed in the specified directory
+  3. if the user does not specify a directory, `ls` their current directory `.`
+
+  opendir() - opens a DIR stream corresponding to the DIR name
+    * returns a pointer to the DIR stream
+    * returns NULL if there is an error
+  
+  readdir() - read a directory 
+    * returns a pointer to a `dirent` structure representing the next DIR entry in the provided DIR stream pointed to by `dirp`
+    * retuns NULL upon reaching the end of the DIR stream or if an error was encountered 
+  
+  closedir() - close a directory
+    * closes the DIR stream associated with `dirp`
+    * also closes the underlying file descriptor associated with `dirp`
+    * returns 0 on success
+    * returns -1 on failure
+
+*/
 int main(int argc, char **argv)
 {
   // Parse command line

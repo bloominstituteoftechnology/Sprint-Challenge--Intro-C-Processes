4780694f355c42fbea432c35fa1317bf23f24279
Finlie <dfinley2917@gmail.com>
2018-06-22 11:55:43 -0400

    Tasks 1 & 2 completed

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/types.h>
 
 /**
  * Main
@@ -7,10 +10,32 @@
 int main(int argc, char **argv)
 {
   // Parse command line
+  char *name;
+  if (argc > 1)
+  {
+    name = strdup(argv[1]);
+  }
+  else
+  {
+    name = ".";
+  }
 
   // Open directory
+  DIR *dir;
+  struct dirent *sd;
+  dir = opendir(name);
+
+  if (dir == NULL)
+  {
+    printf("Unable to open the directory.");
+    exit(1);
+  }
 
   // Repeatly read and print entries
+  while ((sd = readdir(dir)) != NULL)
+  {
+    printf("%s\n", sd->d_name);
+  }
 
   // Close directory
 

2cee1202d867c8a8cb545da503661c307a4b1b8f
Kaitlyn Flynn <kaitlynflynn@gmail.com>
2018-06-22 12:38:26 -0700

    Added notes and Seans solve code to bottom of mine for reference

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -36,3 +36,43 @@ int main(int argc, char **argv)
 
   return 0;
 }
+
+//-----------------------------------------------------
+
+//instructor solve
+
+// int main(int argc, char **argv)
+// {
+//   char *dirname;
+//   // NOTE: Parse command line
+//   // NOTE: no args were passed to the program
+//   if (argc == 1) {
+//     dirname - "."; //NOTE: . = current directory
+//     } else if (argc == 2) {
+//       dirname = argv[1];
+//     } else {
+//       fprintf(stderr, "usage: ./lsls [dirname]\n");
+//       exit(1); //NOTE: can also return(1);
+//     }
+
+//   // NOTE: Open directory
+//   DIR *d = opendir(dirname);
+
+//   // NOTE: Error check on opening the directory
+//   if (d == NULL) {
+//     fprintf(stderr, "./lsls: cannot open directory %s\n", dirname);
+//     exit(2);
+//   }
+
+//   // NOTE: Repeatly read and print entries
+//   struct dirent *entry;
+  
+//   while ((entry = readdir(d)) != NULL) {
+//     printf("%s\n", entry->d_name); //NOTE: %s = print string and \n = new line
+//   }
+
+//   // NOTE: Close directory
+//   closedir(d);
+
+//   return 0;
+// }

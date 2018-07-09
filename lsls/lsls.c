3fa049213706d33348aabb0ba1e056e7dafb1f43
sophiemullerc <32464200+sophiemullerc@users.noreply.github.com>
2018-07-09 12:03:04 -0500

    Corrected Sprint Sophie Muller

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,18 +1,42 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <errno.h>
+
+#define OUTPUT_BUFSIZE 1024
 
 /**
  * Main
  */
 int main(int argc, char **argv)
 {
-  // Parse command line
+	// Parse command line
+	if(argc != 2) {
+		printf("Usage: %s <directory>\n", argv[0]);
+		return 0;
+	}
 
-  // Open directory
+	DIR *dp;
+	int status;
+	struct dirent *direntp;
 
-  // Repeatly read and print entries
+	// Open directory
+	dp = opendir(argv[1]);
+	if (dp == NULL) {
+		perror("opendir");
+		return 1;
+	}
 
-  // Close directory
+	// Repeatly read and print entries
+	while ((direntp = readdir(dp)) != NULL) {
+		printf ("%s\n", direntp->d_name);
+	}
+
+	// Close directory
+	status = closedir(dp);
+	if (status == -1) {
+		perror("closedir");
+		return 1;
+	}
 
   return 0;
 }

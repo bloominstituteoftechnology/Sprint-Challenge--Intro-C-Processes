f1028268bcf1f56c7d21fd00d8096a7ec2e46840
astrebeck <alexstrebeck@gmail.com>
2018-08-10 10:34:13 -0600

    sprint challenge submittion

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -10,31 +10,19 @@ int main(int argc, char **argv)
   DIR *dir;
   char *str[80];
   struct dirent *ent;
-  // Parse command line
   
   // Open directory
   dir = opendir(argv[1]);
-
   ent = readdir(dir);
-
   if (!ent) {
     printf("Nothing in this directory ¯\(ツ)/¯");
   }
   else {
     while((ent = readdir(dir)) != NULL){
 
-      printf("%s\n",ent->d_name);
-      
+      printf("%s\n",ent->d_name);   
     }
   }
-
   closedir(dir);
-
-
-
-  // Repeatly read and print entries
-
-  // Close directory
-
   return 0;
 }

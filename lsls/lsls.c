6c23b7e115447997d4bc7b10effe26a4263caed7
Brandon Hopper <brandonhopper0@gmail.com>
2018-08-10 17:16:35 -0400

    edit lsls.c file

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <dirent.h>
-#include <stdlib.h>
 #include <sys/stat.h>
 
 /**
@@ -10,12 +9,28 @@ int main(int argc, char **argv)
 {
   DIR *d;
   struct dirent *dir;
-  d = opendir(".");
-  if (d) {
-    while ((dir = readdir(d)) != NULL) {
-      printf("%s\n", dir->d_name);
-    }
-    closedir(d);
+  struct stat buf;
+
+  d = (argc > 1) ? opendir(argv[1]) : opendir(".");
+
+  if(d == NULL)
+  {
+    fprintf(stderr, "Error opening directory");
+    return 0;
   }
+
+    while ((dir = readdir(d)) != NULL) { 
+    {
+      stat(dir->d_name, & buf);
+      if(buf.st_mode & __S_IFDIR)
+      {
+        printf("%10s --- %s\n", "<DIR>", dir->d_name);
+      }
+      else
+      {
+        printf("%10llu --- %s\n", buf.st_size, dir->d_name);
+      }
+    }
+  closedir(d);
   return(0);
 }

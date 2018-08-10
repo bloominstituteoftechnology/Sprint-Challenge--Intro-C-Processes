429eb2f60246e737ea1aae206a8f783ff23c8170
eddygonzalez9708 <edward.gonzalez9708@gmail.com>
2018-08-10 15:26:46 -0400

    Recoded lsls and put old code in lsls2 file.

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,144 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <sys/wait.h> 
-
-#define PROMPT "lambda-shell$ "
-
-#define MAX_TOKENS 100
-#define COMMANDLINE_BUFSIZE 1024
-#define DEBUG 1  // Set to 1 to turn on some debugging output, or 0 to turn off
-
-/**
- * Parse the command line.
- *
- * YOU DON'T NEED TO MODIFY THIS!
- * (But you should study it to see how it works)
- *
- * Takes a string like "ls -la .." and breaks it down into an array of pointers
- * to strings like this:
- *
- *   args[0] ---> "ls"
- *   args[1] ---> "-la"
- *   args[2] ---> ".."
- *   args[3] ---> NULL (NULL is a pointer to address 0)
- *
- * @param str {char *} Pointer to the complete command line string.
- * @param args {char **} Pointer to an array of strings. This will hold the result.
- * @param args_count {int *} Pointer to an int that will hold the final args count.
- *
- * @returns A copy of args for convenience.
- */
-
- // Parse command line
-
-  // Open directory
-
-  // Repeatly read and print entries
-
-  // Close directory
-  
-char **parse_commandline(char *str, char **args, int *args_count)
-{
-    char *token;
-    
-    *args_count = 0;
-
-    token = strtok(str, " \t\n\r");
-
-    while (token != NULL && *args_count < MAX_TOKENS - 1) {
-        args[(*args_count)++] = token;
-
-        token = strtok(NULL, " \t\n\r");
-    }
-
-    args[*args_count] = NULL;
-
-    return args;
-}
+#include <dirent.h>
 
 /**
  * Main
  */
-int main(void)
+
+int main(int argc, char **argv)
 {
-    // Holds the command line the user types in
-    char commandline[COMMANDLINE_BUFSIZE];
-
-    // Holds the parsed version of the command line
-    char *args[MAX_TOKENS];
-
-    // How many command line args the user typed
-    int args_count;
-
-    // Shell loops forever (until we tell it to exit)
-    while (1) {
-        // Print a prompt
-        printf("%s", PROMPT);
-        fflush(stdout); // Force the line above to print
-
-        // Read input from keyboard
-        fgets(commandline, sizeof commandline, stdin);
-
-        // Exit the shell on End-Of-File (CRTL-D)
-        if (feof(stdin)) {
-            break;
-        }
-
-        // Parse input into individual arguments
-        parse_commandline(commandline, args, &args_count);
-
-        if (args_count == 0) {
-            // If the user entered no commands, do nothing
-            continue;
-        }
-
-        // Exit the shell if args[0] is the built-in "exit" command
-        if (strcmp(args[0], "exit") == 0) {
-            break;
-        }
-
-        #if DEBUG
-
-        // Some debugging output
-
-        // Print out the parsed command line in args[]
-        for (int i = 0; args[i] != NULL; i++) {
-            printf("%d: '%s'\n", i, args[i]);
-        }
-
-        #endif
-        
-        /* Add your code for implementing the shell's logic here */
-        int rc = fork(); 
-        if (rc < 0) {
-            fprintf(stderr, "Fork Failed"); 
-            exit(1); 
-        }
-        else if (rc == 0) {
-            int result = strcmp(args[0], "cd"); 
-            if (result == 0) {
-                if (args[1] && !args[2]) {
-                    int check_path = chdir(args[1]);
-                    if (check_path < 0) {
-                        printf("bash: cd %s: No such file or directory\n", args[1]); 
-                    }
-                }
-                else if (args[2]) {
-                    printf("bash: cd: too many arguments\n");  
-                }
-            }
-            else {
-                execvp(args[0], args);
-                printf("bash: %s: command not found.\n", args[0]);
-            } 
-        }
-        else {
-            waitpid(rc, NULL, 0); 
-            continue; 
-        } 
+    char *dirname;
+    // Parse command line
+    if (argc == 1)
+    {
+        // set the directory that we're going to be looking at to "."
+        dirname = ".";
     }
+    else if (argc == 2)
+    {
+        dirname = argv[1];
+    }
+    else
+    {
+        fprintf(stderr, "usage: lsls [dirname]\n");
+        exit(1);
+    }
+
+    // Open directory
+    DIR *d = opendir(dirname);
+
+    if (d == NULL)
+    {
+        fprintf(stderr, "lsls: cannot open direcotry %s\n", dirname);
+        exit(1);
+    }
+
+    // Repeatly read and print entries
+    struct dirent *entry;
+
+    while ((entry = readdir(d)) != NULL)
+    {
+        printf("%s\n", entry->d_name);
+    }
+
+    // Close directory
+    closedir(d);
 
     return 0;
 }

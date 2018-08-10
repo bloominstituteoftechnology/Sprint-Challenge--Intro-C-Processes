c3a9c8d7cd702c924f4fd80dd340e1c495868b90
AmyShackles <amyshackles@gmail.com>
2018-08-10 09:11:04 -0700

    Finished MVP
    - Program allows user to enter a directory name as an argument in the command line
    - Program then attempts to open that directory and print the contents
    - If the program cannot open the directory, it prints a statement to that effect and exits
    - If no directory is given on the command line, the program uses the current directory

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,5 +1,31 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <stdlib.h>
+
+
+void ls_directory(const char *directory_name)
+{
+  DIR *directory;
+  struct dirent *entry;
+
+  // Open directory
+  directory = opendir(directory_name);
+  if (directory != NULL)
+  {
+      // Repeatly read and print entries
+    while ((entry = readdir(directory)) != NULL)
+    {
+      printf("%s\n", entry->d_name);
+    }
+  // Close directory
+    closedir(directory);
+  }
+  else
+  {
+    printf("Cannot open directory %s\n", directory_name);
+    exit(1);
+  }
+}
 
 /**
  * Main
@@ -7,12 +33,19 @@
 int main(int argc, char **argv)
 {
   // Parse command line
+  int i;
 
-  // Open directory
+  i = 1;
+  while (i < argc)
+  {
+    ls_directory(argv[i]);
+    i++;
+  }
 
-  // Repeatly read and print entries
-
-  // Close directory
+  if (argc == 1)
+  {
+    ls_directory(".");
+  }
 
   return 0;
 }

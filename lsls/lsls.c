6eccfdf5edc2e2bf580834be244bd5899189eaae
astrebeck <alexstrebeck@gmail.com>
2018-08-10 09:58:38 -0600

    .

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -9,14 +9,15 @@ int main(int argc, char *argv)
   //variables
   DIR *dir;
   char *str[80];
+  struct dirent *ent;
   // Parse command line
   
   // Open directory
   dir = opendir(argv);
 
-  ent = readdir(dir)
+  ent = readdir(dir);
 
-  printf("%s\n",ent->d_name)
+  printf("%s\n",ent->d_name);
 
 
 

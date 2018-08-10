76882bcad03d815de82067ee0ef6236bdf4917ac
Bradiowave <divebombwalker@gmail.com>
2018-08-10 09:04:03 -0700

    finished lsls.c
    MVP complete!

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -9,10 +9,19 @@ int main(int argc, char **argv)
 {
   struct dirent *newdir;
   DIR *d;
+  char *path;
   // Parse command line
+  if (argc > 2) {
+    fprintf(stderr, "Usage: ./lsls <directory name>(optional)");
+    exit(1);
+  } else if (argc == 2) {
+    path = argv[1];
+  } else {
+    path = ".";
+  }
 
   // Open directory
-  d = opendir(".");
+  d = opendir(path);
   if (d == NULL) {
     fprintf(stderr, "Error Opening Directory");
     exit(1);

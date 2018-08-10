e074a1615a121b271180d46652de106428729386
Lorin Fields <lorin.fields@gmail.com>
2018-08-10 09:37:20 -0700

    Task 2 complete

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -6,13 +6,21 @@
  */
 int main(int argc, char **argv)
 {
-  // Parse command line
-
-  // Open directory
-
+// Parse command line
+int argLength = argc;
+char* boss = argv[1];
+char* period = ".";
+char* b2 = (argLength == 2) ? boss : period;
+// Open directory
+DIR *dir;
+struct dirent *ent;
+if ((dir = opendir (b2)) != NULL) {
   // Repeatly read and print entries
-
+  while ((ent = readdir (dir)) != NULL) {
+  printf ("%s\n", ent->d_name);
+  }
   // Close directory
-
+  closedir (dir);
+ 
   return 0;
-}
+}}

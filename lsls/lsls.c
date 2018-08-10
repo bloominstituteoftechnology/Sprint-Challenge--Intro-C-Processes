e5acec1e2989cabeb4d562cf9c6c72cfed7d998f
Bee C <b.choeung@gmail.com>
2018-08-10 13:04:20 -0400

    working on lsls.c

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <dirent.h>
+#include <dirent.h> // let's us open a directory
 
 /**
  * Main
@@ -7,12 +7,20 @@
 int main(int argc, char **argv)
 {
   // Parse command line
-
+  
   // Open directory
 
   // Repeatly read and print entries
 
   // Close directory
+
+  // print out dictory listing user specifies on command line.
+  // IF no directory specified then print contents of current directory
+
+  DIR *directory = opendir(".");
+  char *result = directory == NULL ? "Did not open" : "Succeded!"; // ternary operator
+  printf("Testing opening a directory:  %s\n", result);
+  closedir(directory);
 
   return 0;
 }

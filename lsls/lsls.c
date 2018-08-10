b7a5fc10e1a4579a845610ea333ea65e17291a59
Bradiowave <divebombwalker@gmail.com>
2018-08-10 08:54:26 -0700

    lsls.c prints contents of current directory

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <dirent.h>
 
 /**
@@ -6,13 +7,24 @@
  */
 int main(int argc, char **argv)
 {
+  struct dirent *newdir;
+  DIR *d;
   // Parse command line
 
   // Open directory
+  d = opendir(".");
+  if (d == NULL) {
+    fprintf(stderr, "Error Opening Directory");
+    exit(1);
+  }
 
   // Repeatly read and print entries
-
+  while ( (newdir = readdir(d)) != NULL ) {
+    printf("%s\n", newdir->d_name);
+  }
+  
   // Close directory
+  closedir(d);
 
   return 0;
 }

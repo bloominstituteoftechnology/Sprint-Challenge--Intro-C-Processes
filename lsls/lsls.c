d6ff2ed9248aefd7c7ecb3dffeb703a0e555d3c6
Bee C <b.choeung@gmail.com>
2018-08-10 15:24:45 -0400

    completed lsls.c as per Sean's solution

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <dirent.h> // let's us open a directory
 
 /**
@@ -6,6 +7,36 @@
  */
 int main(int argc, char **argv)
 {
+  char *dirname;
+  if (argc == 1) {
+    // set the directory that we're going to be looking at to "."
+  dirname = ".";
+  } else if (argc == 2) {
+    dirname = argv[1];
+  } else {
+    fprintf(stderr, "usage: lsls [dirname]\n");
+    exit(1);
+  }
+
+  // Open directory
+  DIR * d = opendir(dirname);
+
+  if (d == NULL) {
+    fprintf(stderr, "lsls: cannot open directory %s\n", dirname);
+    exit(2);
+  }
+
+  struct dirent *entry;
+
+  while ((entry = readdir(d)) != NULL) {
+    printf("%s\n", entry->d_name);
+  }
+
+  closedir(d);
+
+  return 0;
+}
+
   // ***print out dictory listing user specifies on command line.
   // ***IF no directory specified then print contents of current directory
 
@@ -18,10 +49,7 @@ int main(int argc, char **argv)
   // Close directory
     //closedir()
 
-  DIR *directory = opendir(".");
-  char *result = directory == NULL ? "Did not open" : "Succeded!"; // ternary operator
-  printf("Testing opening a directory:  %s\n", result);
-  closedir(directory);
-
-  return 0;
-}
+  // DIR *directory = opendir(".");
+  // char *result = directory == NULL ? "Did not open" : "Succeded!"; // ternary operator
+  // printf("Testing opening a directory:  %s\n", result);
+  // closedir(directory);

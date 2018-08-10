fabbc13a4986e04ebdc79b38e80adc52857d68b1
mister-corn <code@ronaldlibago.com>
2018-08-10 11:00:26 -0700

    T+1hr45min: Stretches done.

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <dirent.h>
+#include <sys/stat.h>
+#include <string.h>
 
 /**
  * Main
@@ -32,9 +34,28 @@ int main(int argc, char **argv)
   }
   // Repeatly read and print entries
   struct dirent *currdir = readdir(opened_dir);
-
+  struct stat buf;
+  // Possibly overwrought string manipulation for path arg of stat()
+  char path_str[255];
+  char path_str_tosize[255];
+  strcpy(path_str, path_to);
+  strncat(path_str, "/", 2);
+  printf("opened_dir: %s\n", (char*) path_str);
+  
   while (currdir) {
-    printf("%s\n", currdir->d_name);
+    strcpy(path_str_tosize, path_str);
+    strncat(path_str_tosize, currdir->d_name, 255 - strlen(path_str));
+
+    stat(path_str_tosize, &buf);
+
+    // printf("IFDIR result: %i\n", (int) S_ISDIR(buf.st_mode));
+
+    if (S_ISDIR(buf.st_mode)) {
+      printf("<DIR>\t\t%s\n", currdir->d_name);
+    }
+    else {
+      printf("%ld\t\t%s\n", buf.st_size, currdir->d_name);
+    }
     currdir = readdir(opened_dir);
   }
 

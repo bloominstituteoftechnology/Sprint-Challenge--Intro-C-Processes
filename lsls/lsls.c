df543e5023407821bcbed3e1d8d47937aad62535
astrebeck <alexstrebeck@gmail.com>
2018-08-10 10:04:03 -0600

    .

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -4,7 +4,7 @@
 /**
  * Main
  */
-int main(int argc, char **argv)
+int main(int argc)//, char *argv)
 {
   //variables
   DIR *dir;
@@ -13,7 +13,7 @@ int main(int argc, char **argv)
   // Parse command line
   
   // Open directory
-  dir = opendir(argv);
+  dir = opendir(".");
 
   ent = readdir(dir);
 

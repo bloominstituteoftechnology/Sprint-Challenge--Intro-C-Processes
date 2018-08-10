4f4d1f56f79b97c3c8718ab374f1995ec82f9e5d
David <AquilaVirtual>
2018-08-10 10:45:48 -0700

    MVP - Task 2 complete: lsls

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,18 +1,47 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <stdlib.h>
+
 
 /**
  * Main
  */
+
 int main(int argc, char **argv)
 {
-  // Parse command line
 
+   char *dir_name;
+  // we check if there are no additional arguments added, just print current directory
+  if (argc == 1)
+  {
+    dir_name = ".";
+  }
+  // We have arguments, print that directory
+  else if (argc  > 1)
+  {
+    dir_name = argv[1];
+  }
+  // We have an error
+  else
+  {
+    fprintf(stderr, "Something bahhhd!: './lsls (dir_name)%s\n", dir_name);
+    exit(1);
+  }
   // Open directory
+  struct dirent *de;  //Pointer for directory entry
+  DIR *dir = opendir(dir_name);
+  // read and print entries
+  if (dir)
+  {
+    while ((de = readdir(dir)) != NULL)
+    {
 
-  // Repeatly read and print entries
+       printf("%s\n", de->d_name);
 
+    }
   // Close directory
+    closedir(dir);
+  }
 
   return 0;
 }

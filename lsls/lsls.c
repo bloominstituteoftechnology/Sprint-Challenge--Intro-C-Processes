3e8fdd65c4e213b51cd7091d1a6b8425d6e31c57
samarv <samarvir1996@gmail.com>
2018-08-10 22:06:18 +0530

    completed mvp

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -6,7 +6,32 @@
  */
 int main(int argc, char **argv)
 {
+  
+  printf("\n----Following Are The Command Line Arguments Passed----");
+      printf("%s\n",argv[1]);
   // Parse command line
+  char* path =  argv[1];
+  // DIR *opendir(char *path);
+
+
+  struct dirent *ent;;  // Pointer for directory entry
+ 
+    // opendir() returns a pointer of DIR type. 
+    DIR *dr = opendir(path);
+ 
+    if (dr == NULL)  // opendir returns NULL if couldn't open directory
+    {
+        printf("Could not open current directory" );
+        return 0;
+    }
+ 
+    // Refer http://pubs.opengroup.org/onlinepubs/7990989775/xsh/readdir.html
+    // for readdir()
+    while ((ent = readdir(dr)) != NULL)
+            printf("%s\n", ent->d_name);;
+ 
+    closedir(dr);    
+    return 0;
 
   // Open directory
 

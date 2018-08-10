41c4c8d27d2caf1fe3914eda08fc06def8685787
udykas <udykas@gmail.com>
2018-08-10 12:26:16 -0400

    ls is working unless no 2nd argument is passed.

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -7,12 +7,28 @@
 int main(int argc, char **argv)
 {
   // Parse command line
+  char *dir_path;
+
+  if (argc == 1) {
+    dir_path = '.';
+  } else if ( argc == 2) {
+    dir_path = argv[1];
+  } else {
+    fprintf(stderr, "To execute file, run in this format: './lsls <directory-name>' \n");
+  }
 
   // Open directory
+  DIR *opened_dir = opendir(dir_path);
+  struct dirent *dir;
 
   // Repeatly read and print entries
-
-  // Close directory
+  if (opened_dir) {
+    while((dir = readdir(opened_dir)) != NULL) {
+      printf("%s\n", dir->d_name);
+    }
+      // Close directory
+    closedir(opened_dir);
+  }
 
   return 0;
 }

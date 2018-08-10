225b1942ae5894d77258a3d61bf0d50813c0850f
bitcointroy <travlr2010@gmail.com>
2018-08-10 14:19:59 -0500

    some progress on lsls.c

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -6,12 +6,26 @@
  */
 int main(int argc, char **argv)
 {
+  struct dirent *de; // Pointer for directory entry
+
   // Parse command line
+  // substitute a variable first
+  char *test_dir = ".";
 
   // Open directory
+  DIR *dr = opendir(test_dir); // opendir returns a pointer of DIR type
+
+  if (dr == NULL) 
+  {
+    printf("Could not open directory %s", test_dir);
+    exit(1);
+  }
 
   // Repeatly read and print entries
-
+  while ((de = readdir(dr)) != NULL) 
+  {
+    printf("%s\n", de->d_name);
+  }
   // Close directory
 
   return 0;

92192eabb885e87a5ba8f2cd7a3579483b9efafc
Erinc Emer <erincemer@Erincs-MBP.attlocal.net>
2018-08-10 10:48:29 -0700

    lsls done

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,18 +1,35 @@
 #include <stdio.h>
 #include <dirent.h>
-
+#include <stdlib.h>
 /**
  * Main
  */
 int main(int argc, char **argv)
 {
   // Parse command line
-
+  DIR *dr;
+	struct dirent *de;
   // Open directory
-
+  if (argc == 1) {
+    dr = opendir(".");
+  }
+  else if (argc == 2) {
+    dr = opendir(argv[1]);
+  }
+  else {
+    fprintf(stderr, "There is an error with file opening\n");
+  }
   // Repeatly read and print entries
-
+  if (dr == NULL) {
+    fprintf(stderr, "Error! Something is wrong\n");
+    exit(1);
+  }
+  else {
   // Close directory
-
+    while ((de = readdir(dr)) != NULL) 
+              printf("%s\n", de->d_name);
+      }
+  // Close directory
+  closedir(dr);
   return 0;
 }

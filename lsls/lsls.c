4858aaabc0235871ca79a97c7e2186c4241f494d
maxwashington <mnwashington18@gmail.com>
2018-08-10 12:27:25 -0500

    lsls Task Complete

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,18 +1,48 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <dirent.h>
+
+void ls_dir(const char *dir_name)
+{
+  DIR *directory;
+  struct dirent *entry;
+
+  directory = opendir(dir_name);
+  if (directory != NULL)
+  {
+    while ((entry = readdir(directory)) != NULL)
+    {
+      printf("%s\n", entry->d_name);
+    }
+    closedir(directory);
+  }
+  else
+  {
+    printf("Directory doesnt exist. %s\n", dir_name);
+    exit(1);
+  }
+}
 
 /**
  * Main
  */
+
 int main(int argc, char **argv)
 {
   // Parse command line
+  int x;
 
-  // Open directory
+  x = 1;
+  while (x < argc)
+  {
+    ls_dir(argv[x]);
+    x++;
+  }
 
-  // Repeatly read and print entries
-
-  // Close directory
+  if (argc == 1)
+  {
+    ls_dir(".");
+  }
 
   return 0;
 }

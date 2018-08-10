b9570efeb00651e07181110e2f60140393674c21
William VanDolah <william.vandolah@gmail.com>
2018-08-10 11:06:32 -0500

    able to print file names

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <stdlib.h>
 
 /**
  * Main
@@ -7,12 +8,37 @@
 int main(int argc, char **argv)
 {
   // Parse command line
+  char *directory_name;
+  if (argc == 1)
+  {
+    directory_name = ".";
+  }
+  else if (argc == 2)
+  {
+    directory_name = argv[1];
+  }
+  else
+  {
+    fprintf(stderr, "Too amny arguments! Usage: ./index2 [your directory name]\n");
+    exit(1);
+  }
 
   // Open directory
+  struct dirent *ent;
+  DIR *directory = opendir(directory_name);
+  if (directory == NULL)
+  {
+    fprintf(stderr, "The directory open error");
+    exit(2);
+  }
 
   // Repeatly read and print entries
+  while ((ent = readdir(directory)) != NULL)
+  {
+    printf("%s\n", ent->d_name);
+  }
 
   // Close directory
-
+  closedir(directory);
   return 0;
 }

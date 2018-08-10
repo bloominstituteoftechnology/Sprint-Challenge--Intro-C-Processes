8f1fb74166bdbb1d667e4c1dfb17f032e496c21f
Anne Courtney <annecourtney_1@yahoo.com>
2018-08-10 18:04:14 -0500

    updated my code to work better

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -3,52 +3,40 @@
 
 
 
-void directoryListing(const char *directoryName)
-{
-  DIR *directory;
-  struct dirent *entry;
 
-
-// opening the directory
-  directory = opendir(directoryName);
-  if (directory != NULL)
-  {
-      // read and print entries in the loop
-    while ((entry = readdir(directory)) != NULL)
-    {
-      printf("%s\n", entry->d_name);
-    }
-// Closing out the directory
-    closedir(directory);
-  }
-  else
-  {
-// printing directory name
-    printf("Can't open %s\n", directoryName);
-    exit(1);
-  }
-}
-
-
-// Main ->
-
+/**
+ * Main
+ */
 int main(int argc, char **argv)
 {
-  // Parsing the command line
-    int i;
-    i = 1;
+  // Parse command line
+  char *dirname;
 
-  while (i < argc)
-  {
-    directoryListing(argv[i]);
-    i++;
+  if (argc == 1) {
+    dirname = ".";
+  } else if (argc == 2)  {
+    dirname = argv[1];
+  } else {
+    fprintf(stderr, "You must provide a valid directory name.\n");
+    exit(1);
   }
-//checking to see if the user doesn't enter a directory and if so 
-// the . should become the directory
-    if (argc == 1)
-  {
-    directoryListing(".");
+  // Open directory
+  DIR * d = opendir(dirname);
+
+  if (d == NULL) {
+    fprintf(stderr, "directory cannot be opened.");
+    exit(2);
   }
+  struct dirent *entry;
+
+  // Repeatly read and print entries
+  
+  while ((entry = readdir(d)) != NULL) {
+    printf("%s\n", entry->d_name);
+  }
+
+  // Close directory
+  closedir(d);
 
   return 0;
 }

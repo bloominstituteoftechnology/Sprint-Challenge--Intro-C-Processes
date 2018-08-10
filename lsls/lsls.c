ee29e85caddd9daa076d5e02d0979e216dabbde4
JSNavey <jsnavey@gmail.com>
2018-08-10 13:05:33 -0400

    finish lsls.c

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -11,26 +11,48 @@ entry in the directory.*/
 
 int main(int argc, char **argv)
 {
-  // Parse command line
-  // Open directory
+  // Parse command line  
+  // check the count of argc, 
+  // `.` means current dir
+  // argv[1] = dirname
 
+  char *dirname;
+
+  if (argc == 1)
+  {
+    dirname = ".";
+  }
+  else if (argc == 2)
+  {
+    dirname = argv[1];
+  }
+  else
+  {
+    fprintf(stderr, "./lsls [dirname]\n");
+    exit(1);
+  }
+
+  // Open directory
   // The DIR data type represents a directory stream. It's declared in the header `dirent.h`
   DIR *d;
   struct dirent *dir;
   d = opendir("."); 
-
+  
+  // if error, `opendir()` returns NULL
+  if (d == NULL)
+  {
+    fprintf(stderr, "./lsls: cannot open directory %s\n", dirname);
+    exit(1);
+  }
   // Repeatly read and print entries
   // If find NULL, means no content in that dir
-  if(d)
+  while((dir = readdir(d)) != NULL)
   {
-    while((dir = readdir(d)) != NULL)
-    {
-      printf("%s\n", dir ->d_name);
-    }
+    printf("%s\n", dir->d_name);
+  }
 
   // Close directory
   closedir(d);
-  }
-
+  
   return 0;
 }

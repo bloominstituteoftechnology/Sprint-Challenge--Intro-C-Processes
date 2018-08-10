64d3ef751c7a4860050ff0995457f8826ee381f2
JSNavey <jsnavey@gmail.com>
2018-08-10 17:13:51 -0400

    add stretch goal solution

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -52,6 +52,36 @@ int main(int argc, char **argv)
     printf("%s\n", dir->d_name);
   }
 
+  // stretch
+  /*
+  while((dir = readdir(d)) != NULL)
+  {
+    // stat the entry
+    char fullpath[8192];  // 8192 bytes is size that for a buffer to read and write. Fairly arbitary.
+    snprintf(fullpath, sizeof(fullpath), "%s/%s", dirname, entry->d_name);
+    struct stat stat_buf;
+
+    if (stat(fullpath, &stat_buf) < 0)
+    {
+      fprintf(stderr, "lsls: failed to stat file %s\n", fullpath);
+      exit(3);
+    }
+
+    if (S_ISREG(stat_buf.st_mode))
+    {
+      printf("%10lld %s\n", stat_buf.st_size, entry->d_name);
+    }
+    else if (S_ISDIR(stat_buf.st_mode))
+    {
+      printf("%10s %s\n", "<DIR>", entry->d_name);
+    }
+    else
+    {
+      printf("%10s %s\n", "", entry->d_name);
+    }
+  }
+  */
+
   // Close directory
   closedir(d);
   

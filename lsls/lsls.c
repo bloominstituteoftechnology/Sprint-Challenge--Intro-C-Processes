4b4693f21281b760905ce2c309ec76d1cb3378d8
Zach Campbell <beazlebubbles@gmail.com>
2018-08-10 11:19:34 -0600

    Parsed command line

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -8,6 +8,24 @@ int main(int argc, char **argv)
 {
   // Parse command line
 
+
+  int show_hidden = 0;
+
+  int my_path_index = 0;
+
+  for (int i = 1; i < argc; i++) {
+    if (!strcmp(argv[i], "-a")) {
+      show_hidden = 1;
+    }
+    else {
+      my_path_index = i;
+    }
+
+  }
+
+
+
+
   // Open directory
 
   // Repeatly read and print entries

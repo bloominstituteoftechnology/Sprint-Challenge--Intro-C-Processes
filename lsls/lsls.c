37ee8ed1e2a71b1aee5b75d51305166af276e055
Lorin Fields <lorin.fields@gmail.com>
2018-08-10 10:46:35 -0700

    thought I had both stretc working. But found it's not working correctly. Moving on.

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <dirent.h>
 
+#include <sys/stat.h>
+
+
 /**
  * Main
  */
@@ -12,15 +15,25 @@ char* boss = argv[1];
 char* period = ".";
 char* b2 = (argLength == 2) ? boss : period;
 // Open directory
+struct stat st;
+
 DIR *dir;
 struct dirent *ent;
 if ((dir = opendir (b2)) != NULL) {
   // Repeatly read and print entries
   while ((ent = readdir (dir)) != NULL) {
+  // use stat to pull file size in bytes. then print
+  stat(ent->d_name, &st);
+  printf ("%10lld  ", st.st_size);
   printf ("%s\n", ent->d_name);
+
   }
   // Close directory
   closedir (dir);
  
   return 0;
 }}
+
+// struct stat st;
+// stat(filename, &st);
+// size = st.st_size;

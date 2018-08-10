f95c8e6f966da5dd07a00022f1c719d798b24150
A.D.Faris <adfaris@gmail.com>
2018-08-10 12:56:59 -0400

    MVP Complete

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,18 +1,40 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <stdlib.h>
+#include <sys/stat.h>
 
 /**
  * Main
  */
 int main(int argc, char **argv)
 {
+  DIR *dir;
+  struct stat st;
+  struct dirent *dent;
+
+  if (argc < 2)
+  {
+    printf("enter a directory \n");
+    exit(1);
+  }
   // Parse command line
+  for (int i = 0; i < argc; i++)
+  {
+    // Open directory
+    dir = opendir(argv[i]);
+  }
 
-  // Open directory
-
-  // Repeatly read and print entries
+  if (dir != NULL)
+  {
+    while ((dent = readdir(dir)) != NULL)
+    {
+      // Repeatly read and print entries
+      printf("%s\n", dent->d_name);
+    }
+  }
 
   // Close directory
+  closedir(dir);
 
   return 0;
 }

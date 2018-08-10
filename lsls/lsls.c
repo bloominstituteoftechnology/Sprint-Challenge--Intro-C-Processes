1bc15e5071669b823279f63f200c25bb54ffd01a
honda0306 <christopher.honda92@gmail.com>
2018-08-10 11:14:22 -0700

    Completed rough working version of lsls; also tested compiler in examples directory

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -8,12 +8,33 @@
 int main(int argc, char **argv)
 {
   // Parse command line
+  char *dirname;
 
+  if (argc == 1) {
+    dirname = ".";
+  } else if (argc == 2)  {
+    dirname = argv[1];
+  } else {
+    fprintf(stderr, "You must provide a valid directory name.\n");
+    exit(1);
+  }
   // Open directory
+  DIR *directory;
+  struct dirent *dir;
+  directory = opendir(".");
+
+  if (directory == NULL) {
+    fprintf(stderr, "The given directory %s cannot be opened.", dirname);
+    exit(1);
+  }
 
   // Repeatly read and print entries
+  
+  while ((dir = readdir(directory)) != NULL) {
+    printf("Open directory: %s\n", dir->d_name);
+  }
 
   // Close directory
-
+  closedir(directory);
   return 0;
 }

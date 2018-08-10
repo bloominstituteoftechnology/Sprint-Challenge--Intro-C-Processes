87ebdce85cedc12c3edf3c1c80b62af2ea11785c
jesuarva <jesuarva.developer@gmail.com>
2018-08-10 17:53:12 +0200

    TASK-2: Implemented 'opendir' && 'ls passed-path'

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <stdlib.h>
 
 /**
  * Main
@@ -8,14 +9,31 @@ int main(int argc, char **argv)
 {
   // Parse command line
   char *path = argv[1];
-  // printf("Path: %d\n", argc);
-  // printf("Path: %s\n", path);
+  printf("Arguments: %d\n", argc);
+  printf("Path: %s\n", path);
 
   // Open directory
+  DIR *directory = opendir(path);
+  if (directory == NULL)
+  {
+    printf("Ups, there were an issue opening the directory. Try again");
+    exit(1);
+  }
 
   // Repeatly read and print entries
+  struct dirent *entry;
+  do
+  {
+    entry = readdir(directory);
+    if (entry)
+    {
+      printf("%s\n", entry->d_name);
+    }
+  } while (entry != NULL);
 
   // Close directory
+  closedir(directory);
+  printf("END\n");
 
   return 0;
 }

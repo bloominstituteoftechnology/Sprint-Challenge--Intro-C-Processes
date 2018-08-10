cb603301865b4735d1f1c0f63b7229c59292675a
Dante O. Cuales, Jr <danteocualesjr@gmail.com>
2018-08-11 02:01:36 +0800

    Finally fixed bugs. Now able to print contents of user-specified folder.

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-// dirent.h contains the declarations for the opendir(), readdir(), and closedir() system calls.
 #include <dirent.h>
 #include <stdlib.h>
 #include <sys/stat.h>
@@ -9,6 +8,30 @@
  */
 int main(int argc, char **argv)
 {
+
+  // PARSE COMMAND LINE
+  /*
+  char *filename;
+
+  if (argc == 1)
+  {
+    filename = "index.c";
+    printf("We passed in no arguments.\n");
+  }
+
+  else if (argc == 2)
+  {
+    filename = argv[1];
+    printf("We passed in the file %s \n", filename);
+  }
+
+  else
+  {
+    fprintf(stderr, "You have entered more than one directory name. Enter only one. Sample: ./lsls [your_file_name]\n");
+    exit(1);
+  }
+  */
+
   // PARSE COMMAND LINE
   char *directory_name;
 
@@ -24,57 +47,58 @@ int main(int argc, char **argv)
 
   else
   {
-    fprintf(stderr, "You've inputed too many arguments. Correct input: ./index2 [your_directory_name]\n");
+    fprintf(stderr, "You have entered more than one directory name. Enter only one. Sample: ./lsls [your_directory_name]\n");
     exit(1);
   }
 
 
   // OPEN DIRECTORY
+  /*
+  DIR *directory = opendir(".");
+  char *result = directory == NULL ? "Did not open" : "Successful!";
+  printf("Testing opening a directory: %s\n", result);
+  closedir(directory);
+  */
+
+  // OPEN DIRECTORY
   DIR *directory = opendir(directory_name);
 
   // Check if directory is open
-  if (directory = NULL)
+  if (directory == NULL)
   {
-    fprintf(stderr, "Error in opening directory.\n");
+    fprintf(stderr, "Error in opening directory.");
+    exit(1);
   }
 
   // REPEATEDLY READ AND PRINT ENTRIES
-  struct dirent *ent;
+  struct dirent *entry;
 
-  while ((ent = readdir(directory)) != NULL)
+  while ((entry = readdir(directory)) != NULL)
   {
-    char path[20000];
-    struct stat stat_buf;
-
-    // We need the full path
-    snprintf(path, sizeof(path), "%s/%s", directory_name, ent->d_name);
-
-    if (stat(path, &stat_buf) == -1)
-    {
-      fprintf(stderr, "Failed to show the stats for %s.\n", path);
-      exit(2);
-    }
-
-    if (stat_buf.st_mode & S_IFREG)
-    {
-      // Print the file name and its size in bytes
-      printf("%20lld %s\n", stat_buf.st_size, ent->d_name);
-    }
-
-    else if (stat_buf.st_mode & S_IFDIR)
-    {
-      // Directory name
-      printf("%20lld %s\n", "<DIR>", ent->d_name);
-    }
-
-    else
-    {
-      printf("%10s %s \n", " ", ent->d_name);
-    }
+    printf("%s\n", entry->d_name);
   }
 
-  // Close directory
+  // CLOSE DIRECTORY
   closedir(directory);
+
+  /*
+  struct stat file_stat;
+
+  while (argc-- > 1)
+  {
+    if (lstat(argv[argc], &file_stat) == -1)
+    {
+      fprintf(stderr, "%s\n", strerror(errno));
+    }
+    else
+    {
+      fprintf(stdout, "Links\tUid\tGid\tSize\tName\n");
+      fprintf(stdout, "%u\t%u\t%u\t%u\t%s\n", file_stat.st_nlink,
+              file_stat.st_uid, file_stat.st_gid, file_stat.st_size,
+              argv[argc]);
+    }
+  }
+  */
 
   return 0;
 }

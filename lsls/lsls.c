f5aef759d3eda607814ec7162e7505c0f59435be
FireInjun <mk@FireInjun.com>
2018-08-10 13:13:15 -0400

    Completed main part and looked around for assistance on stretches. Still not SUPER happy with the results and wish to continue more studies with `C`. It's a Fascinating language and has plenty of potential for power and control.

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <sys/stat.h>
+#include <string.h>
 
 /**
  * Main
@@ -7,12 +9,43 @@
 int main(int argc, char **argv)
 {
   // Parse command line
+  char *dirName = argv[1] == NULL ? "." : argv[1];
+  struct dirent *ent;
+  DIR *dirPointer;
+  struct stat statBuffer;
 
   // Open directory
+  if (!(dirPointer = opendir(dirName)))
+  {
+    fprintf(stderr, "Error: Could not open directory %s\n", dirName);
+    return -1;
+  }
 
   // Repeatly read and print entries
+  while (ent = readdir(dirPointer))
+  {
+    char *pathName = strdup(dirName);
 
+    strcat(pathName, "/");
+    strcat(pathName, ent->d_name);
+
+    stat(pathName, &statBuffer);
+
+    if (statBuffer.st_mode & S_IFDIR)
+    {
+      printf("%10s %s\n", "<DIR>", ent->d_name);
+    }
+    else if (statBuffer.st_mode & S_IFREG)
+    {
+      printf("%10lld %s\n", statBuffer.st_size, ent->d_name);
+    }
+    else
+    {
+      printf("%10s %s\n", "<SPECIAL>", ent->d_name);
+    }
+  }
   // Close directory
+  closedir(dirPointer);
 
   return 0;
 }

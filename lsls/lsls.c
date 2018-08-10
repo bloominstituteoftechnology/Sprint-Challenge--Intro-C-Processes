a0d277f99713a62f572e7a268fb2ac0318373d7d
Brandon Hopper <brandonhopper0@gmail.com>
2018-08-10 12:45:34 -0400

    -completed lsls.c

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,18 +1,21 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <stdlib.h>
+#include <sys/stat.h>
 
 /**
  * Main
  */
 int main(int argc, char **argv)
 {
-  // Parse command line
-
-  // Open directory
-
-  // Repeatly read and print entries
-
-  // Close directory
-
-  return 0;
+  DIR *d;
+  struct dirent *dir;
+  d = opendir(".");
+  if (d) {
+    while ((dir = readdir(d)) != NULL) {
+      printf("%s\n", dir->d_name);
+    }
+    closedir(d);
+  }
+  return(0);
 }

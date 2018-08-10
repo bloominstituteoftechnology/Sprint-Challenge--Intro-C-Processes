2cadb3d92712416482556e53b73e75ab7b7ea897
Joanne Jordan <Joanne.k.m.jordan@gmail.com>
2018-08-10 09:46:41 -0700

    Update lsls.c

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -9,10 +9,25 @@ int main(int argc, char **argv)
   // Parse command line
 
   // Open directory
+  struct dirent *de;
+ 
+  DIR *dr = opendir(((argc == 1 ) ? (".") : (argv[1])));
+ 
+  if (dr == NULL)
+  {
+    printf("Could not open directory\n");
+    return 0;
+  }
 
   // Repeatly read and print entries
+ 
+  while ((de = readdir(dr)) != NULL)
+  {
+    printf("%s\n", de->d_name);
+  }
 
-  // Close directory
+  // Close directory  
+  closedir(dr);    
 
   return 0;
 }

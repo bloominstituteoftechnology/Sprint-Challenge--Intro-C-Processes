1f8c333c2694ee48d98fef640613c8d44485feba
Xang Thao <xangthao24@yahoo.com>
2018-08-10 09:44:49 -0700

    Completed mvp Sprint

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <dirent.h>
 
 /**
@@ -7,12 +8,28 @@
 int main(int argc, char **argv)
 {
   // Parse command line
-
+  int count = argc;
+  
   // Open directory
+  DIR *dir;
+  struct dirent *sd;
+
+  dir = opendir(argv[1]);
+
+  if(dir == NULL) {
+    dir = opendir(".");
+  }
+ 
 
   // Repeatly read and print entries
 
+  while( (sd=readdir(dir)) != NULL ) {
+    printf("%s \n", sd->d_name);
+  }
+
   // Close directory
+
+  closedir(dir);
 
   return 0;
 }

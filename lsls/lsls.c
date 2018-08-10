ad452d25ec439cfb0230f32dc22a246c3e00cfce
Usman Javed <ujaved931@gmail.com>
2018-08-10 22:03:45 +0500

    Working on Task 2 Finished basic directory reading program Working on commandline parser now

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
 #include <dirent.h>
 
 /**
@@ -9,10 +11,20 @@ int main(int argc, char **argv)
   // Parse command line
 
   // Open directory
+  struct dirent *de;
+  DIR *dr = opendir(".");
+
+  if (dr == NULL)  // opendir returns NULL if couldn't open directory
+    {
+        printf("Could not open current directory.\n" );
+        exit(1);
+    }
 
   // Repeatly read and print entries
+  while ((de = readdir(dr)) != NULL)
+    printf("%s\n", de->d_name);
 
   // Close directory
-
+  closedir(dr);
   return 0;
 }

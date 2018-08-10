7b588a4f7c265831206e1913833efbc149df4eeb
Kelvin <kelvinli5354@yahoo.com>
2018-08-10 13:07:47 -0400

    updated

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -4,15 +4,16 @@
 /**
  * Main
  */
-int main(int argc, char **argv)
-{
-  // Parse command line
-
-  // Open directory
-
-  // Repeatly read and print entries
-
-  // Close directory
+ int main(void) {
+   DIR *d;
+   struct dirent *dir;
+   d = opendir(".");
+   if (d) {
+     while ((dir = readdir(d)) != NULL) {
+       printf("%s\n", dir->d_name);
+     }
+     closedir(d);
+   }
 
   return 0;
 }

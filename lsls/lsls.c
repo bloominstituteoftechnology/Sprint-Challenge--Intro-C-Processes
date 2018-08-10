8c70c749f872385547614404b571665acef94ac5
Christina Kopecky <kopecky12112@gmail.com>
2018-08-10 09:56:17 -0700

    basic dir listing works
    added comments to indicate what's going on in code

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,18 +1,53 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <errno.h>
+
 
 /**
  * Main
  */
+
+int directoryList(const char *path)
+{
+  struct dirent *entry;
+  DIR *dir;
+
+  //open directory stream
+  dir = opendir(path);
+  if (dir == NULL)
+  {
+    fprintf(stderr, "err opendir %s\n", path);
+    exit(2); //inability to compare - error thrown and exit process
+  }
+
+  while((entry = readdir(dir))){
+    puts(entry->d_name);
+  }
+  //close stream
+  closedir(dir);
+  return 0;
+}
+
 int main(int argc, char **argv)
 {
-  // Parse command line
 
-  // Open directory
+  int counter = 1;
 
-  // Repeatly read and print entries
+  //current working directory if no arguments on command line
 
-  // Close directory
-
+  if (argc == 1)
+  {
+    directoryList(".");
+  }
+  //dir in command line 
+  //tested with ../../Github
+  
+  while(counter < argc){
+    printf("\nDirectory Listing %s\n", argv[counter]);
+    directoryList(argv[counter]);
+    counter++;
+  }
   return 0;
 }

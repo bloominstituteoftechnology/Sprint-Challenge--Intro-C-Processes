a77bfb6eb8d262c1491fa6fdf22ca12192d65772
Ilho Kim <ilhokim@Ilhoui-MacBook-Pro.local>
2018-08-17 11:00:08 -0700

    fix: clean up code

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -12,18 +12,15 @@ int main(int argc, char **argv)
   struct dirent *ent;
 
   // Parse command line
+  // If no directory is specified in command line, print instruction and exit process
   if (argc == 1)
   {
-    printf("Please specify a directory name when running me\n");
+    printf("Please specify a directory name when running me such as: ./lsls [directory name]\n");
     exit(1);
   }
 
   // Open directory
-  for (int i = 0; i < argc; i++)
-  {
-    // printf(" %s\n", argv[i]);
-    dir = opendir(argv[i]);
-  }
+  dir = opendir(argv[1]);
 
   if (dir != NULL)
   {

1c6122d8acc35377cc0405953b1e47c59a6dfb78
Ilho Kim <ilhokim@Ilhoui-MacBook-Pro.local>
2018-08-17 10:53:47 -0700

    feat: MVP complete
     - It took A LOT of time before adding #include <stdlib.h> to resolve error

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,18 +1,45 @@
 #include <stdio.h>
-#include <dirent.h>
+#include <dirent.h> // holds the declarations for DIR, struct dirent, opendir(), readdir(), and closedir()
+#include <unistd.h>
+#include <stdlib.h>
 
 /**
  * Main
  */
 int main(int argc, char **argv)
 {
+  DIR *dir;
+  struct dirent *ent;
+
   // Parse command line
+  if (argc == 1)
+  {
+    printf("Please specify a directory name when running me\n");
+    exit(1);
+  }
 
   // Open directory
+  for (int i = 0; i < argc; i++)
+  {
+    // printf(" %s\n", argv[i]);
+    dir = opendir(argv[i]);
+  }
 
-  // Repeatly read and print entries
-
+  if (dir != NULL)
+  {
+    // Repeatly read and print entries
+    while ((ent = readdir(dir)) != NULL)
+    {
+      printf("%s\n", ent->d_name);
+    }
+  }
+  else
+  {
+    printf("Directory opening error");
+    exit(2);
+  }
   // Close directory
+  closedir(dir);
 
   return 0;
 }

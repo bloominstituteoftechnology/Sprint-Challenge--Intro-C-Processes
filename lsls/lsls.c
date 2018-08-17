644ef09168ec81f91742bffe9a62acd1223cbbf7
april7229 <martinezapril56@yahoo.com>
2018-08-17 19:31:49 -0400

    trying

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,90 +1,100 @@
 #include <stdio.h>
 
-#include <dirent.h>
+#include <dirent.h> //let's us open directory
 
 #include <stdlib.h>
 
 #include <sys/stat.h>
 
-#include <string.h>
-
-#include <errno.h>
-
-
-
-
 //https://linux.die.net/man/3/opendir
-  
 
-     
 /**
  * Main
  */
-// Parse command line  
+// Parse command line
 
-  // check the count of argc, 
+// check the count of argc,
 
-  // `.` means current dir
+// `.` means current dir
 
-  // argv[1] = dirname
+// argv[1] = dirname
 int main(int argc, char **argv)
 {
-  char *dirname;//hold whatever arg passed
+  char *dirname; //hold whatever arg passed
   // Parse command line
-  if(argc == 1){//argc by default is 1 can not be 0 name of program is 1
+
+  if (argc == 1)
+  {
+    //argc by default is 1 can not be 0 name of program is 1
     //set the directory that were going to be looking at tp "."
     dirname = ".";
-
-  }else if (argc == 2){ //second arg
+  }
+  else if (argc == 2)
+  {
+    //second arg
     dirname = argv[1];
-  }else {
+  }
+  else
+  {
     //standard error message
     fprintf(stderr, "usage: lsls [dirname]\n ");
     //return 1; would work to end program
     exit(1);
   }
   //Open directory
-  DIR *d;
-  struct dirent * dir;
-  d = opendir(dirname);
+  // DIR *d;
+  // struct dirent *dir;
+  // d = opendir(dirname);
+
+  DIR *d = opendir(dirname);
 
   // The DIR data type represents a directory stream. It's declared in the header `dirent.h`
-  if (d ==NULL){
-    fprint(stderr,"lsls: cannot open directory %s\n", dirname);
+  if (d == NULL)
+  {
+    fprint(stderr, "lsls: cannot open directory %s\n", dirname);
+    
     exit(2);
   }
 
   // If find NULL, means no content in that dir
   //single directory entry
   //repeatly read and print entries
-   struct dirent *entry;
+  //To define a structure, you must use the struct statement. The struct statement defines a new data type,
+  struct dirent *entry;
 
-  while ((entry = readdir(d)) !=NULL){
+  while ((entry = readdir(d)) != NULL)
+  {
     //stat the entry
-    char fullpath[8192];// 8192 bytes is size that for a buffer to read and write. Fa
+    char fullpath[8192];      
+                                                // 8192 bytes is size that for a buffer to read and write. Fa
     snprint(fullpath, sizeof(fullpath), "%s/%s", dirname, entry->d_name); //snprintf is essentially a function that redirects the output of printf to a buffer.
+   
     struct stat stat_buf;
 
-     if(stat(fullpath, &stat_buf) < 0) {
+    if (stat(fullpath, &stat_buf) < 0)
+    {
 
-       fprintf(stderr,"lsls: failed to stat file %s\n", fullpath);
+      fprintf(stderr, "lsls: failed to stat file %s\n", fullpath);
 
-       exit(3);
-     }
-     if (S_ISREG(stat_buf.st_mode)){
-       
-       printf("10%lld %s\n", stat_buf.st_size, entry->d_name);
+      exit(3);
+    }
 
-      }else if (S_ISDIR(stat_buf.st_mode)){
+    if (S_ISREG(stat_buf.st_mode))
+    {
+     
+      printf("10%lld %s\n", stat_buf.st_size, entry->d_name);
+    }
+    
+    else if (S_ISDIR(stat_buf.st_mode))
+    {
 
-        printf("%10s %s\n","DIR", entry->d_name);
-      }else{
+      printf("%10s %s\n", "DIR", entry->d_name);
+    }
+    else
+    {
 
-        printf("%10s %s\n", "", entry->d_name); 
-
-
-      }
-
+      printf("%10s %s\n", "", entry->d_name);
+    }
   }
   //10 will give it ten spaces make it more prettier
   // while (1) {
@@ -94,10 +104,9 @@ int main(int argc, char **argv)
   //   }
   //   printf();
   // }
-   //close directory
-//asigned interger types off_t
-   closedir(d);
+  //close directory
+  //asigned interger types off_t
+  closedir(d);
 
   return 0;
 }
-

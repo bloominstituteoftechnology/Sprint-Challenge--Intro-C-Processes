3f6ca4ac52bcb4cd6fa755d8ef041620a11e8fc4
Lavell Burton <taylormade800@gmail.com>
2018-08-17 09:10:11 -0700

    initial commit still working on commenting

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -7,12 +7,27 @@
 int main(int argc, char **argv)
 {
   // Parse command line
-
+  DIR *dr;
+  struct dirent *de;
   // Open directory
-
+  if (argc == 1) {
+    dr = opendir(argv[1]);
+  }
+  else 
+  {
+    fprintf(stderr, "Error opening the file\n");
+  }
   // Repeatly read and print entries
-
-  // Close directory
-
+  if (dr == NULL) 
+  {
+    fprintf(stderr, "Error readign entries\n");
+  } 
+  else 
+  {
+    // Close directory
+    while ((de = readdir(dr)) != NULL)
+      printf("%s\n", de->d_name);
+  }
+  closedir(dr);
   return 0;
 }

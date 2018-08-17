5231396d1ed4f1ba2296a8f4272c11db62df5312
Jacob Henry <jacobiswinning@gmail.com>
2018-08-17 11:44:58 -0600

    added lsls.c and compiled sprint

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,18 +1,39 @@
 #include <stdio.h>
 #include <dirent.h>
-
+#include <stdlib.h>
+#include <sys/stat.h>
 /**
  * Main
  */
 int main(int argc, char **argv)
 {
   // Parse command line
-
+  char *directory;
+  struct stat statbuf;
+  char *dir = "<DIR>";
+  if (argc > 1)
+    directory = argv[1];
+  else
+    directory = ".";
+  printf("%s\n", directory);
   // Open directory
-
+  DIR *target = opendir(directory);
+  if (target == NULL)
+  {
+    printf("%s\n", "Cannot open directory");
+    exit(1);
+  }
   // Repeatly read and print entries
-
+  struct dirent *entry;
+  while (entry = readdir(target))
+  {
+    stat(entry->d_name, &statbuf);
+    if (statbuf.st_size != 0)
+      printf("%10lld %s\n", statbuf.st_size, entry->d_name);
+    else
+      printf("%10lls %s\n", dir, entry->d_name);
+  }
   // Close directory
-
+  closedir(target);
   return 0;
 }

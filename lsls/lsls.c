3c13880b200fae949e7f7e342d16fb4a97525906
Herbert Hodgson <herberthodgson@hotmail.com>
2018-08-17 14:41:07 -0400

    Stretch Goal: Print file size in bytes completed

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,14 +1,16 @@
-#include <stdio.h>  // printf function access
-#include <stdlib.h> // exit() function access
-#include <dirent.h> // holds the declarations for DIR, struct dirent, opendir(), readdir(), and closedir()
+#include <stdio.h>    // printf function access
+#include <stdlib.h>   // exit() function access
+#include <dirent.h>   // holds the declarations for DIR, struct dirent, opendir(), readdir(), and closedir()
+#include <sys/stat.h> // contains the struct stat and stat() needed to print file size in bytes.
 
 /**
  * Main
  */
 int main(int argc, char **argv) //taking parameters of argument count, and a pointer to an array of argument values
 {
-  DIR *dir;          // initializing a pointer to variable dir of type DIR
-  struct dirent *dp; // initializing a pointer to a struct dirent named dp
+  DIR *dir;           // initializing a pointer to variable dir of type DIR
+  struct dirent *dp;  // initializing a pointer to a struct dirent named dp
+  struct stat buffer; // structure variable buffer is defined for the stat structure
 
   // Parse command line
   if (argc == 2) // if argument count is greater than 1
@@ -28,7 +30,13 @@ int main(int argc, char **argv) //taking parameters of argument count, and a poi
   // Repeatly read and print entries
   while ((dp = readdir(dir)) != NULL) // readdir() returns NULL when there are no more directory entries.
   {
-    printf("%s\n", dp->d_name); // While there are entries print out the d_name field of the dp struct we declared
+    int status = stat(dp->d_name, &buffer); // The stat() function obtains information about the named file and writes it to the area pointed to by the buffer argument (struct stat buffer).
+    // declaring status an integer value to capture if the stat function returns a 0 or a -1
+    if (status == -1) // upon successful completion, 0 is returned. Otherwise -1 is returned to indicate an error.
+    {
+      fprintf(stderr, "Failed stat function at %s\n", dp->d_name);
+    }
+    printf("%10ld  %s\n", buffer.st_size, dp->d_name); // While there are entries print out the file size in bytes from st_size field of the buffer struct and d_name field of the dp struct we declared
   }
   // Close directory
   closedir(dir); // closes the directory opened previously by opendir() when we are done

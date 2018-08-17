528eb7798c22f997e58d27adbe5a080fda642f89
maribelcuales <maribelcuales@gmail.com>
2018-08-18 00:35:34 +0800

    passed codes to parse command line

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,4 +1,5 @@
-#include <stdio.h>
+#include <stdio.h>  // install the necessary libraries
+#include <stdlib.h> // to call the exit function
 #include <dirent.h>
 
 /**
@@ -7,6 +8,15 @@
 int main(int argc, char **argv)
 {
   // Parse command line
+  char *dir_name; // initialize the directory
+  if (argc == 1) {  // no argument/path was received; argc default is 1
+    dir_name = "."; // set the directory to "."
+  } else if (argc == 2) { // if an argument/path was received (bec argc > default 1)
+    dir_name = argv[1]; // set the directory to 1 which is the path; 0 is assigned for the lsls path
+  } else {
+    fprintf(stderr, "There is an error. Use this format: lsls [directory_name]\n"); // in case of error, print this error message
+    exit(1);  // exit with code 1
+  }
 
   // Open directory
 

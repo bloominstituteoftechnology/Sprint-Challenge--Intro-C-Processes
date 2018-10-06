407db546f8b8890d024a74d67eb077fc668d1c7e
Lisa Canini <three_nineteen@yahoo.com>
2018-10-06 10:54:37 -0700

    added file sizes

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,23 +1,32 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <sys/stat.h>
 
-/**
+    /**
  * Main
  */
 int main(int argc, char **argv)
 {
   DIR *d;
   struct dirent *dir;
+  struct stat buf;
   printf("Enter a directory: \n");
   scanf("%s", argv);
   d = opendir(argv);
 
-  if( d == NULL) {
+  if( !d ) {
     d = opendir(".");
+  }
+
+  if (d == NULL) {
+    printf("There was an error opening this directory");
+    exit(1);
   }
 
   while ((dir = readdir(d)) != NULL)
   {
+    stat(dir->d_name, &buf);
+    printf("%10ld  ", buf.st_size);
     printf("%s\n", dir->d_name);
   }
   closedir(d);

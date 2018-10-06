f7b4cf14d73c2a1bfca61d207adb499b760defe5
unknown <jesse.reichel@live.com>
2018-10-06 10:47:46 -0700

    lsls complete

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -6,13 +6,27 @@
  */
 int main(int argc, char **argv)
 {
-  // Parse command line
+  DIR *directory;
+  struct dirent *entry;
+  directory = opendir(argv[1]);
+  if (directory == NULL) {
+    printf("Cannot open directory '%s'\n", directory);
+    return 1;
+  }
+  rc = fork();
+  if(rc < 0) {
+    fprintf(stderr, "Fork failed \n");
+    exit(1)
+  } else if(rc == 0) {
+    while ((entry = readdir(directory)) != NULL) {
+      printf("%s \n", entry->d_name);  
+    }
+  } else {
+    int wc = waitpid(rc, NULL, 0);
+    closedir(directory);
+  }
 
-  // Open directory
 
-  // Repeatly read and print entries
-
-  // Close directory
 
   return 0;
 }

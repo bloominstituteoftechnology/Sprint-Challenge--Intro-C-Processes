fb2eb922d5b67be0b70d9c7ff47d848a2b4261f2
unknown <alexisreyes90@gmail.com>
2018-10-06 12:43:35 -0400

    Task 2 50% complete, stuck on reading the rest of the file directories

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -4,15 +4,25 @@
 /**
  * Main
  */
-int main(int argc, char **argv)
+int main(int argc, char **argv) // main takes in a argv pointer. Is argc number or args given?
 {
-  // Parse command line
+  DIR *directoryName; // Store directory name in a pointer
+  struct dirent *pointDir; // struct that finds serial # and Directory name
 
-  // Open directory
+  if(argc == 1) // need to check that arg[1] is set to something
+  {
+    directoryName = opendir(argv[1]); // set dir to that arg index, and open it, need pointer path for Opendir
 
-  // Repeatly read and print entries
+  } else { // that directory doesn't exit and can't be located
 
-  // Close directory
+    printf("File directory does not exist.\n");
+
+    // exit(1); do I need to exit?
+  }
+
+  // Now I need to iterate and find all directories. Do... While? Or NULL check? With while loop?
+  
+
 
   return 0;
 }

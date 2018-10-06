26a153e9985dadc1914f6b81d9bdce9694e72a1b
kammagik <kammagik@yahoo.com>
2018-10-06 12:10:24 -0700

    sprint challenge

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -6,13 +6,23 @@
  */
 int main(int argc, char **argv)
 {
+  
+  struct dirent *dr;
+  DIR *dir;
   // Parse command line
-
+  if (argc < 1) {
+    //if parameter holds more than 1 argument open?
   // Open directory
-
+  dir = opendir(".");
+  }
   // Repeatly read and print entries
-
+  if (dir == NULL) {
+    printf("error\n");
+    exit(1);
+  }
+  while(( dr = readdir(dir))!= NULL)
+    printf("%s\n", dr->d_name);
   // Close directory
-
+  closedir(dir);
   return 0;
 }

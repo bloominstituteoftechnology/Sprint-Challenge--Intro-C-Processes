11082927f72b9300b95ec993649d9caa11721d5c
darthMinimus <irishseanvaldivia@gmail.com>
2018-10-06 19:22:27 -0500

    Successfully completed lsls challenge!

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,18 +1,45 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <sys/stat.h>
 
-/**
- * Main
- */
+
+
 int main(int argc, char **argv)
 {
-  // Parse command line
+  // dirent is the directory entry 
+  struct dirent *ent;
+  DIR *dir;
+  // parse the command line parameters
+  if (argc > 2) {
+    // if too many args, print error and exit
+    printf("Error! Too many arguments!");
+    exit(1);
+  } else if (argc < 2) {
+    // if no args passed in by user, assign "." value to dir
+    dir = opendir(".");
+  } else {
+    // otherwise, assign the directory the user provides
+    dir = opendir(argv[1]);
+  }
 
-  // Open directory
+  if (dir == NULL) {
+    printf("Directory not found!\n");
+    exit(1);
+  }
 
-  // Repeatly read and print entries
-
+  while((ent = readdir(dir))!= NULL)
+  {
+    if(argc < 2)
+    {
+      printf("./%s\n", ent->d_name);
+    }
+    else
+    {
+      printf("%s\n", ent->d_name);
+    }
+  }
   // Close directory
+  closedir(dir);
 
   return 0;
 }

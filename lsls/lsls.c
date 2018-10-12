f2fb4fd2eb565c37b6d91261ed6a835636c46884
Randy Calderon <randycweb@gmail.com>
2018-10-12 09:02:38 -0700

    Added read and print + close

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -8,20 +8,33 @@ int main(int argc, char **argv)
 {
   // Parse command line
   if (argc < 2) {
-    printf('Usage: ./lsls <directory name>');
+    printf("Usage: %s <directory name>\n", argv[0]);
     return 0;
   }
 
   // Open directory
   DIR *directory;
-  if(directory == NULL) {
-    printf("Cannot open directory '%s'\n", argv[1]);
-  } else {
-    directory = opendir('.');
-  }
-  // Repeatly read and print entries
+  struct dirent *entry;
 
+  if((directory = opendir(argv[1])) == NULL)
+  {
+    perror("opendir");
+    return 0;
+  }
+
+  // Repeatly read and print entries
+  int i = 0;
+  while ((entry = readdir(directory)) != NULL) 
+  {
+    i++;
+    printf("\n%s", entry -> d_name );
+  }
   // Close directory
+  if (closedir(directory) == -1) 
+  {
+    perror("closedir");
+    return 0;
+  }
 
   return 0;
 }

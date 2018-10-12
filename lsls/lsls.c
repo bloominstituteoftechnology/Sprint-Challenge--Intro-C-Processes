07fe9137a20203217d1f27c6b005755eb7f1f10f
Ari Mercado <ari@Apples-Mac-3.local>
2018-10-12 10:15:45 -0700

    parse command line using opendir

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <stdlib.h>
 
 /**
  * Main
@@ -7,8 +8,26 @@
 int main(int argc, char **argv)
 {
   // Parse command line
+  // use opendir() system call
+  for (int i = 0; i < argc; i++) {
+    printf("%s\n", argv[i]);
+  }
+  char *dirname;
 
+  if (argc == 1) {
+    dirname = ".";
+  } else if (argc == 2) {
+    dirname = argv[1];
+  } else {
+    fprintf(stderr, "usage: ./lsls [dirname]\n");
+    exit(1);
+  }
   // Open directory
+  DIR *d = opendir(dirname);
+  if (d == NULL) {
+    fprintf(stderr, "cannot open directory: %\n", dirname);
+    exit(2);
+  }
 
   // Repeatly read and print entries
 

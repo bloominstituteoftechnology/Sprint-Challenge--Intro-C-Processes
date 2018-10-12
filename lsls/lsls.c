0e28374421196a684328396275e71556decb11a9
Randy Calderon <randycweb@gmail.com>
2018-10-12 08:41:30 -0700

    Added parse command line

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -7,9 +7,18 @@
 int main(int argc, char **argv)
 {
   // Parse command line
+  if (argc < 2) {
+    printf('Usage: ./lsls <directory name>');
+    return 0;
+  }
 
   // Open directory
-
+  DIR *directory;
+  if(directory == NULL) {
+    printf("Cannot open directory '%s'\n", argv[1]);
+  } else {
+    directory = opendir('.');
+  }
   // Repeatly read and print entries
 
   // Close directory

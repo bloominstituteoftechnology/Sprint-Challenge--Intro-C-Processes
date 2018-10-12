e3792fbef26eb8bc9d0f821c09675a0157b7ea8e
Eric B <timberwolf.eric@gmail.com>
2018-10-12 16:54:23 -0500

    Getting started on directory printouts

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,14 +1,29 @@
 #include <stdio.h>
 #include <dirent.h>
-
+#include <stdlib.h>
 /**
  * Main
  */
 int main(int argc, char **argv)
 {
+  char *dirid;
   // Parse command line
+  if (argc == 1)
+  {
+    dirid = '.';
+  }
+  else if (argc == 2)
+  {
+    
+  }
 
   // Open directory
+  DIR *dirpoint = opendir(dirid);
+  if (d == NULL) // checking if directory exists based on DIR return
+  {
+    printf("This directiory cannot be found or opened: %s\n", dirpoint);
+    exit(); // exit if directory name not found 
+  }
 
   // Repeatly read and print entries
 

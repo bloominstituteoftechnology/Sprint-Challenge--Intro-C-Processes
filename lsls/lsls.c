5e655a22b9053e8e569855875caff146289db0df
Daniel Chamorro <dan@dcitllc.com>
2018-10-12 14:23:14 -0400

    Testing lsls

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -7,12 +7,21 @@
 int main(int argc, char **argv)
 {
   // Parse command line
+  DIR *dir;
 
   // Open directory
+  dir = opendir(".");
+
+  if (dir == NULL)
+  {
+    printf("Error! Cant open directory. \n");
+    exit(1);
+  }
 
   // Repeatly read and print entries
 
   // Close directory
+  closedir(dir);
 
   return 0;
 }

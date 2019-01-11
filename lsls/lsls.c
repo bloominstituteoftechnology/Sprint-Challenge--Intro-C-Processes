b9aa8f81020b26149ef9697233e33fbb7eb16305
Cameron Ray <cameronray@Camerons-MacBook-Pro.local>
2019-01-11 13:53:40 -0800

    comments added for clarity

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -13,24 +13,24 @@ int main(int argc, char **argv)
   struct dirent *ent;
   DIR *strm;
   // Parse command line
-  if (argc==2) {
+  if (argc==2) { // if 2 arguments passed in
     // Open directory
-    if ((strm=opendir(argv[1]))!=NULL) {
+    if ((strm=opendir(argv[1]))!=NULL) { // if the user specified a directory name that opens a dir
       while ((ent=readdir(strm))!=NULL){
         struct stat buf;
         char subdir[1028] = "";
-        strcat(subdir, argv[1]);
-        if (argv[1][strlen(argv[1])-1]!='/') {
+        strcat(subdir, argv[1]); // here we need to construct a file path from the argument passed in by use
+        if (argv[1][strlen(argv[1])-1]!='/') { // add a slash at the end if not there already
           strcat(subdir,"/");
         }
         strcat(subdir, ent->d_name);
-        strcat(subdir,"\0");
+        strcat(subdir,"\0"); // make sure to end the string with a null character
         stat(subdir, &buf);
         // Repeatly read and print entries
         if ((buf.st_mode & S_IFDIR)!=0) {
-          printf("<DIR> %s\n",ent->d_name);
+          printf("<DIR> %s\n",ent->d_name); // print <DIR> instead of byte size if it's a directory
         } else {
-          printf("%lld %s\n", buf.st_size,ent->d_name);
+          printf("%lld %s\n", buf.st_size,ent->d_name); // print byte size if it's a file
         }
     }
     // Close directory
@@ -39,9 +39,9 @@ int main(int argc, char **argv)
       printf("Program has error.");
       exit(-1);
   }
-  // Parse command line
+  // if one argument is given open current directory:
   } else if (argc==1) {
-    if ((strm=opendir("./"))!=NULL) {
+    if ((strm=opendir("./"))!=NULL) { // is current directory not null?
       while ((ent=readdir(strm))!=NULL){
         struct stat buf;
         char subdir[1028] = "";

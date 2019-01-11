816fb68d03c1e89ba0ef77292e458ea4b712f254
Luisan327 <luisangelsc@gmail.com>
2019-01-11 11:28:35 -0600

    prints file sizes

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,27 +1,32 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <string.h>
+#include <sys/stat.h>
 
 /**
  * Main
  */
 int main(int argc, char **argv)
 {
+  struct stat buf;
   struct dirent *de;
   DIR *d;
 
   if (argv[1] != '\0'){
     d = opendir(argv[1]);
   } else {
-  d = opendir(".");
+    d = opendir(".");
   }
 
-  //printf("%s\n", argv);
-  //printf("%s\n", argv[1]);
-
+  char path[] = "./";
 
   /* print the i-node and name for each file: */
-  while((de = readdir(d)) != NULL)
-      printf("%s\n", de->d_name);
+  while((de = readdir(d)) != NULL) {
+      char * pathcpy = strdup(path);
+      strcat(pathcpy, de->d_name);
+      stat(pathcpy, &buf);
+      printf("%lld %s\n",  buf.st_size, de->d_name);
+  }
 
   closedir(d);
 

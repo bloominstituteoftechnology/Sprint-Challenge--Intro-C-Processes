35ef832125464f182e9b36113c8fbc46df66e2f5
Luisan327 <luisangelsc@gmail.com>
2019-01-11 10:47:13 -0600

    prints file names. w/ and w/out cmd-line arguments

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -6,6 +6,27 @@
  */
 int main(int argc, char **argv)
 {
+  struct dirent *de;
+  DIR *d;
+
+  if (argv[1] != '\0'){
+    d = opendir(argv[1]);
+  } else {
+  d = opendir(".");
+  }
+
+  //printf("%s\n", argv);
+  //printf("%s\n", argv[1]);
+
+
+  /* print the i-node and name for each file: */
+  while((de = readdir(d)) != NULL)
+      printf("%s\n", de->d_name);
+
+  closedir(d);
+
+  return 0;
+}
   // Parse command line
 
   // Open directory
@@ -13,6 +34,3 @@ int main(int argc, char **argv)
   // Repeatly read and print entries
 
   // Close directory
-
-  return 0;
-}

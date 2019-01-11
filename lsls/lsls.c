c4b1e786c25547b924785e0c16763d5881e8d3b5
cubsfan46350 <cubsfan.46350@msn.com>
2019-01-11 11:02:23 -0600

    Sucessfully parsed command line

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -7,7 +7,10 @@
 int main(int argc, char **argv)
 {
   // Parse command line
-
+  printf("\nCommand-line arguments:\n");
+  for (int count = 0; count < argc; count++) {
+    printf( " argv[%d]  %s\n", count, argv[count] );
+  }
   // Open directory
 
   // Repeatly read and print entries

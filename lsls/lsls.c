6d9890dfa36f98f586d34a0067447d6a852fbf75
Csillag61 <csillagok@gmail.com>
2019-01-11 11:41:42 -0500

    Parse command line

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,18 +1,24 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <stdlib.h>
+#include <sys/stat.h> //for the stat() call
 
 /**
  * Main
  */
-int main(int argc, char **argv)
+int main(int argc, char **argv)//argv to process commend-line arguments
+//two predefined variables - the count of the command-line arguments in argc 
+//and the individual arguments as character strings in the pointer array argv
 {
+  
+  struct dirent *pDirent;//Reads the next directory entry from the DIR* returned by opendir()
+  DIR *pDir;//way to use the opendir function
+  struct stat sizebuf;
   // Parse command line
-
-  // Open directory
-
-  // Repeatly read and print entries
-
-  // Close directory
+  if (argc < 2) {
+    printf("Usage: ./lsls directory");
+    exit(1);//using exit() function
+  }
 
   return 0;
 }

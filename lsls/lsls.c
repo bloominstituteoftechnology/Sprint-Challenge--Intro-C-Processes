e604f3977e720c97b79ce192a0ed696612aa870d
verydecent <wnjhwng@gmail.com>
2019-01-11 09:32:08 -0800

    lsls.c

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -4,15 +4,46 @@
 /**
  * Main
  */
-int main(int argc, char **argv)
+int main(int argc, char **argv) // main takes in a argv pointer. Is argc number or args given?
 {
-  // Parse command line
+  struct dirent *pointDir; // struct that finds serial # and Directory name
+  char *directoryName; // Store directory name in a pointer
 
-  // Open directory
+  if(argc == 1) // need to check that arg[1] is set to something
+  {
+    directoryName = "."; // set dir
+    printf("directoryName: %s", directoryName);
 
-  // Repeatly read and print entries
+  } else if(argc == 2) {
 
-  // Close directory
+    directoryName = argv[1];
+    printf("directoryName: %s", directoryName);
+  }
+
+  if(opendir(directoryName) == NULL)
+  {
+    printf("No such directory exists.\n");
+    return 1;
+  }
+  // Now I need to iterate and find all directories. Do... While? Or NULL check? With while loop?
+  DIR *dir = opendir(directoryName);
+  if(directoryName == NULL) // check if there is a dir to open.
+  {
+    printf("Trouble opening directory\n");
+    exit(1);
+    
+  } else {
+    
+    while((pointDir = readdir(dir)) != NULL) // reads from the directory given in opendir to read via dir Struct
+    {
+      printf ("%s\n", pointDir->d_name); // prints out a list of all directories
+    }
+
+  }
+
+  // we need to close the directory after its done running.
+  closedir(dir);
+
 
   return 0;
 }

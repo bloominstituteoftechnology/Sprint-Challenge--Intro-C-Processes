80141b72c9cd1bf8fee759a6adf43862764ada78
Joseph Chretien-Golden <joechre@gmail.com>
2019-01-11 14:33:49 -0600

    finished sprint

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -6,13 +6,40 @@
  */
 int main(int argc, char **argv)
 {
-  // Parse command line
+  // initialize dir as a the DIR data type with a pointer.
+  DIR *dir = opendir(argv[1]);
 
-  // Open directory
+  // this is the struct which refers to the DIR entry and allows certain methods on it like d_name
+  struct dirent *pDir;
 
-  // Repeatly read and print entries
+  // if there is less than 2 in the argument count then there has not been a path specified, so show the current directory contents.
+  if (argc < 2)
+  {
+    // show current if no args
+    dir = opendir(".");
+  }
+  else
+  {
+    // if the path is not valid then show an error in the command line.
+    if(dir == NULL)
+    {
+      // invalid directory
+      printf("Unable to open directory: %s\n", argv[1]);
+      // exit
 
-  // Close directory
+  return 0;
+  }
+}
+
+ // loop through 
+  while((pDir = readdir(dir)) != NULL)
+  {
+    // print off the name of the entry via d_name method
+    printf("%s\n", pDir->d_name);
+  }
+
+  // close directory
+  closedir(dir);
 
   return 0;
 }

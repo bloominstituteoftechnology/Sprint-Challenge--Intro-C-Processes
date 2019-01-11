f75dda38072ef3f0c63f10018c3159a5a6743ba6
Cameron Ray <cameronray@Camerons-MacBook-Pro.local>
2019-01-11 09:58:38 -0800

    directory list prints correctly.

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -9,12 +9,59 @@
  */
 int main(int argc, char **argv)
 {
-  // Parse command line
-  // Open directory=
-  // Repeatly read and print entries
-  // Close directory
-  // Parse command line
   
+  struct dirent *ent;
+  DIR *strm;
+  // Parse command line
+  if (argc==2) {
+    // Open directory
+    if ((strm=opendir(argv[1]))!=NULL) {
+      while ((ent=readdir(strm))!=NULL){
+        struct stat buf;
+        char subdir[1028] = "";
+        strcat(subdir, argv[1]);
+        if (argv[1][strlen(argv[1])-1]!='/') {
+          strcat(subdir,"/");
+        }
+        strcat(subdir, ent->d_name);
+        strcat(subdir,"\0");
+        stat(subdir, &buf);
+        // Repeatly read and print entries
+        if ((buf.st_mode & S_IFDIR)!=0) {
+          printf("<DIR> %s\n",ent->d_name);
+        } else {
+          printf("%lld %s\n", buf.st_size,ent->d_name);
+        }
+    }
+    // Close directory
+    closedir(strm);
+    } else {
+      printf("Program has error.");
+      exit(-1);
+  }
+  // Parse command line
+  } else if (argc==1) {
+    if ((strm=opendir("./"))!=NULL) {
+      while ((ent=readdir(strm))!=NULL){
+        struct stat buf;
+        char subdir[1028] = "";
+        strcat(subdir, "./");
+        strcat(subdir, ent->d_name);
+        strcat(subdir,"\0");
+        stat(subdir, &buf);
+        if ((buf.st_mode & S_IFDIR)!=0) {
+          printf("<DIR> %s\n",ent->d_name);
+        } else {
+          printf("%lld %s\n", buf.st_size,ent->d_name);
+        }
+    }
+    closedir(strm);
+    } else {
+      printf("Program has error.");
+      exit(-1);
+    }
+  }
+
 
   
   return 0;

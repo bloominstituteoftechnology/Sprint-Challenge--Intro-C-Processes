14a6f18ae20a77fcedd021f0f52ee7b86838ab05
Akshay Gadkari <akshay.gadkari@gmail.com>
2019-01-11 10:29:41 -0800

    MVP

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,18 +1,40 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <sys/stat.h>
+#include <string.h>
 
 /**
  * Main
  */
+
+/* int main(int argc, char **argv) */
 int main(int argc, char **argv)
 {
   // Parse command line
+  struct dirent *dirent;
+  DIR *dir;
+  /* int argc = 1; */
+  /* char *name; */
+  struct stat buf;
 
-  // Open directory
-
-  // Repeatly read and print entries
-
+  if (argv[1] != NULL) {
+    printf("%d is argc", argc);
+    dir = opendir(argv[1]);
+  }
+  else {
+    dir = opendir(".");
+  }
+  char filepath[] = "./";
+  
+  while ((dirent = readdir(dir)) != NULL) {
+    /* printf("[%s]\n", dirent -> d_name); */
+    char * pathcpy = strdup(filepath);
+    strcat(pathcpy, dirent->d_name);
+    stat(pathcpy, &buf);
+    printf("%11ld %s\n", buf.st_size, dirent->d_name);
+  }
+  
   // Close directory
-
+  closedir(dir);
   return 0;
 }

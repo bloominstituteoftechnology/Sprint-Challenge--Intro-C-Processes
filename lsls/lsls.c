a3324b6487713998cd5867b9ed1c5b2a6fe2eee2
Sasha <taylork85@gmail.com>
2019-01-11 11:40:07 -0600

    initial commit

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -4,15 +4,21 @@
 /**
  * Main
  */
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv){
+  DIR *w_dir;
   // Parse command line
+  int count;
+    for (count=0; count<argc; count++)
+        puts (argv[count]);
 
   // Open directory
+  w_dir = opendir(argv[1]);
 
   // Repeatly read and print entries
 
+
   // Close directory
+  closedir(w_dir);
 
   return 0;
 }

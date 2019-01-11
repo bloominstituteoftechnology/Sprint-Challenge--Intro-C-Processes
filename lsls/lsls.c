12a5ebf830041433a065b3efca75723e05e4d80f
Csillag61 <csillagok@gmail.com>
2019-01-11 11:50:48 -0500

    opendir function lsls2.c

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -20,5 +20,12 @@ int main(int argc, char **argv)//argv to process commend-line arguments
     exit(1);//using exit() function
   }
 
+  // Open directory
+  pDir = opendir(argv[1]);// opens the directory and returns a pointer to a variable of type DIR that will be used later. 
+  if (pDir == NULL) { //If there is an error, opendir() returns NULL.
+    printf("Cannot open directory '%s'\n", argv[1]);// The individual values of the parameters may be accessed with argv[1]
+    exit(1); //using exit() function
+  }
+
   return 0;
 }

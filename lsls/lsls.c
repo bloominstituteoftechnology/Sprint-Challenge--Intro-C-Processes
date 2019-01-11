7277935d9c75f7e50b5c033e297cab97bdd7d80e
James Cassidy <cassidyjamesw@gmail.com>
2019-01-11 13:24:40 -0500

    mvp

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,18 +1,45 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <stdlib.h>
+#include <sys/stat.h>
 
-/**
- * Main
- */
 int main(int argc, char **argv)
 {
-  // Parse command line
+  struct stat buf;
+  struct dirent *de;
+  char *dirname;
 
-  // Open directory
+  if (argc > 2)
+  {
+    printf("Error: Too many command line arguments \n");
+    exit(1);
+  }
+  else if (argc == 1)
+  {
+    dirname = ".";
+  }
+  else
+  {
+    dirname = argv[1];
+  }
 
-  // Repeatly read and print entries
+  DIR *d = opendir(dirname);
 
-  // Close directory
+  if (d == NULL)
+  {
+    printf("Error: Directory not found.\n");
+    exit(1);
+  }
 
+  char path[] = "./";
+
+  while ((de = readdir(d)) != NULL)
+  {
+    char *pathcpy = strdup(path);
+    strcat(pathcpy, de->d_name);
+    stat(pathcpy, &buf);
+    printf("%lld %s\n", buf.st_size, de->d_name);
+  }
+  closedir(d);
   return 0;
 }

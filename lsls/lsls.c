d85859be687eee9a7d526ae17ad7922e36c3be3c
duckets615 <33557104+duckets615@users.noreply.github.com>
2019-01-11 13:46:54 -0500

    MVP complete

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <stdlib.h>
 
 /**
  * Main
@@ -7,12 +8,29 @@
 int main(int argc, char **argv)
 {
   // Parse command line
+  DIR *dir;
+  struct dirent *dp;
+  char *dir_name;
+
+  if (argc >= 2) {
+    dir_name = argv[1];
+  } else {
+    dir_name = ".";
+  }
 
   // Open directory
+  if ((dir = opendir(dir_name)) == NULL) {
+    perror("Error opening directory.");
+    exit(1);
+  }
 
   // Repeatly read and print entries
+  while ((dp = readdir(dir)) != NULL) {
+    printf("%s\n", dp->d_name);
+  }
 
   // Close directory
+  closedir(dir);
 
   return 0;
 }

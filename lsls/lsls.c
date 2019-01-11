7f57325665b5f7bc18cb85b17bfd4f4e2e42cecb
MichelleOk <umbralcompendium@gmail.com>
2019-01-11 11:56:18 -0700

    final commit during s.c

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <stdlib.h>
 
 /**
  * Main
@@ -7,12 +8,27 @@
 int main(int argc, char **argv)
 {
   // Parse command line
-
+  char *dirname;
+  if (argc == 1) {
+    dirname = '.';
+  } else if (argc == 2) {
+    dirname = argv[1];
+  } else {
+    printf(stderr, "usage: lsls [dir]");
+    exit(1);
+  }
   // Open directory
-
+  DIR *dirpt = opendir(dirname);
+  if (dirpt == NULL) {
+    printf(stderr, "Error, directory [ %s ] could not be opened", dirname);
+    exit(2);
+  }
   // Repeatly read and print entries
-
+  struct dirent *entrypt;
+  while ((entrypt = readdir(dirpt)) != NULL) {
+    printf("%s\n", entrypt->d_name);
+  }
   // Close directory
-
+  closedir(dirpt);
   return 0;
 }

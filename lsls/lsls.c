487c68f3475ab5695d89b39bef4dbd4623e096b9
jaymzsocool <james.mcraejr@gmail.com>
2019-01-11 17:14:00 -0500

    solution corrections

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -6,34 +6,68 @@
 /**
  * Main
  */
+// int main(int argc, char **argv)
+// {
+//   char *dirr;
+//   // Parse command line
+//   if (argc == 1)
+//   {
+//     dirr = ".";
+//   }
+//   else
+//   {
+//     dirr = argv[1];
+//   }
+//   // Open directory
+//   DIR *dirp = opendir(dirr);
+//   if (dirp == NULL)
+//   {
+//     printf("Can't open that");
+//   }
+//   // Repeatly read and print entries
+//   struct dirent *dent;
+//   struct stat fstats;
+//   dent = readdir(dirp);
+//   while(dent != NULL)
+//   {
+//     stat(dent->d_name, &fstats);
+//     printf("%d ----- %s", fstats.st_size, dent->d_name);
+//   }
+//   // Close directory
+//   closedir(dirp);
+//   return 0;
+// }
+
 int main(int argc, char **argv)
 {
-  char *dirr;
+  char *dirname;
   // Parse command line
-  if (argc == 1)
+  if (argc >= 2)
   {
-    dirr = ".";
+    dirname = argv[1];
   }
   else
   {
-    dirr = argv[1];
+    dirname = ".";
   }
   // Open directory
-  DIR *dirp = opendir(dirr);
-  if (dirp == NULL)
-  {
-    printf("Can't open that");
-  }
+  DIR *d = opendir(dirname);
+  
   // Repeatly read and print entries
-  struct dirent *dent;
-  struct stat fstats;
-  dent = readdir(dirp);
-  while(dent != NULL)
-  {
-    stat(dent->d_name, &fstats);
-    printf("%d ----- %s", fstats.st_size, dent->d_name);
+  struct dirent *de;  
+  while((de = readdir(d)) != NULL)
+  {    
+    struct stat buf;
+    char fullpath[8000];
+    sprintf(fullpath, "%s/%s", dirname, de->d_name);
+    
+    if (stat(fullpath, &buf) == -1)
+    {
+      printf("%s\n", de->d_name);
+    }
+    printf("%10lld %s\n", buf.st_size, de->d_name);
   }
   // Close directory
-  closedir(dirp);
+  closedir(d);
   return 0;
 }

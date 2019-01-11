f8fd7033cfba2c2306859cacfc5f74e614ff5f23
Andrew Liao <andrewoliao@gmail.com>
2019-01-11 13:43:53 -0500

    Fix size output when listing directories. Complete MVP

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -2,6 +2,7 @@
 #include <dirent.h>
 #include <stdlib.h>
 #include <sys/stat.h>
+#include <string.h>
 
 /**
  * Main
@@ -12,6 +13,7 @@ int main(int argc, char **argv)
   int i;
   int length = argc - 1;
   char *commands[length];
+  char *path;
 
   for(i = 1; i< argc; i++){
   	commands[i-1] = argv[i];	
@@ -22,16 +24,21 @@ int main(int argc, char **argv)
 
   if (length == 0){
   	//default
-  	dir = opendir(".");
+  	path = "./";
+  	// dir = opendir(".");
   } else if (length == 1 ){
   	//path specified
-  	dir = opendir(commands[length-1]);
+  	path = commands[length-1];
+  	// dir = opendir(path);
   } else {
   	//too many arguments provided
-  	dir = opendir(commands[length-1]);
-  	printf("More arguments provided then expected");
+  	path = commands[length-1];
+  	// dir = opendir(path);
+   	printf("More arguments provided then expected");
   }
- 
+ 	
+ 	dir = opendir(path);
+
   if (dir == NULL){
   	printf("Error: Directory not found/n");
   	exit(1);
@@ -39,14 +46,17 @@ int main(int argc, char **argv)
 
   // Repeatly read and print entries
   struct dirent *ent = readdir(dir);
+  char full_path[200];
   char *name;
   struct stat buf;
-
+  
   while(ent != NULL){
 		name = ent->d_name;  	
-  	stat(name, &buf);
-
-		printf("%10lld", buf.st_size);
+		strcpy(full_path,path);
+		strcat(full_path,"/");
+		strcat(full_path,name);
+		stat(full_path, &buf);
+		printf("%10lld ", buf.st_size);
   	printf("    %s\n", ent->d_name);
   	ent = readdir(dir);
   }

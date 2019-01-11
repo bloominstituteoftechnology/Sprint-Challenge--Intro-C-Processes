75ddf95c3009afe170a36127a3499481f9d24ea0
jaymzsocool <james.mcraejr@gmail.com>
2019-01-11 13:13:23 -0500

    can't seem to get it working, will work on answers now

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,18 +1,39 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <sys/stat.h>
+#include <string.h>
 
 /**
  * Main
  */
 int main(int argc, char **argv)
 {
+  char *dirr;
   // Parse command line
-
+  if (argc == 1)
+  {
+    dirr = ".";
+  }
+  else
+  {
+    dirr = argv[1];
+  }
   // Open directory
-
+  DIR *dirp = opendir(dirr);
+  if (dirp == NULL)
+  {
+    printf("Can't open that");
+  }
   // Repeatly read and print entries
-
+  struct dirent *dent;
+  struct stat fstats;
+  dent = readdir(dirp);
+  while(dent != NULL)
+  {
+    stat(dent->d_name, &fstats);
+    printf("%d ----- %s", fstats.st_size, dent->d_name);
+  }
   // Close directory
-
+  closedir(dirp);
   return 0;
 }

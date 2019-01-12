0524788a787f81466e7457d707f17df44f300003
Linda Yang <lindayang@Lindas-iMac.fios-router.home>
2019-01-11 20:17:19 -0500

    Completed parse command line

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,46 +1,57 @@
 #include <stdio.h>
 #include <dirent.h>
 #include <string.h>
-#include <sys/stat.h>
+// #include <sys/stat.h>
 
 /**
  * Main
  */
 int main(int argc, char **argv)
 {
-  DIR *dir;
-  struct dirent *dp;
-  struct stat buf;
+  // DIR *dir;
+  char *dir_name;
+  // struct dirent *dp;
+  // struct stat buf;
   // Parse command line
-  if (argc < 2) {
-    printf("Open: ./lsls <directory>\n");
+  if (argc == 1) {
+    dir_name = ".";
+    printf("Directory: %s\n", dir_name);
+  } else if(argc == 2) {
+    dir_name = argv[1];
+    printf("Directory: %s\n", dir_name);
+  } else {
+    printf("Cannot open directory\n");
     return 1;
   }
+  // if (argc < 2) {
+  //   printf("Open: ./lsls <directory>\n");
+  //   return 1;
+  // }
 
   // Open directory
-  dir = opendir(argv[1]);
-  if (dir == NULL) {
-    printf("Cannot open '%s'\n", argv[1]);
-    return 1;
-  }
+  // dir = opendir(argv[1]);
+  // if (dir == NULL) {
+  //   printf("Cannot open '%s'\n", argv[1]);
+  //   return 2;
+  // }
   // if ((dir = opendir (argv[1])) == NULL) {
   //   perror("Cannot open '%s'\n", argv[1]);
   //   exit(1);
   // }
 
   // Repeatly read and print entries
-  if ((dp = readdir(dir)) != NULL) {
-    stat(dp->d_name, &buf);
-    if (strcmp(dp->d_name, argv[1]) != 0) {
-      printf("%s\n", dp->d_name);
-      printf("file size is %lld\n", buf.st_size);
+  // if ((dp = readdir(dir)) != NULL) {
+  //   stat(dp->d_name, &buf);
+  //   if (strcmp(dp->d_name, argv[1]) != 0) {
+  //     printf("%s\n", dp->d_name);
+  //     printf("file size is %lld\n", buf.st_size);
       
-      return -1;
-    }
-  }
+  //     return -1;
+  //   }
+  // }
 
   // Close directory
-  closedir(dir);
+  // closedir(dir);
 
   return 0;
 }

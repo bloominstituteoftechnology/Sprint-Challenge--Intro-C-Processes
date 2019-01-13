4f1ee94b16dc40bdedfdea9cc62d5279127a06fa
cubsfan46350 <cubsfan.46350@msn.com>
2019-01-12 18:30:15 -0600

    made some adjustments; still figuring out how to print out sizes

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,21 +1,41 @@
 #include <stdio.h>
-#include <dirent.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <dirent.h>	
+
 
 /**
  * Main
  */
+
+
+
+
+
 int main(int argc, char **argv)
 {
   // Parse command line
-  printf("\nCommand-line arguments:\n");
-  for (int count = 0; count < argc; count++) {
-    printf( " argv[%d]  %s\n", count, argv[count] );
+  char *dirname;
+  if (argc == 1) {
+    dirname = ".";
+  }
+  else if (argc == 2) {
+    dirname = argv[1];
+  }
+  else {
+    printf(stderr, "usage: lsls [dir]\n");
+    exit(1);
   }
   // Open directory
   DIR *d;
-  struct dirent *dir;
-  d = opendir("../examples");
+  d = opendir(dirname);
+  if (d == NULL) {
+    printf(stderr, "lsls: cannot open %s\n", dirname);
+    exit(2);
+  }
   // Repeatly read and print entries
+  struct dirent *dir;
   if (d) {
     while ((dir = readdir(d)) != NULL) {
       printf("%s\n", dir->d_name);

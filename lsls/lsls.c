f8cb93a31efb9d2a3778cd591799437b505a2f82
Ryan Walker <rytwalker@gmail.com>
2019-02-15 13:42:27 -0500

    answered questions

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -3,22 +3,26 @@
 #include <string.h>
 #include <stdlib.h>
 #include <sys/stat.h>
+#include <unistd.h>
 
 /**
  * Main
  */
 int main(int argc, char **argv)
 {
-  // Pointer for directory entry
   struct dirent *de;
-  // Pointer
   struct stat buf;
   char *path;
+  char full_path[100];
+  char src[100];
+  strcpy(full_path, "./");
+  // strcat(full_path, src);
 
   // Parse command line
   if (argv[1] != NULL)
   {
     path = argv[1];
+    strcat(full_path, path);
   }
   else
   {
@@ -37,14 +41,28 @@ int main(int argc, char **argv)
 
   printf("Testing path: %s\n", path);
 
+  printf("beginning: %s\n", full_path);
   // Repeatly read and print entries
   while ((de = readdir(d)) != NULL)
   {
-    printf("%llu %s\n", de->d_ino, de->d_name);
+
+    // ./ + path + de->d_name
+
+    // strcat(full_path, de->d_name);
+    printf("%s\n", full_path);
+    // if (stat(full_path, &buf) != -1)
+    {
+      printf("%10lld %s\n", buf.st_size, de->d_name);
+      // }
+      // else
+      // {
+      //   printf("There was an error accessing the file size.\n");
+      //   exit(1);
+      // }
+    }
+
+    // Close directory
+    closedir(d);
+
+    return 0;
   }
-
-  // Close directory
-  closedir(d);
-
-  return 0;
-}

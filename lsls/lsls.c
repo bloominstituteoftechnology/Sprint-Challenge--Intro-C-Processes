751bc6d3147d0fbeff19f1b5b26586222474722f
stdemicheli <stdemicheli@gmail.com>
2019-02-15 18:33:17 +0100

    Adds ability to print current directory names.

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,4 +1,12 @@
+#include <dirent.h>
+#include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <time.h>
+#include <fcntl.h>
 #include <dirent.h>
 
 /**
@@ -7,12 +15,50 @@
 int main(int argc, char **argv)
 {
   // Parse command line
-
+    
+    char *currentDir;
+    char *inputDir;
+    struct dirent *entry;
+    
+    currentDir = argv[0];
+    
+    if (argc >= 1) {
+        // If number of arguments is higher then one, then take the first argument
+        inputDir = argv[1];
+    }
+    
   // Open directory
+    DIR *dir;
+    
+    printf("Current dir: %s\n", currentDir);
+    printf("Argc : %d\n", argc);
 
-  // Repeatly read and print entries
-
+    
+    if (argc > 1) {
+        printf("Opened dir\n");
+        // Check specific directory
+        
+    } else {
+        printf("Opened current dir\n");
+        dir = opendir(".");
+        // Check current directory
+        entry = readdir(dir);
+        
+    }
+    
+    // Repeatly read and print entries
+    while (entry != NULL) {
+        printf("%s\n", entry->d_name);
+        entry = readdir(dir);
+    }
+    
+    
   // Close directory
+    
+    if (dir != NULL) {
+        closedir(dir);
+    }
 
   return 0;
 }
+

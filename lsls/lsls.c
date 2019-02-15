f144003cc15a67edd90817f68b7c2cca389acb9b
Kevin Brack <brackkevin@gmail.com>
2019-02-15 12:17:17 -0600

    Reading directory from both cl arguments and prompt

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,34 +1,56 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <string.h>
+
+#define MAX_TOKENS 2
+
+char **parse_commandline(char *str, int *argc, char **args)
+{
+  char *token;
+  *argc = 0;
+  token = strtok(str, " \t\n\r");
+
+  while (token != NULL && *argc < MAX_TOKENS)
+  {
+    args[(*argc)++] = token;
+    token = strtok(NULL, " \t\n\r");
+  }
+  args[*argc] = NULL;
+  return args;
+}
 
 /**
  * Main
  */
 int main(int argc, char **argv)
 {
-  // Parse command line
   char commandline[1024];
   struct dirent *pDirent;
   DIR *pDir;
 
-  // printf("Please enter a directory\n>>> ");
-  // fgets(commandline, sizeof(commandline), stdin);
-  // printf("COMMAND: %s\n", commandline); // <-- Debugging
-  // if (commandline[0] == '\0')
-  // {
-  //   // sets the string to "./" if no directory is chosen
-  //   commandline[0] = '.';
-  //   commandline[1] = '/';
-  //   commandline[2] = '\0';
-  // }
-  // parse arguments?
   if (argc < 2)
   {
-    printf("Usage: lsls <dirname>\n");
-    return 1;
+    // Parse command line
+    printf("Please enter a directory\n>>> ");
+    fgets(commandline, sizeof(commandline), stdin);
+    printf("COMMAND: %s\n", commandline); // <-- Debugging
+    if (commandline[0] == '\0')
+    {
+      // sets the string to "./" if no directory is chosen
+      // TODO: currently not working
+      commandline[0] = '.';
+      commandline[1] = '/';
+      commandline[2] = '\0';
+    }
+    char **clargs = parse_commandline(commandline, &argc, argv);
+    pDir = opendir(clargs[0]);
+  }
+  else
+  {
+    // parse arguments?
+    pDir = opendir(argv[1]);
   }
   // Open directory
-  pDir = opendir(argv[1]);
   if (pDir == NULL)
   {
     printf("Can not open directory %s\n", commandline);

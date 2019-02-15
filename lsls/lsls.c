eb9945537d354a782fec3ac462d0053d58d2de00
Brock Maraee Rohloff <00AgentB@gmail.com>
2019-02-15 09:55:14 -0700

    Opens Directory from Path
    - If path is invalid, exits with message to stdout.

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <stdlib.h>
 
 /**
  * Main
@@ -7,15 +8,21 @@
 int main(int argc, char **argv)
 {
   // Parse command line
-  char *dirpath;
-
+  char *path;
+  // Check for empty command line
   if (argc > 1) {
-    dirpath = argv[1];
+    path = argv[1];
   } else {
-    dirpath = ".";
+    path = ".";
   } 
 
   // Open directory
+  DIR *directory = opendir(path);
+  // Check if directory could not be opened.
+  if (directory == NULL) {
+    printf("Directory could not be opened: %s\n", path);
+    exit(1);
+  }
 
   // Repeatly read and print entries
 

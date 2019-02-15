295e21b766c2fd0ffb6eb7417e9f945b02dfc00c
ezra davis davis <ezra.rr.davis@gmail.com>
2019-02-15 09:44:55 -0800

    size and files displaying

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <dirent.h>
-
+#include <string.h>
+# include <sys/stat.h>
+# include <stdlib.h>
 /**
  * Main
  */
@@ -11,17 +13,29 @@ int main(int argc, char **argv)
   // Parse command line
 
   DIR *dir;
+ 
+  
+  struct dirent *sd;
 
-  dir = argc < 1 ? opendir(".";) : opendir(*(argv+1));
+  dir = (argc < 2) ? opendir(".") : opendir(*(argv+1));
 
   if(dir == NULL){
-    printf("Nice try OMEGALUL")
+    printf("Nice try OMEGALUL");
+  }
+
+  while( (sd=readdir(dir)) != NULL){
+    struct stat buf;
+    
+    stat((argv[1],sd->d_name), &buf);
+    printf("%ld", buf.st_size);
+    printf("              ");
+    printf("%s\n", sd->d_name);
   }
 
 
 
 
-
+  closedir(dir);
   // Open directory
 
   // Repeatly read and print entries

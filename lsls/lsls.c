d33a0360ed27ff7fd2ebc73bbc6988a1118f70c3
Steven O'Neill <oneillsteven957@gmail.com>
2019-02-15 11:27:55 -0600

    parse command line

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -6,10 +6,21 @@
  */
 int main(int argc, char **argv)
 {
+  char *dirName;
+  DIR *d;
+  struct dirent *ent;
+  struct stat buf;
   // Parse command line
-
+  if(argc < 2) {
+    dirName = ".";
+  } else if (argc == 2) {
+    dirName = argv[1];
+  } else {
+    printf("Could not open directory '%s'", argv[1]);
+    return 1;
+  }
   // Open directory
-
+  
   // Repeatly read and print entries
 
   // Close directory

e478df8db5f5adf8ad15715aef41228d9c56ba5b
Andrew <ghobrial.andrew@gmail.com>
2019-02-15 13:56:32 -0500

    main objective complete

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -9,17 +9,50 @@
  */
 int main(int argc, char **argv)
 {
-  // Parse command line
-  char *path;
-  if (argc > 1) {
-    path = argv[1];
-  } else {
-    path = ".";
-  } 
-  
-  // Open directory
 
-  // Repeatly read and print entries
+  struct dirent *dirName;
+  struct stat buffer;
+  char *dirPath;
+  char *filePathInfo;
+  DIR *dir;
+
+  // Parse command line
+  if (argc > 1) {
+    dirPath = argv[1];
+  } else {
+    dirPath = ".";
+  } 
+
+  // Open directory
+  if ((dir = opendir(dirPath)) == NULL) {
+    printf("Invalid directory: %s\n", dirPath);
+    exit(1);
+  }
+
+  // Repeatedly read and print entries:
+  while((dirName = readdir(dir)) != NULL) {
+
+    filePathInfo = malloc(strlen(path) + strlen(name -> d_name) + 2);
+
+    if (strcmp(dirName -> d_name, ".") != 0 && strcmp(dirName -> d_name, "..") != 0) {
+      sprintf(filePathInfo, "%s/%s", dirPath, dirName -> d_name);
+    } else {
+      sprintf(filePathInfo, "%s", dirName -> d_name);
+    }
+
+    // Print properties of the file as well if accessible
+    if (stat(filePathInfo, &buffer) != -1) {
+      if(buffer.st_mode == 16895) {
+        printf("<DIR> %s\n", name -> d_name);
+      } else {
+        printf("%10ld %s\n", buffer.st_size, name -> d_name);
+      }     
+    }
+    ////
+
+    free(filePathInfo);
+
+  }
 
   // Close directory
 

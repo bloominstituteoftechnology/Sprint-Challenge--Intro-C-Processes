c17e5f5e287f470573f427ad1c05be27410afdee
ezra davis davis <ezra.rr.davis@gmail.com>
2019-02-15 08:31:07 -0800

    messing around if opendir/teneries

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -10,6 +10,18 @@ int main(int argc, char **argv)
 {
   // Parse command line
 
+  DIR *dir;
+
+  dir = argc < 1 ? opendir(".";) : opendir(*(argv+1));
+
+  if(dir == NULL){
+    printf("Nice try OMEGALUL")
+  }
+
+
+
+
+
   // Open directory
 
   // Repeatly read and print entries

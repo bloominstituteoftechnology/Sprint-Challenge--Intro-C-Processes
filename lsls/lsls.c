a028be41201a2c96a06e5676296031f2b88a2255
Andrew <ghobrial.andrew@gmail.com>
2019-02-15 13:13:56 -0500

    logic to parse cmd

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <string.h>
 
 /**
  * Main
@@ -7,7 +10,13 @@
 int main(int argc, char **argv)
 {
   // Parse command line
-
+  char *path;
+  if (argc > 1) {
+    path = argv[1];
+  } else {
+    path = ".";
+  } 
+  
   // Open directory
 
   // Repeatly read and print entries

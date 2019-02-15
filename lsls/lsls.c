344ccf0e2fa7a8c013fefad867f713b85b042d45
Kevin Brack <brackkevin@gmail.com>
2019-02-15 10:43:32 -0600

    Sets up a prompt, stores as string, currently repeats it

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -7,6 +7,14 @@
 int main(int argc, char **argv)
 {
   // Parse command line
+  char commandline[1024];
+
+  while (1)
+  {
+    printf("Please enter a directory\n");
+    fgets(commandline, sizeof(commandline), stdin);
+    printf("COMMAND: %s\n", commandline); // <-- Debugging
+  }
 
   // Open directory
 

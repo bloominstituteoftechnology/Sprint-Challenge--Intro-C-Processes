a32a46488b00cef721255ad5dc129e1005663742
Max <maxkajiwara@gmail.com>
2019-02-15 10:42:43 -0800

    Still working on getting the path right

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -2,24 +2,35 @@
 #include <dirent.h>
 #include <unistd.h>
 #include <sys/stat.h>
+#include <string.h>
+#include <stdlib.h>
 
 /**
  * Main
  */
 int main(int argc, char **argv)
 {
-  // Parse command line & open directory
-  DIR *d = opendir(argv[1] ? argv[1] : ".");
+  // Parse command line
+  char *directory = argv[1] ? argv[1] : ".";
+
+  //Open directory
+  DIR *d = opendir(directory);
 
   // Repeatly read and print entries
   struct dirent *entry;
   while ((entry = readdir(d)) != NULL)
   {
     char *name = entry->d_name;
+    char *path = malloc(strlen(directory) + strlen(name) + 1);
+
+    strcpy(path, directory);
+    strcat(path, "/");
+    strcat(path, name);
 
     struct stat buf;
-    stat(name, &buf);
+    stat(path, &buf);
 
+    printf("%s:\n", path);
     printf("%10ld %s\n", buf.st_size, name);
   }
 

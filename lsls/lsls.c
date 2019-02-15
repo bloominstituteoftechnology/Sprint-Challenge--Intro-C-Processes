d1f74c738ad83573f92446ddfc3591838f1e5e85
Kevin Brack <brackkevin@gmail.com>
2019-02-15 15:17:29 -0600

    Added file sizes to output

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <dirent.h>
 #include <string.h>
+#include <sys/stat.h>
+#include <stdlib.h>
 
 #define MAX_TOKENS 2
 
@@ -19,6 +21,17 @@ char **parse_commandline(char *str, int *argc, char **args)
   return args;
 }
 
+char *combine_path(char *fdir, char *fname)
+{
+  // printf("Debugging in path %s/%s\n", fdir, fname);
+  char *result = malloc(strlen(fdir) + strlen(fname) + 2);
+  strcpy(result, fdir);
+  strcat(result, "/");
+  strcat(result, fname);
+  // printf("Debugging out path %s\n", result);
+  return result;
+}
+
 /**
  * Main
  */
@@ -27,13 +40,14 @@ int main(int argc, char **argv)
   char commandline[1024];
   struct dirent *pDirent;
   DIR *pDir;
+  char *path;
 
   if (argc < 2)
   {
     // Parse command line
     printf("Please enter a directory\n>>> ");
     fgets(commandline, sizeof(commandline), stdin);
-    printf("COMMAND: %s\n", commandline); // <-- Debugging
+    // printf("COMMAND: %s\n", commandline); // <-- Debugging
     if (commandline[0] == '\0')
     {
       // sets the string to "./" if no directory is chosen
@@ -44,11 +58,13 @@ int main(int argc, char **argv)
     }
     char **clargs = parse_commandline(commandline, &argc, argv);
     pDir = opendir(clargs[0]);
+    path = clargs[0];
   }
   else
   {
     // parse arguments?
     pDir = opendir(argv[1]);
+    path = argv[1];
   }
   // Open directory
   if (pDir == NULL)
@@ -60,7 +76,10 @@ int main(int argc, char **argv)
   // Repeatly read and print entries
   while ((pDirent = readdir(pDir)) != NULL)
   {
-    printf("%s\n", pDirent->d_name);
+    char *full_path = combine_path(path, pDirent->d_name);
+    struct stat buf;
+    stat(full_path, &buf);
+    printf("%10ld %s\n", buf.st_size, pDirent->d_name);
   }
   // Close directory
   closedir(pDir);

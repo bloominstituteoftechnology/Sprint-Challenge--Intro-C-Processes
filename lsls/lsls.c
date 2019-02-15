c7cb05509ef061feae59719fabe2c00d088a3493
Brock Maraee Rohloff <00AgentB@gmail.com>
2019-02-15 11:20:20 -0700

    Reorganize and Add Comments.

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -9,40 +9,42 @@
  */
 int main(int argc, char **argv)
 {
-  // Parse command line
+  struct dirent *name;
+  struct stat buffer;
   char *path;
-  // Check for empty command line
+  char *filepath;
+  DIR *directory;
+
+  // Parse command line and Check for empty command line:
   if (argc > 1) {
     path = argv[1];
   } else {
     path = ".";
   } 
 
-  // Open directory
-  DIR *directory = opendir(path);
-  // Check if directory could not be opened.
-  if (directory == NULL) {
+  // Open directory and Check if directory could not be opened:
+  if ((directory = opendir(path)) == NULL) {
     printf("Directory could not be opened: %s\n", path);
     exit(1);
   }
 
-  // Repeatly read and print entries
-  struct dirent *name;
-  struct stat buffer;
-  char *filepath; 
- 
+  // Repeatly read and print entries:
   while((name = readdir(directory)) != NULL) {
     filepath = malloc(strlen(path) + strlen(name -> d_name) + 1);
 
+    // Build filepath for stat() and Check if the item name is "." or "..":
     if (strcmp(name -> d_name, ".") != 0 && strcmp(name -> d_name, "..") != 0) {
       sprintf(filepath, "%s/%s", path, name -> d_name);
     } else {
       sprintf(filepath, "%s", name -> d_name);
     }
 
-    stat(filepath, &buffer);
+    // Get stats of file and Check if unable to get stats:
+    if (stat(filepath, &buffer) != -1) {
+      printf("%10ld %s\n", buffer.st_size, name -> d_name);
+    }
 
-    printf("%10ld %s\n", buffer.st_size, name -> d_name);
+    
     free(filepath);
   }
 

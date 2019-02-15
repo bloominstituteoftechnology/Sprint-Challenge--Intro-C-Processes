7ad9adb61f9e800f530534594c2cc8d313909f20
alejandrok93 <alejandrok93@gmail.com>
2019-02-15 08:54:32 -0800

    Reading argument from command line, open directory if provided

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -6,9 +6,25 @@
  */
 int main(int argc, char **argv)
 {
+  char *directory;
+  ;
   // Parse command line
+  if (argc > 1)
+  {
+    printf("more than one command!\n");
+    directory = argv[1];
+  }
 
   // Open directory
+  DIR *d;
+  if (directory)
+  {
+    d = opendir(directory);
+  }
+  else
+  {
+    d = opendir(".");
+  }
 
   // Repeatly read and print entries
 

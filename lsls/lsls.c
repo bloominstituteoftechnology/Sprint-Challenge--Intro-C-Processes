4b4bbfbb14e2c4ca1674165193d852a69327b634
Edwin Torres <edwin.torres.jr2@gmail.com>
2019-02-15 13:53:04 -0500

    reads file length and displays

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <string.h>
+#include <sys/stat.h>
+
 /**
  * Main
  */
@@ -18,19 +21,30 @@ int main(int argc, char **argv)
   }
   else if (argc > 2)
   {
-    printf("Input Error, please specify path");
+    printf("Input Error: check path syntax\n");
+    exit(1);
   }
   // Open directory
   DIR *d = opendir(dir);
+
   if (d == NULL)
   {
-    printf("Error");
+    printf("Error\n");
+    exit(1);
   }
   // Repeatly read and print entries
   struct dirent *entry;
+  struct stat buf;
+  char pathName[100];
+
   while ((entry = readdir(d)) != NULL)
   {
-    printf("%s\n", entry->d_name);
+    strcpy(pathName, dir);
+    strcat(pathName, "/");
+    strcat(pathName, entry->d_name);
+
+    stat(pathName, &buf);
+    printf("%10ld %s\n", buf.st_size, entry->d_name);
   }
 
   // Close directory

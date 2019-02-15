c4f9e380ced13de97f49eabe51119afb83f7bedc
Drew <drewsup123c@gmail.com>
2019-02-15 11:00:20 -0700

    finished with main functionality still need to print size

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -6,14 +6,32 @@
  */
 int main(int argc, char **argv)
 {
+  // printf("code ran\n");
+  // printf("0:%s\n",argv[0]);
+  // printf("1:%s\n",argv[1]);
+  // printf("2:%s\n",argv[2]);
+  // printf("DONE");
   // Parse command line
-
+  int length = argc;
+  char* starting_dir;
+  if(length >= 2){
+    starting_dir = argv[1];
+  }else{
+    starting_dir = ".";
+  }
   // Open directory
-
-  // Repeatly read and print entries
-
-  // Close directory
-
+  DIR *dir;
+  struct dirent *ent;
+  ent = readdir(dir);
+  if((dir = opendir(starting_dir)) != NULL){// Following code runs if current dir does not return Null
+    while((ent = readdir(dir)) != NULL){
+      // Repeatly read and print entries
+      printf("Read Directory : %s\n", ent->d_name);
+    }
+  }else{
+    printf("\nWARNING: Directory returned NULL please try again\n");
+  }
+  closedir(dir);
   return 0;
 }
 

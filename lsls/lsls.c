66c75fbcbe78b2df01955202b05363cfa3538e7f
ezra davis davis <ezra.rr.davis@gmail.com>
2019-02-15 10:39:46 -0800

    cleaned up some code

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -10,8 +10,7 @@
 
 char* concat(const char *s1, const char *s2)
 {
-    char *result = malloc(strlen(s1) + strlen(s2) + 1); // +1 for the null-terminator
-    // in real code you would check for errors in malloc here
+    char *result = malloc(strlen(s1) + strlen(s2) + 1);
     strcpy(result, s1);
     strcat(result, s2);
     return result;
@@ -19,23 +18,21 @@ char* concat(const char *s1, const char *s2)
 
 int main(int argc, char **argv)
 {
-  // Parse command line
-
   DIR *dir;
  
   
   struct dirent *sd;
   struct stat buf;
 
-  dir = (argc < 2) ? opendir("./") : opendir(*(argv+1));
+  dir = !argv[1]? opendir("./") : opendir(*(argv+1));
 
   if(dir == NULL){
     printf("Nice try OMEGALUL");
   }
   
   while( (sd=readdir(dir)) != NULL){    
-    
-    int error = stat((concat(argv[1] ? argv[1] : "./",sd->d_name)), &buf);
+    char* s = concat(argv[1] ? argv[1] : "./",sd->d_name);
+    int error = stat(s, &buf);
     if(error == -1){
       printf("error");
       printf("\n%s",concat(argv[1],sd->d_name));
@@ -43,17 +40,8 @@ int main(int argc, char **argv)
     printf("\n%ld", buf.st_size);
     printf("              ");
     printf("%s\n", sd->d_name);
+    free(s);
   }
-
-
-
-
   closedir(dir);
-  // Open directory
-
-  // Repeatly read and print entries
-
-  // Close directory
-
   return 0;
 }

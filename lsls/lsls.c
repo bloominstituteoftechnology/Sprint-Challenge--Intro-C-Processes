dcdcca5a7a150fdd59a3ddc444db8d09d7a00ace
Linh Bouniol <paulalinh89@gmail.com>
2019-02-15 08:10:51 -0800

    Got directories printed

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <stdlib.h>
 
 /**
  * Main
@@ -7,12 +8,29 @@
 int main(int argc, char **argv)
 {
   // Parse command line
+  // By default the current directory is represented by "."
+  char *directoryToEnumerate = ".";
 
   // Open directory
+  DIR *directoryEnumerator = opendir(directoryToEnumerate);
+  
+  if (directoryEnumerator == NULL) {
+    perror("Error opening directory");
+    exit(1);
+  }
 
   // Repeatly read and print entries
+  struct dirent *directoryEntry;
+  
+  while ((directoryEntry = readdir(directoryEnumerator))) {
+    // Get the name of the directory entry
+    char *fileName = directoryEntry->d_name;  // Use -> b/c directoryEntry is a pointer
+    
+    printf("  %s\n", fileName);
+  }
 
   // Close directory
+  closedir(directoryEnumerator);
 
   return 0;
 }

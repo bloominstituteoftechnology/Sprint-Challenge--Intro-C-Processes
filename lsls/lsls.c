76d22ecfb5ff4df4674389a782c534487798a92b
Simon Elhoej <simonelhoej@gmail.com>
2019-02-15 18:28:14 +0100

    first test on lsls

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,18 +1,43 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <string.h>
 
 /**
  * Main
  */
 int main(int argc, char **argv)
 {
-  // Parse command line
+  
+  char input[100];
+  char isRunning = 1;
 
-  // Open directory
+  printf("Enter a directory path or 'Q' to quit");
+  scanf("%s", input);
 
-  // Repeatly read and print entries
+  if (strcmp(input, "Q") == 0 || strcmp(input, "q") == 0) 
+  {
+    isRunning = 0;
+  }
 
-  // Close directory
+  while(isRunning)
+  {
+    DIR *folder;
+    struct dirent *entry;
+    folder = opendir(input);
+
+    if (folder == NULL)
+    {
+        printf("Unable to read directory");
+        return(1);
+    }
+    
+    while ((entry=readdir(folder)))
+    {
+      printf("%s, entry->d_name");
+    }
+
+    closedir(folder);
+  }
 
   return 0;
 }

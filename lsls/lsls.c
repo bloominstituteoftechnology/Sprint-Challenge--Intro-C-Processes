56956308db26eb8774847b2d700bc10c9d356642
jeanfern5 <jeanfern555@gmail.com>
2019-02-15 11:38:58 -0600

    Completed lsls.c

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-#include <dirent.h>
-
+#include <dirent.h> //includes DIR, struct dirent, opendir(), readdir(), and closedir()
+#include <sys/stat.h> //includes stat() to be used to get size of entries
 
 /**
  * Main
@@ -8,12 +8,40 @@
 int main(int argc, char **argv)
 {
   // Parse command line
+  // Open directory - opendir()
+  // Repeatly read and print entries - readdir()
+  // Close directory - closedir()
 
-  // Open directory
+  DIR *dir;
+  struct dirent *dirent;
+  struct stat filesize;
 
-  // Repeatly read and print entries
+  if (argc > 2)
+    {
+      printf("Openning...\n");
+      dir = opendir(argv[1]);
+    }
+  else if (argc < 2)
+  {
+    printf("There was no specified directory, printing content from '.' directory.\n");
+    dir = opendir(".");
+  }
+  else
+  {
+    if (dir == NULL)
+    {
+      printf("Cannot open directory '%s'.\n", argv[1]);
+      exit(1);
+    }
+  }
 
-  // Close directory
+  while ((dirent = readdir(dir)) != NULL)
+  {
+    stat("./lsls.c", &filesize);
+    printf("File Size: %lld bytes - Directory: %s\n", filesize.st_size, dirent->d_name);
+  }
 
+  closedir(dir);
   return 0;
 }
+

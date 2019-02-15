7f2362cd5fab27a56352a20edd3428c60fb7cbc5
Katie Cleary <katiecleary@airmaster.lan>
2019-02-15 11:56:08 -0500

    open/read/close directions implemented

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -6,29 +6,36 @@
  */
 int main(int argc, char **argv)
 {
+  //declarations
+  DIR *dir;
+  struct dirent *ent;
+  struct stat buf; 
+ 
+
   // Parse command line --- pass any parameters to opendir() --- more info here: [commandline.c](examples/commandline.c).
 
-  // Open directory ---  Call `opendir()`
+  // Open directory 
+  dir = opendir(argv[1]);
+  if (dir == NULL) {
+    printf("Cannot open '%s'\n", argv[1]);
+    return 1;
+  }
 
-  // Repeatly read and print entries --- repeatedly call `readdir()`
-    
-    /* RETURNS FROM `readdir()` 
-    struct dirent *ent;
-    //some code here 
-    ent = readdir(d);
-    printf("%s\n", ent->d_name);// d_name = file name, a string
-    */
-      
+  // Repeatedly read and print entries 
+  if ((ent = readdir(dir)) != NULL) {
+    stat(ent->d_name, &buf); // d_name = file name, a string
+    if (strcmp(ent->d_name, argv[1]) != 0) { // used string compare (strcmp) to test for correct file
+      printf("%s\n", ent->d_name);
+      printf("file size is %lld\n", buf.st_size); //print file size req 
+      return -1;
+    }
     /* FILE SIZE REQ:
-      struct stat buf;
-      stat("./lsls.c", &buf);
-      printf("file size is %lld\n", buf.st_size);
-       
-      // To get the size of entries, you'll need to use the `stat()
-      // Use `%10lld` to print the size in a field of width 10
+    // WHY SPECIFY FIELD WIDTH? Use `%10lld` to print the size in a field of width 10
     */
+  }
 
-  // Close directory --- call `closedir()`
+  // Close directory 
+  closedir(dir);
 
   return 0;
 }

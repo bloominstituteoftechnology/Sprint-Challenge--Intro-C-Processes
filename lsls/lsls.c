9282d9b93c13eacd8a3f77df1f3b07668d8c8e01
Steven O'Neill <oneillsteven957@gmail.com>
2019-02-15 15:26:18 -0600

    fixed some warnings
    Also added some code that kind of resembles the code that was present in lecture

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -14,17 +14,18 @@ int main(int argc, char **argv)
   // Parse command line
   if(argc < 2) {
     dirName = ".";
+    // Opens directory
     d = opendir(dirName);
-  } else if (argc > 2) {
-    dirName = argv[1];
-    // Open directory
-    d = opendir(dirName);
-  } else {
-    if(d == NULL) {
-      printf("Could not open directory '%s'\n", argv[1]);
-      exit(1);
-    }
   }
+  if (argc > 2) {
+    dirName = argv[1];
+    // Opens directory
+    d = opendir(dirName);
+  }  
+  if(d == NULL) {
+    printf("Could not open directory '%s'\n", argv[1]);
+  }
+  
   
   // Repeatly read and print entries
   while((ent = readdir(d)) != NULL) {
@@ -34,4 +35,38 @@ int main(int argc, char **argv)
   // Close directory
   closedir(d);
   return 0;
+
+  // Modified code to kind of resemble the lecture code
+
+  // char *dirName;
+  // DIR *d;
+  // struct dirent *ent;
+  // struct stat buf;
+  // // Parse command line
+  // if(argc <= 1) {
+  //   dirName = ".";
+  // }
+  // if (argc >= 2) {
+  //   dirName = argv[1];
+  // }
+
+  // // Open directory
+  // d = opendir(dirName);
+  // if(d == NULL) {
+  //   printf("Could not open directory '%s'\n", argv[1]);	
+  // }
+
+  
+  // // Repeatly read and print entries
+  // while((ent = readdir(d)) != NULL) {
+  //   char fullpath[4096];
+  //   snprintf(fullpath, sizeof(fullpath), "%s/%s", dirName, ent -> d_name);
+  //   if(stat(ent -> d_name, &buf) < 0) {
+  //     fprintf(stderr, "failed to stat file %s\n", fullpath);
+  //   }
+  //   printf("File size: %10lld  Directory: %s\n", buf.st_size, ent -> d_name);
+  // }
+  // // Close directory
+  // closedir(d);
+  // return 0;
 }

b669e835cb15fd4449a4ee93e4eaaf9a65eba783
lajawanti <myemail>
2019-02-15 13:25:22 -0500

    lsls.c -- file-size

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <dirent.h>
-#include <dirent.h>
-#include <sys/types.h>
+//#include <sys/types.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/stat.h>
 
 /**
  * Main
@@ -13,13 +13,14 @@ int main(int argc, char **argv)
     char commandline;
     DIR *directory;
     struct dirent *read_directory; 
+    struct stat fs;
 
     // Parse command line
     while (strcmp(&commandline, "exit")!= 0)
     {
         // Receive input from stdin
         // shell should print a prompt for the user
-        printf("lambda-shell(Enter exit to exit)$");
+        printf("\nlambda-shell(Enter exit to exit)$");
         scanf("%s", &commandline);
 
         if (strcmp(&commandline, "exit") == 0)
@@ -45,11 +46,20 @@ int main(int argc, char **argv)
 
         //Upon successful completion, readdir() returns a pointer to an object of type struct dirent.
         // Repeatly read and print entries
+
+        /*Stat is a system call that you can use to get information about files 
+        struct stat {
+            .....
+            off_t    st_size;     // File size in bytes 
+            ....
+        }*/
         while((read_directory = readdir(directory)) != NULL)
         {
-            printf(">> %s  %d\n", read_directory->d_name, read_directory->d_reclen);
+            printf("\n %s\t\t", read_directory->d_name);
+            int exists = stat(read_directory->d_name, &fs);
+            printf(" %ld bytes", fs.st_size);
         }
-      
+        
         // Close directory
         closedir(directory); 
 

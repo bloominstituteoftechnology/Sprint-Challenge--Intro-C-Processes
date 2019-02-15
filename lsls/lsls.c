8cbb3651cd2d4d3f1638802741396d1926b490be
lndubose <lndubose13@gmail.com>
2019-02-15 10:53:28 -0600

    Printing out size and file names
    Have the basic printing out. Now moving on to parse the command line.

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,18 +1,42 @@
 #include <stdio.h>
 #include <dirent.h>
-
+#include <stdlib.h>
+#include <sys/stat.h>
 /**
  * Main
  */
 int main(int argc, char **argv)
 {
+  DIR *odir;
+  struct dirent *rdir;
+  struct stat buf;
   // Parse command line
-
+  if (argc < 2)
+  {
+    argv[1] = "./";
+  }
   // Open directory
+  odir = opendir(".");
+  if (odir == NULL)
+  {
+    fprintf(stderr, "Cannot find directory\n");
+    exit(2);
+  }
 
   // Repeatly read and print entries
+  while ((rdir = readdir(odir)) != NULL)
+  {
+    if (stat(rdir->d_name, &buf) < 0)
+    {
+      fprintf(stderr, "Error\n");
+      exit(2);
+    }
+
+    printf("%10ld %s\n", buf.st_size, rdir->d_name);
+  }
 
   // Close directory
+  closedir(odir);
 
   return 0;
 }

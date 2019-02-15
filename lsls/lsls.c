ea4fb5b5173f9f017c00fce3e4dfdf926f938230
Ryan Walker <rytwalker@gmail.com>
2019-02-15 14:01:09 -0500

    created path maker function

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -39,30 +39,43 @@ int main(int argc, char **argv)
     exit(0);
   }
 
-  printf("Testing path: %s\n", path);
+  // printf("Testing path: %s\n", path);
 
-  printf("beginning: %s\n", full_path);
+  // printf("beginning: %s\n", full_path);
   // Repeatly read and print entries
   while ((de = readdir(d)) != NULL)
   {
 
     // ./ + path + de->d_name
 
-    // strcat(full_path, de->d_name);
-    printf("%s\n", full_path);
-    // if (stat(full_path, &buf) != -1)
+    for (int i = strlen(full_path); i > 0; i--)
     {
-      printf("%10lld %s\n", buf.st_size, de->d_name);
-      // }
-      // else
-      // {
-      //   printf("There was an error accessing the file size.\n");
-      //   exit(1);
-      // }
+      if (full_path[i] == '/')
+      {
+        for (int j = 0; j < strlen(de->d_name); j++)
+        {
+          full_path[i + j + 1] = de->d_name[j];
+        }
+        full_path[i + strlen(de->d_name) + 1] = '\0';
+        break;
+      }
     }
 
-    // Close directory
-    closedir(d);
-
-    return 0;
+    // strcat(full_path, de->d_name);
+    // printf("%s\n", full_path);
+    if (stat(full_path, &buf) != -1)
+    {
+      printf("%10lld %s\n", buf.st_size, de->d_name);
+    }
+    else
+    {
+      printf("There was an error accessing the file size.\n");
+      exit(1);
+    }
   }
+
+  // Close directory
+  closedir(d);
+
+  return 0;
+}

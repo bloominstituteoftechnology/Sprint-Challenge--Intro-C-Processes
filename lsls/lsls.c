8b661e86e833fc9ed2dd236062addaffea774701
Simon Elhoej <simonelhoej@gmail.com>
2019-02-15 19:31:14 +0100

    Implemented file size

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,43 +1,62 @@
 #include <stdio.h>
 #include <dirent.h>
 #include <string.h>
+#include <sys/stat.h>
 
 /**
  * Main
  */
 int main(int argc, char **argv)
 {
-  
-  char input[100];
+  char input[5];
   char isRunning = 1;
 
-  printf("Enter a directory path or 'Q' to quit");
-  scanf("%s", input);
-
-  if (strcmp(input, "Q") == 0 || strcmp(input, "q") == 0) 
-  {
-    isRunning = 0;
-  }
-
-  while(isRunning)
-  {
+  do {
     DIR *folder;
     struct dirent *entry;
     folder = opendir(input);
+    struct stat buf;
+    char *path;
+
+    if (argc < 2)
+    {
+      path = ".";
+    }
+    else if (argc > 2)
+    {
+      exit(1);
+    }
+    else
+    {
+      path = strtok(argv[1], "\t\n\r");
+    }
+
+    folder = opendir(path);
 
     if (folder == NULL)
     {
-        printf("Unable to read directory");
-        return(1);
+        printf("Unable to find directory");
+        exit(1);
     }
     
     while ((entry=readdir(folder)))
     {
       printf("%s, entry->d_name");
+      printf("%s, buf.st_size");
     }
 
     closedir(folder);
-  }
 
+    printf("Press Q to quit or anything else to repeat.");
+    scanf("%s", input);
+
+    if (strcmp(input, "Q") == 0 || strcmp(input, "q") == 0) 
+    {
+      isRunning = 0;
+    }
+
+  } while (isRunning)
+
+    
   return 0;
 }

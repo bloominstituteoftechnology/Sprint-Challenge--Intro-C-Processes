27d4066360fdf2db8b909cc19fe6a600fbf85577
ckirkwood94 <ckirkwood94@gmail.com>
2019-02-15 13:11:34 -0500

    lsls MVP complete

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,18 +1,44 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <stdlib.h>
+#include <sys/stat.h>
 
 /**
  * Main
  */
 int main(int argc, char **argv)
 {
-  // Parse command line
+  // Define dirname based on arg count
+  const char *dirname;
+  if (argc == 1)
+  {
+    dirname = ".";
+  }
+  else
+  {
+    dirname = argv[1];
+  }
 
   // Open directory
+  DIR *dir;
+  struct dirent *entry;
+
+  // Error handling
+  if ((dir = opendir(dirname)) == NULL)
+  {
+    fprintf(stderr, "Cannot open %s", dirname);
+    exit(1);
+  }
 
   // Repeatly read and print entries
-
+  while ((entry = readdir(dir)) != NULL)
+  {
+    struct stat buf;
+    char *fullpath = ("%s/%s", dirname, entry->d_name);
+    stat(fullpath, &buf);
+    printf("%10lld %s\n", buf.st_size, entry->d_name);
+  }
   // Close directory
-
+  closedir(dir);
   return 0;
 }

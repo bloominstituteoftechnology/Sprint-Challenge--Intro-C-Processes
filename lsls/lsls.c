2ed3dffd72096f9d05b8d9f754a0cb0869743839
Ryan Walker <rytwalker@gmail.com>
2019-02-15 11:36:22 -0500

    saving path and printing contents to commandline

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,18 +1,43 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <string.h>
 
 /**
  * Main
  */
 int main(int argc, char **argv)
 {
+  // Pointer for directory entry
+  struct dirent *de;
+  char *path;
   // Parse command line
 
+  // read input
+  // fgets(commandline, sizeof(commandline), stdin);
+
   // Open directory
+  if (argv[1] != NULL)
+  {
+    path = argv[1];
+  }
+  else
+  {
+    path = ".";
+  }
+
+  DIR *d = opendir(path);
+  printf("Testing: %s\n", d == NULL ? "FAIL" : "PASS");
+  printf("Testing path: %s\n", path);
 
   // Repeatly read and print entries
+  // Repeatly read and print entries
+  while ((de = readdir(d)) != NULL)
+  {
+    printf("%llu %s\n", de->d_ino, de->d_name);
+  }
 
   // Close directory
+  closedir(d);
 
   return 0;
 }

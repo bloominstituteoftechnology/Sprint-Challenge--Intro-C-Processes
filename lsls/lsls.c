998765f7b2ce45d235edd2e8c32b3777b7e43c7b
Brock Maraee Rohloff <00AgentB@gmail.com>
2019-02-15 11:07:28 -0700

    Prints Out Name of File and Size in Bytes.

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <dirent.h>
 #include <stdlib.h>
+#include <sys/stat.h>
+#include <string.h>
 
 /**
  * Main
@@ -26,11 +28,24 @@ int main(int argc, char **argv)
 
   // Repeatly read and print entries
   struct dirent *name;
+  struct stat buffer;
+  char *filepath; 
  
   while((name = readdir(directory)) != NULL) {
-    printf("%s\n", name -> d_name);
+    filepath = malloc(strlen(path) + strlen(name -> d_name) + 1);
+
+    if (strcmp(name -> d_name, ".") != 0 && strcmp(name -> d_name, "..") != 0) {
+      sprintf(filepath, "%s/%s", path, name -> d_name);
+    } else {
+      sprintf(filepath, "%s", name -> d_name);
+    }
+
+    stat(filepath, &buffer);
+
+    printf("%10ld %s\n", buffer.st_size, name -> d_name);
+    free(filepath);
   }
-  
+
   // Close directory
 
   return 0;

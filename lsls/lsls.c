a7eafc4dc23700d5fa876263ea1c97ac75363165
lndubose <lndubose13@gmail.com>
2019-02-15 11:13:16 -0600

    Test pass

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <dirent.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/stat.h>
 /**
  * Main
@@ -10,27 +11,36 @@ int main(int argc, char **argv)
   DIR *odir;
   struct dirent *rdir;
   struct stat buf;
+  char *path;
   // Parse command line
   if (argc < 2)
   {
-    argv[1] = "./";
+    path = ".";
   }
+  else if (argc > 2)
+  {
+    printf("usage: ./lsls pathname");
+  }
+  else
+  {
+    path = strtok(argv[1], " \t\n\r");
+  }
+
+  printf("%s\n", path);
+
   // Open directory
-  odir = opendir(".");
+  odir = opendir(path);
   if (odir == NULL)
   {
     fprintf(stderr, "Cannot find directory\n");
     exit(2);
   }
-
+  // printf("Pointer: %p\n", odir);
   // Repeatly read and print entries
   while ((rdir = readdir(odir)) != NULL)
   {
-    if (stat(rdir->d_name, &buf) < 0)
-    {
-      fprintf(stderr, "Error\n");
-      exit(2);
-    }
+    // printf("%s\n", rdir->d_name);
+    stat(rdir->d_name, &buf);
 
     printf("%10ld %s\n", buf.st_size, rdir->d_name);
   }

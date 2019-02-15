e32eb37c083d11a3e0f82a6340d2abeab7b57643
Edwin Torres <edwin.torres.jr2@gmail.com>
2019-02-15 12:48:09 -0500

    opens directory

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,17 +1,32 @@
 #include <stdio.h>
 #include <dirent.h>
-
 /**
  * Main
  */
 int main(int argc, char **argv)
 {
   // Parse command line
+  char *dir;
 
+  if (argc == 2)
+  {
+    dir = argv[1];
+  }
+  else if (argc < 2)
+  {
+    dir = ".";
+  }
+  else if (argc > 2)
+  {
+    printf("Input Error, please specify path");
+  }
   // Open directory
-
+  DIR *d = opendir(dir);
+  if (d == NULL)
+  {
+    printf("Error");
+  }
   // Repeatly read and print entries
-
   // Close directory
 
   return 0;

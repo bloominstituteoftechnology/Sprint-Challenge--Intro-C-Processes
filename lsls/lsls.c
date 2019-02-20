6dfb015f9934f0a51f2cdcf3b5c510236b2869b2
Samantha Gatt <smfgatt@gmail.com>
2019-02-19 19:25:02 -0500

    Adds a help flag and increases max character count

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <dirent.h>
 #include <stdlib.h>
+#include <string.h>
 
-#define MAX_COMMAND_CHARS 50
+#define MAX_COMMAND_CHARS 107
 
 /**
  * Main
@@ -11,9 +12,9 @@ int main(int argc, char **argv)
 {
     // Parse command line
     
-    if (argc != 2)
+    if (argc != 2 || strcmp(argv[1], "--help") == 0)
     {
-        fprintf(stderr, "usage: enter a path to a directory\n(spaces are not permitted)\n");
+        fprintf(stderr, "usage: enter a path to a directory (cannot exceed 100 characters)\n");
         exit(1);
     }
 

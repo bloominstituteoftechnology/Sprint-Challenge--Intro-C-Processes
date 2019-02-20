026591ee261752f22bd2cd6cd10b24e8fcfe3255
Samantha Gatt <smfgatt@gmail.com>
2019-02-19 19:01:07 -0500

    Parses command line and opens desired directory

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,18 +1,37 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <stdlib.h>
+
+#define MAX_COMMAND_CHARS 50
 
 /**
  * Main
  */
 int main(int argc, char **argv)
 {
-  // Parse command line
+    // Parse command line
+    
+    if (argc != 2)
+    {
+        fprintf(stderr, "usage: enter a directory to be listed\n(spaces are not permitted in directory name or path)\n");
+        exit(1);
+    }
 
-  // Open directory
+    char *path = argv[1];
 
-  // Repeatly read and print entries
+    // Open directory
 
-  // Close directory
+    DIR *dir = opendir(path);
+
+    if (dir == NULL)
+    {
+        fprintf(stderr, "error: couldn't open desired directory\n");
+        exit(1);
+    }
+
+    // Repeatly read and print entries
+
+    // Close directory
 
   return 0;
 }

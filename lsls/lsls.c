fc6a9100139e52e5d83a84bd9d5c15d0fa6ab090
Farhan Farooqui <farhan@farooqui.net>
2019-02-22 12:01:18 -0500

    parses current directory

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,14 +1,32 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <sys/stat.h>
 
 /**
  * Main
  */
 int main(int argc, char **argv)
 {
+  DIR *dir;
   // Parse command line
-
+  if (argc < 2) 
+  {
+    dir = opendir(".");
+  }else {
+    dir=opendir(argv[1]);
+  }
   // Open directory
+  struct dirent* directory_entry;
+  struct stat buf;
+
+  while(1){
+    directory_entry = readdir(dir);
+    if (directory_entry == NULL){
+      break;
+    }
+    printf("%s\n", directory_entry->d_name);
+  }
+  
 
   // Repeatly read and print entries
 

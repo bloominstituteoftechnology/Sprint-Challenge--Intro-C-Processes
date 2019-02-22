bf16fb93d9ba586204eb385e932d9eeb089644aa
SeanOne <basement365@gmail.com>
2019-02-22 12:35:07 -0800

    working

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include <dirent.h>
 
 /**
@@ -7,15 +8,39 @@
 int main(int argc, char **argv)
 {
   char commands[1024];
+  char *args[100];
+
+  char **parse_commands(char *str, int *argc, char **args) {
+    
+    char *token;
+    *argc = 0;
+
+    token = strtok(str, " \t\n\r");
+
+    while (token != NULL && *argc < 100) {
+      args[(*argc)++] = token;
+
+      token = strtok(NULL, " \t\n\r");
+    }
+
+    args[*argc] = NULL;
+
+    return args;
+  }
 
   while(1) {
-    printf("lssh$");
+    printf("lssh$ ");
 
     fgets(commands, sizeof(commands), stdin);
 
+    parse_commands(commands, &argc, args);
+
     if(argc == 0) {
-      return 0;
+      continue;
+    } else {
+      printf("%d", argc);
     }
+
 
   }
   // Parse command line

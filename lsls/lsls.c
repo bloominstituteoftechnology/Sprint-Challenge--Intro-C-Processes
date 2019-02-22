2acfede2ec9871d5d7db77f8265b15d50b776490
Kevin Sooter <kevin.m.sooter@gmail.com>
2019-02-22 10:46:22 -0600

    Command Line Args Parsed

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <stdlib.h>
 
 /**
  * Main
@@ -7,6 +8,16 @@
 int main(int argc, char **argv)
 {
   // Parse command line
+  char *path;
+  // Check for empty command line
+  if (argc > 1)
+  {
+    path = argv[1];
+  }
+  else
+  {
+    path = ".";
+  }
 
   // Open directory
 

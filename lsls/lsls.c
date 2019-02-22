9781b43c83ad54c8860e89e9a13d5e3d071502af
txiong000 <tkx422@gmail.com>
2019-02-22 11:14:44 -0600

    parsing works

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -7,7 +7,12 @@
 int main(int argc, char **argv)
 {
   // Parse command line
-  
+  printf("argc : %d\n", argc);
+  int index = 0;
+  for (index = 0; index < argc; index++) {
+    printf("argv[%d] : %s\n",index, argv[index]);
+  }
+
   // Open directory
 
   // Repeatly read and print entries

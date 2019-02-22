f2dfabc6fa1dc3b40b5b01887ddc3592635c02b6
michaelagard <mdagard@gmail.com>
2019-02-22 12:05:21 -0500

    outline problem in comments

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,18 +1,34 @@
 #include <stdio.h>
 #include <dirent.h>
 
-/**
- * Main
- */
+#define MAXLIST 100
+
+/*******
+* Main *
+*******/
+
 int main(int argc, char **argv)
+// figure out  what to do with argc and argv
 {
+  // Create command line instance ?
+  // perhaps store variable to parse here
+
   // Parse command line
+  // Create function to parse the command line
+  // take string and check against predefined strings to see if they match
+  // handle unrecognize commands
 
   // Open directory
+  // opendir(); // http://man7.org/linux/man-pages/man3/opendir.3.html
+  // change active directory
 
   // Repeatly read and print entries
+  // readdir(3); // http://pubs.opengroup.org/onlinepubs/7908799/xsh/readdir.html
+  // display active directory
+  // create a for loop to read and print all file names and their sizes
 
   // Close directory
+  // closedir(); // http://pubs.opengroup.org/onlinepubs/007904975/functions/closedir.html
 
   return 0;
 }

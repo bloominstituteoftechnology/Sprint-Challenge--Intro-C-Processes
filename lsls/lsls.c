763c57ab538c08820dbc83c454ab28826ce31115
michaelagard <mdagard@gmail.com>
2019-02-22 15:48:04 -0500

    add print directory results

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <sys/stat.h>
 
 /*******
 * Main *
@@ -12,36 +13,28 @@
 int main(int argc, char **argv)
 // figure out  what to do with argc and argv
 {
-  // int terminate = 0;
-  // while (!terminate)
-  // {
-
-  // Create command line input
-  // Pass input to Parse command line
-
   // Parse command line
-  // utilize strcmp()
-  // Create function to parse the command line
-  // take string and check against predefined strings to see if they match
-  // handle unrecognize commands
-
+  (void)argc;
+  (void)argv;
   // Open directory
-  DIR *opendir(char *path);
-  printf("%d\n", dir);
-  // opendir(); // http://man7.org/linux/man-pages/man3/opendir.3.html
-  // opens a dir stream that corresponds to the dir name
-  // returns a pointer to the dir stream.
-  // change active directory
+  DIR *d = opendir(".");
 
   // Repeatly read and print entries
-  struct dirent *readdir(DIR * d);
-  // readdir(3); // http://pubs.opengroup.org/onlinepubs/7908799/xsh/readdir.html
-  // function to display active directory
-  // for loop to read and print all file names and their sizes
+  struct dirent *de;
+
+  // print directory results
+  while ((de = readdir(d)) != NULL)
+  {
+    char *name = de->d_name;
+
+    struct stat buf;
+
+    stat(name, &buf);
+
+    printf("%10lld %s\n", buf.st_size, name);
+  }
 
   // Close directory
-  closedir(DIR * d);
-  // closedir(); // http://pubs.opengroup.org/onlinepubs/007904975/functions/closedir.html
-  // }
+  closedir(d);
   return 0;
 }

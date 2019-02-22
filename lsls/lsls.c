d83d87375436496039b7d33e217f515eeab513e2
Evan Carlstrom <evan.carlstrom@gmail.com>
2019-02-22 11:42:46 -0500

    set up basic argument handling cases for command line inputs

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -7,7 +7,20 @@
 int main(int argc, char **argv)
 {
   // Parse command line
-  /* initial commit */
+  /* check to make sure that proper arguments are being passed on the command line */
+
+  struct dirent *Directory;
+  DIR *dir;
+
+  if(dir == NULL) {
+    fprintf(stderr, "Please provide a directory!\n"); /* error handling for non-argument cases */
+  }
+  else if(argc < 2) {
+    dir = opendir(".");
+  }
+  else {
+    dir = open(argv[1]);
+  }
   // Open directory
 
   // Repeatly read and print entries

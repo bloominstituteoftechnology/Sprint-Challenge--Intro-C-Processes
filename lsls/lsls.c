10a078f07b59b31723aeed8d1cc966943b5697a5
AM_Myrick <amyrick.820@gmail.com>
2019-02-22 11:18:29 -0500

    compiles examples and begins work on lsls

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <stdlib.h>
+#include <errno.h>
 
 /**
  * Main
@@ -7,7 +9,23 @@
 int main(int argc, char **argv)
 {
   // Parse command line
+  if (argc < 2)
+	{
+		// Print current dir entries
+    void *dir = opendir(".");
 
+    if (dir == NULL)
+    {
+      printf("%d\n", errno);
+      exit(1);
+    }
+	}
+  else
+  {
+    // Print argv[1] entries
+
+  }
+  
   // Open directory
 
   // Repeatly read and print entries

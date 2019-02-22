c6a6df85752825ede03abca612e3fad5336abc43
pedro <pedroluismontesinosrivera@gmail.com>
2019-02-22 13:49:53 -0500

    working on lsls (not tested yet)

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -7,11 +7,14 @@
 int main(int argc, char **argv)
 {
   // Parse command line
-
+  char *path = argv[1] != NULL ? argv[1] : ".";
   // Open directory
-
+  DIR *dir = opendir(path);
   // Repeatly read and print entries
+  struct dirent *ent;
+  while((ent == readdir(dir)) != NULL){
 
+  }
   // Close directory
 
   return 0;

4bdc68e458f8bb0885ed17b6837cbc8fc87b496d
michaelagard <mdagard@gmail.com>
2019-02-22 12:15:59 -0500

    add more notes

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,34 +1,38 @@
 #include <stdio.h>
 #include <dirent.h>
 
-#define MAXLIST 100
-
 /*******
 * Main *
 *******/
 
+// function to print current working directory
+
+// function to take new command
+
 int main(int argc, char **argv)
 // figure out  what to do with argc and argv
 {
-  // Create command line instance ?
-  // perhaps store variable to parse here
+  while (1)
+  {
+    // Create command line instance ?
+    // perhaps store variable to parse here
 
-  // Parse command line
-  // Create function to parse the command line
-  // take string and check against predefined strings to see if they match
-  // handle unrecognize commands
+    // Parse command line
+    // Create function to parse the command line
+    // take string and check against predefined strings to see if they match
+    // handle unrecognize commands
 
-  // Open directory
-  // opendir(); // http://man7.org/linux/man-pages/man3/opendir.3.html
-  // change active directory
+    // Open directory
+    // opendir(); // http://man7.org/linux/man-pages/man3/opendir.3.html
+    // change active directory
 
-  // Repeatly read and print entries
-  // readdir(3); // http://pubs.opengroup.org/onlinepubs/7908799/xsh/readdir.html
-  // display active directory
-  // create a for loop to read and print all file names and their sizes
+    // Repeatly read and print entries
+    // readdir(3); // http://pubs.opengroup.org/onlinepubs/7908799/xsh/readdir.html
+    // display active directory
+    // for loop to read and print all file names and their sizes
 
-  // Close directory
-  // closedir(); // http://pubs.opengroup.org/onlinepubs/007904975/functions/closedir.html
-
+    // Close directory
+    // closedir(); // http://pubs.opengroup.org/onlinepubs/007904975/functions/closedir.html
+  }
   return 0;
 }

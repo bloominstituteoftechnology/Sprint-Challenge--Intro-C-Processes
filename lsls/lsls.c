9e4c05c69c4388867a452c3b2d55c0091e07fee0
txiong000 <tkx422@gmail.com>
2019-02-22 12:19:27 -0600

    mvp

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -6,18 +6,30 @@
  */
 int main(int argc, char **argv)
 {
+  char *directory;
+  DIR *dir;
+  struct dirent *dp;
   // Parse command line
   printf("argc : %d\n", argc);
   int index = 0;
   for (index = 0; index < argc; index++) {
+    if (argc < 2) {
+      directory = ".";
+      printf("directory: %s\n", directory);
+    } else if (argc == 2){
+        directory = argv[1];
+        printf("directory: %s\n", directory);
+    }  
     printf("argv[%d] : %s\n",index, argv[index]);
   }
 
   // Open directory
-
+  dir = opendir(directory);
   // Repeatly read and print entries
-
+  while ((dp = readdir(dir)) != NULL) {
+    printf("%s\n", dp->d_name);
+  }
   // Close directory
-
+  closedir(dir);
   return 0;
 }

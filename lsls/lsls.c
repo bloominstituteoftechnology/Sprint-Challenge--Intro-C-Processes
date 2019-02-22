f30c65d707071df1f4dcdc7044cff00b5da65e96
pedro <pedroluismontesinosrivera@gmail.com>
2019-02-22 15:26:28 -0500

    mvp finished

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <string.h>
+#include <stdlib.h>
 
 /**
  * Main
@@ -8,14 +12,22 @@ int main(int argc, char **argv)
 {
   // Parse command line
   char *path = argv[1] != NULL ? argv[1] : ".";
-  // Open directory
+  //Open directory
   DIR *dir = opendir(path);
   // Repeatly read and print entries
   struct dirent *ent;
-  while((ent == readdir(dir)) != NULL){
+  while ((ent = readdir(dir)) != NULL){
+    char *name = ent->d_name;
+    char directory[strlen(path) + strlen(name)];
+    strcpy(directory, path);
+    strcat(directory, "/");
+    strcat(directory, name);
 
+    struct stat buf;
+    stat(directory, &buf);
+    printf("%ld %s\n", buf.st_size, name);
   }
   // Close directory
-
+  closedir(dir);
   return 0;
 }

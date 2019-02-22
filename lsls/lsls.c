d4bad58612fea14a6cdf87f68c58ae31a943c382
Scott Bren <smbren@ncsu.edu>
2019-02-22 11:53:07 -0500

    first pass

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,18 +1,42 @@
 #include <stdio.h>
 #include <dirent.h>
 
+
+int open_directory(char *dirname) {
+  return opendir(dirname);
+}
+
+int read_directory(dirname) {
+  return readdir(dirname);
+}
+
+struct dirent *pDirent;
+DIR *pDir;
+char dir;
 /**
  * Main
  */
 int main(int argc, char **argv)
 {
   // Parse command line
+  if (argc < 2) {
+    dir = '.';
+  } else {
+    dir = argv[1];
+  }
 
   // Open directory
+  pDir = open_directory(dir);
+
+  while(pDir != NULL) {
+    pDirent = read_directory(pDir);
+    printf("dirent.d_name");
+  }
 
   // Repeatly read and print entries
 
   // Close directory
+  closedir(pDir);
 
   return 0;
 }

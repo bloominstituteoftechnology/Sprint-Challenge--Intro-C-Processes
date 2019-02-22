ac590714cfa541bbbd486c4abe5ae56bcf34509c
Riley-Brown <riley@riley.gg>
2019-02-22 14:40:35 -0500

    added code to print out file size

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <sys/stat.h>
 
 /**
  * Main
@@ -17,14 +18,24 @@ int main(int argc, char **argv)
 
   // printf("Directory to visit: %s\n", argv[1]);
   // printf("Number of Command line args: %d\n", argc);
-  int i;
 
+  // prints command line args info
+  int i;
   printf("There are %d command line argument(s):\n", argc);
 
   for (i = 0; i < argc; i++)
   {
     printf("   %s\n", argv[i]);
   }
+
+  // returns file size of directory
+  struct stat buf;
+  // stat(".", &buf);
+  (argv > 1) ? stat(argv[1], &buf) : stat(".", &buf);
+  printf("file size is %lld\n", buf.st_size);
+  // argc > 1 ? printf("File size is %lld\n", buf.st_size);
+
+  // opens directory passed in from command line
   DIR *d;
   struct dirent *dir;
   d = (argc > 1) ? opendir(argv[1]) : opendir(".");

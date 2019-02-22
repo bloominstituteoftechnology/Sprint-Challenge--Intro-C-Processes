90f6944b0060249b167551cf0c1af63f00f24610
Daniel Ferrer <danfrrr13@gmail.com>
2019-02-22 15:34:27 -0500

    Fixed core dump error.

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -14,6 +14,7 @@ int main(int argc, char **argv)
   }
   // Open directory
   DIR *d;
+  // DIR *d2;
   d = opendir(argv[1]);
   if (d == NULL) {
     printf("opendir() failed");
@@ -21,16 +22,34 @@ int main(int argc, char **argv)
   }
   // Repeatly read and print entries
   struct dirent *entry;
+  // struct dirent *entry2;
   struct stat buf;
+  // struct stat buf2;
   char path[512];
-  while (entry != NULL) {
-    entry = readdir(d);
+  // char path2[512];
+  // int i = 0;
+  while ((entry = readdir(d)) != NULL) {
+    // entry = readdir(d);
     sprintf(path, "%s/%s", argv[1], entry->d_name);
     stat(path, &buf);
     if (buf.st_mode & S_IFDIR) {
       printf("<DIR> %s\n", entry->d_name);
+      // d2 = opendir(path);
+      // if (i != 0 && i != 1) {
+      //   while (entry2 != NULL) {
+      //     entry2 = readdir(d2);
+      //     sprintf(path2, "%s/%s", path, entry2->d_name);
+      //     stat(path2, &buf2);
+      //     if (buf2.st_mode & S_IFDIR) {
+      //       printf("    <DIR> %s \n", entry2->d_name);
+      //     } else {
+      //       printf("    %ld %s \n", buf2.st_size, entry2->d_name);
+      //     }
+      //   }
+      // }
+      // i++;
     } else {
-      printf("%ld %s\n", buf.st_size, entry->d_name);
+      printf("%o%ld %s \n",buf.st_mode, buf.st_size, entry->d_name);
     }
   }
   // Close directory

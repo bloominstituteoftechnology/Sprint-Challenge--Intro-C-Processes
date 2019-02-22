17a94ef43d2c4d67e0d1f0eb7aaccdb9cf418ac5
luiscmartinez <mrluismartinezzz@gmail.com>
2019-02-22 09:32:03 -0800

    compiled successfully & prints out input directory

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,18 +1,51 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <dirent.h>
+
+void ls_dir(const char *dir_name)
+{
+  DIR *directory;
+  struct dirent *entry;
+  struct stat *buf;
+
+  directory = opendir(dir_name);
+  if (directory != NULL)
+  {
+    while ((entry = readdir(directory)) != NULL)
+    {
+      // stat(entry->d_name, &buf);
+      printf("%s\n", entry->d_name);
+    }
+    closedir(directory);
+  }
+  else
+  {
+    printf("Directory doesnt exist. %s\n", dir_name);
+
+    exit(1);
+  }
+}
 
 /**
  * Main
  */
+
 int main(int argc, char **argv)
 {
   // Parse command line
+  int x;
 
-  // Open directory
+  x = 1;
+  while (x < argc)
+  {
+    ls_dir(argv[x]);
+    x++;
+  }
 
-  // Repeatly read and print entries
-
-  // Close directory
+  if (argc == 1)
+  {
+    ls_dir(".");
+  }
 
   return 0;
 }

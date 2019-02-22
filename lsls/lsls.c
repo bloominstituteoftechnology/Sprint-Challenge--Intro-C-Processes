5ef5db8cb0b4a78954609d5a51ab309f26b61c14
Evan Carlstrom <evan.carlstrom@gmail.com>
2019-02-22 13:48:28 -0500

    finished <DIR> marking stretch goal, looking at output formatting now

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -35,13 +35,25 @@ int main(int argc, char **argv)
   while((Directory = readdir(dir)) != NULL) {
     if(argc < 2) {
       stat(Directory->d_name, &buf); /* assign properties to Directory if an entry is found */
-      printf("%10lld %s\n", buf.st_size, Directory->d_name); /* return directory length and name */
+      // printf("%10lld %s\n", buf.st_size, Directory->d_name); /* return directory length and name */
+      if(buf.st_mode == 16895) { /* tried this with 16877 which is Linux dir octal, but 16895 must be used because native OS is Windows */
+        printf("%s %s\n", "<DIR>", Directory->d_name); /* replaces d_name output with <DIR> */
+      }
+      else {
+        printf("%10lld %s\n", buf.st_size, Directory->d_name);
+      }
     }
     else {
       char *path = malloc(strlen(argv[1] + strlen(Directory->d_name) + 2));
       sprintf(path, "%s%s", argv[1], Directory->d_name);
       stat(path, &buf);
-      printf("%10lld %s\n", buf.st_size, Directory->d_name);
+      // printf("%10lld %s\n", buf.st_size, Directory->d_name);
+      if(buf.st_mode == 16895) {
+        printf("%s %s\n", "<DIR>", Directory->d_name);
+      }
+       else {
+        printf("%10lld %s\n", buf.st_size, Directory->d_name);
+      }
       free(path);
     }
   }

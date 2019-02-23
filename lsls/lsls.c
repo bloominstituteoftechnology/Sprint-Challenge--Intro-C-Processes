6d0113c7b8b7a31adebcaa763016c764598742cb
sean-one <basement365@gmail.com>
2019-02-22 17:04:21 -0800

    going to work on answers file

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,48 +1,86 @@
 #include <stdio.h>
 #include <string.h>
 #include <dirent.h>
+#include <unistd.h>
 
 /**
  * Main
  */
 int main(int argc, char **argv)
 {
-  char commands[1024];
-  char *args[100];
+  // printf("there were %d arguments", argc);
 
-  char **parse_commands(char *str, int *argc, char **args) {
+  char **get_info(char *str) {
+    struct dirent *ptr_dirent;
+
+    printf("the directory is: %s\n", str);
     
-    char *token;
-    *argc = 0;
+    DIR *ptr_dir;
+    ptr_dir = opendir(str);
 
-    token = strtok(str, " \t\n\r");
-
-    while (token != NULL && *argc < 100) {
-      args[(*argc)++] = token;
-
-      token = strtok(NULL, " \t\n\r");
+    if(ptr_dir == NULL) {
+      printf("Cannot open directory %s\n", str);
+      // exit(1);
     }
 
-    args[*argc] = NULL;
-
-    return args;
-  }
-
-  while(1) {
-    printf("lssh$ ");
-
-    fgets(commands, sizeof(commands), stdin);
-
-    parse_commands(commands, &argc, args);
-
-    if(argc == 0) {
-      continue;
-    } else {
-      printf("%d", argc);
+    while ((ptr_dirent = readdir(ptr_dir)) != NULL) {
+      printf("[%s]\n", ptr_dirent->d_name);
     }
 
-
+    closedir(ptr_dir);
+    // return 0;
   }
+
+  if (argc < 1) {
+    printf("There was an error");
+
+  } else if (argc == 1) {
+    char cwd[250];
+
+    // get current working directory
+    if (getcwd(cwd, sizeof(cwd)) != NULL) {
+      // printf("Current working directory: %s\n", cwd);
+      // print out current working directory
+      get_info(cwd);
+
+    }
+  }
+  // char commands[1024];
+  // char *args[100];
+
+  // char **parse_commands(char *str, int *argc, char **args) {
+    
+  //   char *token;
+  //   *argc = 0;
+
+  //   token = strtok(str, " \t\n\r");
+
+  //   while (token != NULL && *argc < 100) {
+  //     args[(*argc)++] = token;
+
+  //     token = strtok(NULL, " \t\n\r");
+  //   }
+
+  //   args[*argc] = NULL;
+
+  //   return args;
+  // }
+
+  // while(1) {
+  //   printf("lssh$ ");
+
+  //   fgets(commands, sizeof(commands), stdin);
+
+  //   parse_commands(commands, &argc, args);
+
+  //   if(argc == 0) {
+  //     continue;
+  //   } else {
+  //     printf("%d", argc);
+  //   }
+
+
+  // }
   // Parse command line
 
   // Open directory

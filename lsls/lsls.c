dae14c35f8a58ac851e5f0503f49230128134541
sumi419 <sasgar04@gmail.com>
2019-02-23 15:12:38 -0800

    lsls mvp met 🎉

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <sys/stat.h>
 
 /**
  * Main
@@ -7,12 +8,37 @@
 int main(int argc, char **argv)
 {
   // Parse command line
+  // char *dir_path;
+  // printf("There are %d command line argument(s):\n", argc);
+  int i;
+  for (i = 0; i < argc; i++)
+  {
+    printf("   %s\n", argv[i]);
+  }
 
+  // file size of directory
+  struct stat buf;
+  (argv > 1) ? stat(argv[1], &buf) : stat(".", &buf);
+  printf("file size is %lld\n", buf.st_size);
+
+  DIR *d;
+  struct dirent *dir;
   // Open directory
+  d = (argc > 1) ? opendir(argv[1]) : opendir(".");
+  if (d)
+  {
 
-  // Repeatly read and print entries
-
-  // Close directory
-
-  return 0;
+    // Repeatly read and print entries
+    while ((dir = readdir(d)) != NULL)
+    {
+      printf("%s\n", dir->d_name);
+    }
+    // Close directory
+    if (closedir(d) == -1)
+    {
+      perror("closedir");
+      return 0;
+    }
+  }
+  return (0);
 }

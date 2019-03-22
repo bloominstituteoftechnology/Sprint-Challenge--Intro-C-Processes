9dfed45594a2ecdfa3060a65350559a742f07444
Carlos Lantigua <clantigua954@gmail.com>
2019-03-22 11:37:50 -0400

    initial setup of args and variables

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <sys/stat.h>
 
 /**
  * Main
@@ -7,8 +8,29 @@
 int main(int argc, char **argv)
 {
   // Parse command line
+  char *dir_name;
+  DIR *dp;
+  dirent *enter;
+  stat fileStat;
 
   // Open directory
+
+  // check if no input
+  if (argc <= 1)
+  {
+    // print current directory
+    dir_name = ".";
+  }
+  // if directory in input (argv[1]), make a ref to it
+  else if (argc == 2)
+  {
+    dir_name = argv[1];
+  }
+  // catch any errors or incorrect directories
+  else
+  {
+    printf("Cannot open dir '%s'!!", argv[1]);
+  }
 
   // Repeatly read and print entries
 

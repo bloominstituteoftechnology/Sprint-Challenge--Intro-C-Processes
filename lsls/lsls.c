064510cb1cfb7dd7f9c87652860e78d8e0232020
JacobABrennan <jacobabrennan@gmail.com>
2019-03-22 13:40:29 -0400

    List directories by path

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -6,13 +6,35 @@
  */
 int main(int argc, char **argv)
 {
-  // Parse command line
-
-  // Open directory
-
-  // Repeatly read and print entries
-
-  // Close directory
-
-  return 0;
+    // Parse command line
+    char *path = ".";
+    int ARG_POS_PATH = 1;
+    if(argc > ARG_POS_PATH)
+    {
+        path = argv[ARG_POS_PATH];
+        fprintf(stdout, "%s\n", path);
+    }
+    // Open directory
+    DIR * directory_file = opendir(path);
+    fprintf(stdout, "%d, %p\n", argc, directory_file);
+    // Handle errors opening directory
+    if(directory_file == NULL)
+    {
+        fprintf(stdout, "Error opening directory: %s\n", path);
+        exit(1);
+    }
+    struct dirent * current_directory;
+    // Repeatly read and print entries
+    while(1)
+    {
+        current_directory = readdir(directory_file);
+        if(current_directory == NULL)
+        {
+            break;
+        }
+        fprintf(stdout, "%s\n", current_directory->d_name);
+    }
+    // Close directory
+    closedir(directory_file);
+    return 0;
 }

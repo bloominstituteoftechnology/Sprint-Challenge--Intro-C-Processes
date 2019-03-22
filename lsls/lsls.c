258117044d0779dc8b1a0595d6508036297eca03
Barbara Hunter <commandandcode@gmail.com>
2019-03-22 12:16:13 -0400

    parse and size

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <sys/stat.h>
 
 /**
  * Main
@@ -7,6 +8,19 @@
 int main(int argc, char **argv)
 {
   // Parse command line
+  int i;
+  for (i = 0; i < argc; i++)
+  {
+    printf("   %s\n", argv[i]);
+  }
+
+  // file size of directory
+  struct stat buf;
+  (argv > 1) ? stat(argv[1], &buf) : stat(".", &buf);
+  printf("file size is %lld\n", buf.st_size);
+
+  DIR *d;
+  struct dirent *dir;
 
   // Open directory
 

26dedb709ba4408ae8d907e2400bc4f7335dcbf0
Leianne Louis <leilouis112@gmail.com>
2019-06-07 13:54:49 -0400

    refactored so that comand line is being parsed

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -6,54 +6,71 @@
  * Main
  */
 
-void append(char* s, char c) {
-  int len = strlen(s);
-  s[len] = c;
-  s[len+1] = '\0';
-}
+
+
+// int main(int argc, char **argv)
+// {
+//   // Parse command line
+//   char dirName[1000];
+//   char fileName[255];
+//   char fullName[1000];
+//   DIR* dir;
+//   int files = 0;
+//   struct dirent *entry;
+  
+  
+//   printf("Enter directory:");
+//   fgets(dirName, 1000, stdin);
+//   dirName[strlen(dirName) - 1] = '\0';
+
+
+//   // Open directory
+//   if(strlen(dirName) == 0) {
+//     dir = opendir(".");
+//   } else {
+//     dir = opendir(dirName);
+//   }
+
+//   if(dir == NULL) {
+//     printf("Cant find that directory");
+//   } 
+
+//   // Repeatly read and print entries
+
+//   while( (entry=readdir(dir)) )
+//     {
+//       files++;
+//       struct stat buf;
+//       strncpy(fileName, entry->d_name, 254);
+//       stat(entry->d_name, &buf); 
+
+//       printf("File Name: %s, File size: %lld\n", entry->d_name, buf.st_size);
+//     }
+  
+
+
+//   // Close directory
+//   closedir(dir);
+//   return 0;
+// }
 
 int main(int argc, char **argv)
-{
-  // Parse command line
-  char dirName[1000];
-  char fileName[255];
-  char fullName[1000];
+{	
+  int i;
   DIR* dir;
-  int files = 0;
   struct dirent *entry;
-  
-  
-  printf("Enter directory:");
-  fgets(dirName, 1000, stdin);
-  dirName[strlen(dirName) - 1] = '\0';
-
-
-  // Open directory
-  if(strlen(dirName) == 0) {
-    dir = opendir(".");
+  struct stat buf;
+  if (argv > 1) {
+    dir = opendir(argv[1]);
   } else {
-    dir = opendir(dirName);
+    dir = opendir(".");
   }
-
-  if(dir == NULL) {
-    printf("Cant find that directory");
-  } 
-
-  // Repeatly read and print entries
 
   while( (entry=readdir(dir)) )
     {
-      files++;
       struct stat buf;
-      strncpy(fileName, entry->d_name, 254);
-      stat(entry->d_name, &buf); 
-
+      stat(entry->d_name, &buf);
       printf("File Name: %s, File size: %lld\n", entry->d_name, buf.st_size);
     }
-  
-
-
-  // Close directory
-  closedir(dir);
-  return 0;
+   // file size of directory
 }

fd0d605ff768d670f51da06740fb040cfd2f2f06
Sergey Osipyan <osipyansergey@gmail.com>
2019-06-07 09:40:39 -0700

    answers.md file done

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+
 
 /**
  * Main
@@ -7,9 +10,20 @@
 int main(int argc, char **argv)
 {
   // Parse command line
-
+  char *path;
+  if (argc > 1) {
+    path = argv[1];
+  } else {
+    path = ".";
+  }
   // Open directory
-
+  struct directory *d;
+  DIR *directory = opendir(path);
+  if (directory == NULL) {
+    printf("%s is not a valid directory\n", path);
+    exit(0);
+  }
+  
   // Repeatly read and print entries
 
   // Close directory
